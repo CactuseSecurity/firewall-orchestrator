fb3dba5b3df042670fda146672a460dff1e0af3e
Tim Purschke <tim@cactus.de>
2020-05-17 20:24:46 +0200

    fix3

--- roles/auth_frontend/files/web/htdocs/auth/index.php
+++ roles/auth_frontend/files/web/htdocs/auth/index.php
@@ -1,6 +1,4 @@
 <?php
-// $Id: index.php,v 1.1.2.8 2012-04-30 17:21:25 tim Exp $
-// $Source: /home/cvs/iso/package/web/htdocs/Attic/index.php,v $
 	$stamm="";
 	require_once ("db-input.php");
 	if (isset($_SESSION)) session_destroy();
@@ -10,39 +8,18 @@
 	setlocale(LC_CTYPE, "de_DE.UTF-8");
 	$request = $cleaner->clean_structure($_REQUEST);
 	$session = $cleaner->clean_structure($_SESSION);
-	
-//	require_once ('multi-language.php');
-	$page="ind";
-	$abmelden = $request['abmelden'];
-	if (isset($abmelden)  && $abmelden == "true") {
-//		if (!isset($_SESSION)) session_start();
-		require_once ("db-base.php");
-		require_once ("db-gui-config.php");
-		require_once ("operating-system.php");  // for syslogging
-		$config = new Config();
-		$_SESSION["loglevel"] = $config->getLogLevel();
-		$_SESSION["logtarget"] = $config->getLogTarget();
-		$_SESSION["logfacility"] = $config->getLogFacility();
-		$log = new LogConnection();
-		$log->log_login("ITSecOrg User " . $session["dbuser"] . " logged out.");
-		session_destroy();
-		header("Location: index.php");
-	}
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>ITSecOrg Login</title>
+<title>Firewall Orchestrator Login</title>
 <meta name="robots" content="index,follow">
 <meta http-equiv="cache-control" content="no-cache">
 <meta name="revisit-after" content="2 days">
 <meta http-equiv="content-language" content="de">
 
 <link rel="stylesheet" type="text/css" href="<?php echo $stamm ?>css/firewall.css">
-<script type="text/javascript" src="<?php echo $stamm ?>js/client.js"></script>
-<script type="text/javascript" src="<?php echo $stamm ?>js/script.js"></script>
-
 </head>
 <body class="index" onLoad="document.login.Username.focus();">
 <div id="farbe_oben"><img src="<?php echo $stamm ?>img/1p_tr.gif" width="100%" height="10"></div>
@@ -57,7 +34,7 @@
  <table align="center">
 	<tr><td><a href="http://www.cactus.de" target="_blank"><img src="<?php echo $stamm ?>img/cactus_logo.gif" width="187" height="67"></a></td></tr>
 	<tr><td width="50">&nbsp;</td></tr>
- 	<tr><td><h2>ITSecOrg Version <?php include ("version.inc.php"); ?></h2></td></tr>
+ 	<tr><td><h2>Firewall Orchestrator</h2></td></tr>
  </table>
  <p>
  &nbsp;</p><p>

a8e7c5f120f70e21f94fabcc86f337a1d060a350
NilsPur <48684538+NilsPur@users.noreply.github.com>
2020-05-29 00:00:01 +0200

    Updated Client
    -only .net core 3.1 dependence
    -separated windows/linux/mac projects for less dependencies

---
+++ client/eto.forms/Firewall-Orchestrator/JsonReader.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace Firewall_Orchestrator
+{
+    static class JsonReader
+    {
+        public static DataSet ReadString(string JsonString)
+        {
+            DataSet data = JObject.Parse(JsonString)["data"].ToObject<DataSet>();
+
+            return data;
+        }
+    }
+}

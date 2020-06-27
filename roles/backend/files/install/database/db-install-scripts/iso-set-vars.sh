5a9b650b079bd24fce9322b5c006f2549dee578b
NilsPur <48684538+NilsPur@users.noreply.github.com>
2020-06-27 12:43:46 +0200

    Removed Unused Variables and Added Double Quotes
    Corrected unused OUT and ERROR_OUT as well as Double quote to prevent globbing and word splitting.

--- roles/backend/files/install/database/db-install-scripts/iso-set-vars.sh
+++ roles/backend/files/install/database/db-install-scripts/iso-set-vars.sh
@@ -4,11 +4,11 @@
 
 PATH=$PATH:/bin:/usr/bin
 
-OUT='logger -t ITSecOrg:db-init.sh -p local6.notice'
-ERROR_OUT='logger -t ITSecOrg:db-init.sh -p local6.error'
+##!UNUSED! OUT='logger -t ITSecOrg:db-init.sh -p local6.notice'
+##!UNUSED! ERROR_OUT='logger -t ITSecOrg:db-init.sh -p local6.error'
 if [ -z "$1" ]
 then
-	if [ -z $ISOBASE ]
+	if [ -z "$ISOBASE" ]
 	then
 	        ISOBASE="/usr/share/itsecorg"
 			# echo "ISOBASE was not set. Using default directory $ISOBASE."

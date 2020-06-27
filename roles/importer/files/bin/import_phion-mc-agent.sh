908f132427e90ff416038ecf0bac9db3d92112eb
NilsPur <48684538+NilsPur@users.noreply.github.com>
2020-06-27 14:12:10 +0200

    Super Linter

--- roles/importer/files/bin/import_phion-mc-agent.sh
+++ roles/importer/files/bin/import_phion-mc-agent.sh
@@ -8,7 +8,3 @@
 
 /bin/cp -u -r /opt/phion/rangetree/configroot/* /var/phion/home/itsecorg/ >> /var/tmp/itsecorg.log 2>&1;
 /bin/chown -R itsecorg:users /var/phion/home/itsecorg >> /var/tmp/itsecorg.log 2>&1;
-
-
-
-

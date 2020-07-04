aa3fc345c90083ce7635ffd76ba3c9326b5c5580
Tim Purschke <tmp@cactus.de>
2020-07-04 21:08:13 +0200

    bit of documentation in auth module

--- roles/frontend-new/files/Blazor/FWO/FWO/Backend/Auth/AuthStateProvider.cs
+++ roles/frontend-new/files/Blazor/FWO/FWO/Backend/Auth/AuthStateProvider.cs
@@ -18,16 +18,29 @@ namespace FWO.Auth
 
             var user = new ClaimsPrincipal(identity);
 
-            return Task.FromResult(new AuthenticationState(user));
+            return Task.FromResult(new AuthenticationState(user));            
         }
 
         public void AuthenticateUser(string Username, string Password)
-        {
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, Username),
-            }, "Fake authentication type");
+        {           
 
+            string auth_type = ""; // default = not authenticated = empty
+
+            // if (correct_credentials(Username, Password))
+            // {
+                auth_type = "Fake authentication type";
+            // }          
+
+            var identity = new ClaimsIdentity
+            (
+                new Claim[] { new Claim(ClaimTypes.Name, Username) }, 
+                auth_type
+            );
+            // the above could have also be written as:
+            // Claim[] claims = new Claim[1];
+            // claims[0] = new Claim(ClaimTypes.Name, Username);
+            // ClaimsIdentity identity2 = new ClaimsIdentity(claims, "Fake");
+  
             var user = new ClaimsPrincipal(identity);
 
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));

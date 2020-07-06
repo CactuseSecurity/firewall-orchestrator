2f1d1b5acfc01845cf2ca714ecd410ae80653288
alf-cactus <alf@cactus.de>
2020-07-06 16:53:03 +0200

    cleaned up python syntax errors

--- roles/frontend/files/web/htdocs/inctxt/navi_vert_config_main.inc.php
+++ roles/frontend/files/web/htdocs/inctxt/navi_vert_config_main.inc.php
@@ -2,7 +2,7 @@
 // $Id: navi_vert_config_main.inc.php,v 1.1.2.6 2012-04-30 17:21:21 tim Exp $
 // $Source: /home/cvs/iso/package/web/htdocs/inctxt/Attic/navi_vert_config_main.inc.php,v $
 	echo '<div id="menu">' . $language->get_text_msg('settings', 'html') . '<ul>';
-	echo '<li><a href="/config/change_pw.php">' . $language->get_text_msg('change_password', 'html') . '</a></li>';
+//	echo '<li><a href="/config/change_pw.php">' . $language->get_text_msg('change_password', 'html') . '</a></li>';
 	if ($allowedToConfigureDevices) { echo '<li><a href="/config/config_system.php">' . $language->get_text_msg('change_devices', 'html') . '</a></li>'; }
 //	if ($allowedToConfigureUsers) { echo '<li><a href="/config/config_user.php">Benutzerdaten</a></li>'; }
 	if ($allowedToConfigureClients) echo '<li><a href="/config/config_client.php">' . $language->get_text_msg('change_clients', 'html') . '</a></li>';

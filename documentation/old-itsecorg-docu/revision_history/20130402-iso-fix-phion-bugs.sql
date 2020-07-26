0a98e4cb646ec739b24aadcee1d202fdfc2810ff
Tim Purschke <tim@cactus.de>
2020-07-26 10:24:38 +0200

    deleting old-itsecorg-docu
    has been moved into FWO-unused repo

--- documentation/old-itsecorg-docu/revision_history/20130402-iso-fix-phion-bugs.sql
+++
@@ -1,8 +0,0 @@
-
-Replace files:
-- importer/CACTUS/ISO/import.pm
-- importer/CACTUS/ISO/import/phion.pm
- 
-This repairs
-- readline() on unopened filehandle IN at CACTUS/ISO/import/phion.pm line 295.
-- imports errors when using % delimiter in (rule) comments

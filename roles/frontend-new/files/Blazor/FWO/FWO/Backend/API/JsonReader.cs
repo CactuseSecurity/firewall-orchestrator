d74993009604d9c06366de647047568c9114180e
NilsPur <nils@purschke.de>
2020-08-04 19:43:04 +0200

    Update

--- roles/frontend-new/files/Blazor/FWO/FWO/Backend/API/JsonReader.cs
+++ roles/frontend-new/files/Blazor/FWO/FWO/Backend/API/JsonReader.cs
@@ -1,26 +1,150 @@
-﻿//using Newtonsoft.Json.Linq;
-using System;
+﻿using FWO.Backend.Data.API;
+using Microsoft.AspNetCore.Razor.Language;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json;
+//using System.Text.Json;
 
 namespace FWO
 {
-    public class JsonReader
+    public static class JsonReader
     {
-        public static DataSet ReadString(string JsonString)
+        /*
+        public static void ReadFilter(string JsonStringFilter)
         {
-            //JsonString = "Müll";
+            JsonElement DataRoot = ReadString(JsonStringFilter);
+            
 
-            JsonDocument JsonQuery = JsonDocument.Parse(JsonString);        
+            JsonElement.ObjectEnumerator DataRootEnum = DataRoot.EnumerateObject();
 
-            DataSet data = JsonSerializer.Deserialize<DataSet>(JsonString); 
+            if (DataRootEnum.MoveNext())
+            {
+                JsonProperty JsonFilter = DataRootEnum.Current;
+                JsonElement JsonFilterData = JsonFilter.Value;
 
-            //DataSet data = JObject.Parse(JsonString)["data"].ToObject<DataSet>();
+                JsonElement.ArrayEnumerator JsonFilterEnum = JsonFilterData.EnumerateArray();
 
-            return data;
+                if (JsonFilterEnum.MoveNext())
+                {
+                    List<AttributeDefinition> AttributeDefinitions = new List<AttributeDefinition>();
+                    List<Attribute> Attributes = new List<Attribute>();
+
+                    JsonElement.ObjectEnumerator JsonFilterDataObjectEnum = JsonFilterEnum.Current.EnumerateObject();
+
+
+                    while (JsonFilterDataObjectEnum.MoveNext())
+                    {
+                        AttributeDefinitions.Add(new AttributeDefinition
+                        {
+                            Name = JsonFilterDataObjectEnum.Current.Name,
+                            Type = JsonFilterDataObjectEnum.Current.Value.ValueKind
+                        });
+
+                        switch (JsonFilterDataObjectEnum.Current.Value.ValueKind)
+                        {
+                            case JsonValueKind.Undefined:
+                                
+                                break;
+                            case JsonValueKind.Object:
+
+                                break;
+                            case JsonValueKind.Array:
+
+                                break;
+                            case JsonValueKind.String:
+                                JsonFilterDataObjectEnum.Current.Value.GetString();
+                                break;
+                            case JsonValueKind.Number:
+                                JsonFilterDataObjectEnum.Current.Value.GetDouble();
+                                break;
+                            case JsonValueKind.True:
+                                JsonFilterDataObjectEnum.Current.Value.GetBoolean();
+                                break;
+                            case JsonValueKind.False:
+                                JsonFilterDataObjectEnum.Current.Value.GetBoolean();
+                                break;
+                            case JsonValueKind.Null:
+                                
+                                break;
+                            default:
+
+                                break;
+                        }
+
+                        Attributes.Add(new Attribute
+                        {
+                            JsonFilterDataObjectEnum.Current.Value.
+                        });
+                    }
+                }
+
+                else
+                {
+
+                }
+
+                while (JsonFilterEnum.MoveNext())
+                {
+                    JsonFilterEnum.Current;
+                }
+            }
+
+            FilterData FilterData = new FilterData
+            {
+                Name = JsonFilter.Name,
+                AttributeDefinitions = AttributeDefinitions,
+
+            };
+        }
+
+        private static JsonElement ReadString(string JsonString)
+        {
+            JsonDocument jsonDocument = JsonDocument.Parse(JsonString);
+            JsonElement DataRoot = jsonDocument.RootElement.GetProperty("data");
+
+
+            return DataRoot;
+        }
+
+        private static void GetFilterRec(JsonElement CurrentElement)
+        {
+            switch (CurrentElement.ValueKind)
+            {
+                case JsonValueKind.Undefined:
+                    break;
+                case JsonValueKind.Object:
+                    CurrentElement.EnumerateObject();
+                    break;
+                case JsonValueKind.Array:
+                    CurrentElement.EnumerateArray();
+                    break;
+                case JsonValueKind.String:
+                    break;
+                case JsonValueKind.Number:
+                    CurrentElement.GetDouble();
+                    break;
+                case JsonValueKind.True:
+                //return true;
+                case JsonValueKind.False:
+                //return false;
+                case JsonValueKind.Null:
+                    //return null;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        */
+
+        public static DataTable ToDataTable(string JsonString)
+        {
+            return JObject.Parse(JsonString)["data"].ToObject<DataTable>();
+        }
+
+        public static string[] ToStringArray(string JsonString)
+        {
+            return JObject.Parse(JsonString)["data"].ToObject<string[]>();
         }
     }
 }
---
+++ roles/frontend-new/files/Blazor/FWO/FWO/Backend/Data/API/FilterData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FWO.Backend.Data.API
+{
+    public class FilterData
+    {
+        public string Name { get; set; }
+
+        public AttributeDefinition[] AttributeDefinitions { get; set; }
+
+        public FilterDataObject[] FilterObjects { get; set; }
+    }
+}

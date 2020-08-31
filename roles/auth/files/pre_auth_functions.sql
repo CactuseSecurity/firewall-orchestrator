30a6ca959552695c8a322b350d2f6db172ad8646
Tim Purschke <tmp@cactus.de>
2020-08-31 15:47:13 +0200

    visible cont.

--- roles/auth/files/pre_auth_functions.sql
+++ roles/auth/files/pre_auth_functions.sql
@@ -40,9 +40,7 @@ BEGIN
     RETURN;
 END;
 $BODY$;
-
-/* needs to be completed & tested
-
+/*
 CREATE OR REPLACE FUNCTION public.get_visible_managements_per_tenant(integer)
     RETURNS SETOF device_type 
     LANGUAGE 'plpgsql'
@@ -54,7 +52,6 @@ DECLARE
 	b_can_view_all_devices boolean;
     i_dev_id integer;
 BEGIN
---    SELECT INTO b_can_view_all_devices bool_or(tenant_can_view_all_devices) FROM tenant_to_user JOIN tenant USING (tenant_id) WHERE tenant_to_user.user_id=i_user_id;
     SELECT INTO b_can_view_all_devices tenant_can_view_all_devices FROM tenant WHERE tenant_id=i_tenant_id;
     IF b_can_view_all_devices THEN
         FOR i_mgm_id, v_mgm_name IN SELECT mgm_id, mgm_name FROM management
@@ -64,23 +61,16 @@ BEGIN
     ELSE
         -- return all managements belonging to devices the tenant can view - derive it from get_visible_devices_per_tenant:
         -- create a distinct list of all mgm_id, mgm_name tuples the tenant can view 
-
-        FOR i_mgm_id, i_mgm_name IN SELECT id, name FROM UNION get_visible_devices_per_tenant(i_tenant_id)
+--        RETURN SELECT DISTINCT mgm_id, mgm_name FROM management WHERE mgm_id WHERE mgm_id IN (SELECT id as dev_id FROM get_visible_devices_per_tenant(i_tenant_id));
+        FOR i_mgm_id, v_mgm_name IN SELECT DISTINCT mgm_id, mgm_name FROM management WHERE mgm_id IN (SELECT mgm_id FROM device WHERE dev_id IN (SELECT id FROM get_visible_devices_per_tenant(i_tenant_id))
         LOOP
-            SELECT INTO i_mgm_id mgm_id FROM device WHERE dev_id=i_dev;
-            RETURN NEXT ROW (SELECT mgm_id, mgm_name FROM management WHERE mgm_id=i_mgm_id);
+            RETURN NEXT ROW (i_mgm_id, i_mgm_name);
         END LOOP;
-
-        -- FOR i_mgm_id, v_mgm_name IN SELECT mgm_id, mgm_name FROM tenant JOIN tenant_to_device USING (tenant_id) JOIN management ON (tenant_to_device.device_id=device.dev_id) WHERE tenant.tenant_id=i_tenant_id
-        -- LOOP
-        --     RETURN NEXT i_mgm_id;
-        -- END LOOP;
     END IF;
 	RETURN;
 END;
 $BODY$;
 */
-
 /*
 CREATE OR REPLACE FUNCTION public.get_visible_managements_per_tenant(integer)
     RETURNS SETOF device_data_table 

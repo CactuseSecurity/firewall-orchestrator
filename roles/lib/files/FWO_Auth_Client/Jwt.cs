09e3840afbda83a70320f22f17baf40605eb1c07
Tim Purschke <tmp@cactus.de>
2020-09-16 22:49:20 +0200

    simplifying jwt

--- roles/lib/files/FWO_Auth_Client/Jwt.cs
+++ roles/lib/files/FWO_Auth_Client/Jwt.cs
@@ -1,45 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Principal;
-using System.Text;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
 namespace FWO.Auth.Client
 {
     public class Jwt
     {
-        private string jwt_generator_private_key_file = "/usr/local/fworch/etc/secrets/jwt_public_key.pem";
-        private readonly string publicJWTKey = "8f4ce02dabb2a4ffdb2137802b82d1283f297d959604451fd7b7287aa307dd298668cd68a432434d85f9bcff207311a833dd5b522870baf457c565c7a716e7eaf6be9a32bd9cd5420a0ebaa9bace623b54c262dcdf35debdb5388490008b9bc61facfd237c1c7058f5287881a37492f523992a2a120a497771954daf27666de2461a63117c8347fe760464e3a58b3a5151af56a0375c8b34921101c91425b65097fc69049f85589a58bb5e5570139c98d3edb179a400b3d142a30e32d1c8e9bbdb90d799fb81b4fa6fb7751acfb3529c7af022590cbb845a8390b906f725f079967b269cff8d2e6c8dbcc561b37c4bdd1928c662b79f42fe56fe108a0cf21e08";
+        private string jwt_generator_public_key_file = "../../../etc/secrets/jwt_public_key.pem";
+        private readonly string publicJWTKey;
         private readonly string TokenString;
         private readonly JwtSecurityTokenHandler Handler;
         private readonly JwtSecurityToken Token;
 
         public Jwt(string TokenString)
-        {
-            // Get private key from
-            // absolute path: "fworch_home/etc/secrets/jwt_private.key"
-            // relative path:  "../../../etc/secrets"
+        {   // Get private key from file
             try
             {
-                // privateJWTKey = File.ReadAllText("../../../../../../../etc/secrets/jwt_private.key");
-                publicJWTKey = File.ReadAllText(jwt_generator_private_key_file).TrimEnd();
+                publicJWTKey = File.ReadAllText(jwt_generator_public_key_file).TrimEnd();
             }
             catch (Exception e)
             {
                 ConsoleColor StandardConsoleColor = Console.ForegroundColor;
-
                 Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.Out.WriteAsync($"Error while trying to read private key : \n Message \n ### \n {e.Message} \n ### \n StackTrace \n ### \n {e.StackTrace} \n ### \n");
+                Console.Out.WriteAsync($"Error while trying to read public key : \n Message \n ### \n {e.Message} \n ### \n StackTrace \n ### \n {e.StackTrace} \n ### \n");
                 Console.Out.WriteAsync($"Using fallback key! \n");
-
                 Console.ForegroundColor = StandardConsoleColor;
             }
 
@@ -52,15 +40,13 @@ namespace FWO.Auth.Client
             } 
             catch (Exception e)
             {
-                Console.Out.WriteAsync($"Auth_Client:: error while reading JWT: \n Message \n ### \n {e.Message} \n ### \n StackTrace \n ### \n {e.StackTrace} \n ### \n");
+                Console.Out.WriteAsync($"Auth_Client:: error while adding (not validating) JWT: \n Message \n ### \n {e.Message} \n ### \n StackTrace \n ### \n {e.StackTrace} \n ### \n");
             }
         } 
 
         static private byte[] FromBase64Url(string base64Url)
         {
-            // return Convert.FromBase64String(base64Url);
             string padded = base64Url.Length % 4 == 0 ? base64Url : base64Url + "====".Substring(base64Url.Length % 4);
-            // return Convert.FromBase64String(padded);
             return Convert.FromBase64String(padded.Replace("_", "/").Replace("-", "+"));
         }
 
@@ -72,15 +58,12 @@ namespace FWO.Auth.Client
             if (tokenParts[2] == null || tokenParts[2] == String.Empty)
                 return false;
             bool isPrivateKey = false;
-            int bytesRead = 0;
             bool verified = true;
-            string pubKey = AuthClient.ExtractKeyFromPemAsString(publicJWTKey, isPrivateKey);
-            RsaSecurityKey pubKeyRsa = AuthClient.ExtractKeyFromPem(publicJWTKey, false);
             
             try
             { // source: https://stackoverflow.com/questions/34403823/verifying-jwt-signed-with-the-rs256-algorithm-using-public-key-in-c-sharp (#29)                
                 RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-                rsa.ImportSubjectPublicKeyInfo(Convert.FromBase64String(pubKey), out bytesRead);
+                rsa.ImportSubjectPublicKeyInfo(Convert.FromBase64String(AuthClient.ExtractKeyFromPemAsString(publicJWTKey, isPrivateKey)), out _);
 
                 TokenValidationParameters validationParameters = new TokenValidationParameters
                 {
@@ -91,12 +74,9 @@ namespace FWO.Auth.Client
                     ValidateLifetime = true,
                     IssuerSigningKey = new RsaSecurityKey(rsa)
                 };
-                // invalidate Token
-                string faultyTokenString = TokenString + "fault";
                 SecurityToken validatedSecurityToken = null;
                 JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
                 handler.ValidateToken(TokenString, validationParameters, out validatedSecurityToken);
-                // handler.ValidateToken(faultyTokenString, validationParameters, out validatedSecurityToken); // todo: write tests
                 JwtSecurityToken validatedJwt = validatedSecurityToken as JwtSecurityToken;
             }
             catch (SecurityTokenInvalidSignatureException SignFault) {

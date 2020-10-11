9523a2a4c0ddcec0cbc5d1e1201c6a4090721302
NilsPur <48684538+NilsPur@users.noreply.github.com>
2020-10-11 11:44:46 +0200

    Update

--- roles/frontend/files/FWO_UI/Data/API/Service.cs
+++ roles/frontend/files/FWO_UI/Data/API/Service.cs
@@ -8,13 +8,25 @@ namespace FWO.Ui.Data.Api
 {
     public class Service
     {
+        [JsonPropertyName("svc_id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("svc_uid")]
+        public string Uid { get; set; }
+
         [JsonPropertyName("svc_name")]
         public string Name { get; set; }
 
         [JsonPropertyName("svc_port")]
         public int? Port { get; set; }
 
+        [JsonPropertyName("svc_port_end")]
+        public int? PortEnd { get; set; }
+
         [JsonPropertyName("stm_svc_typ")]
         public ServiceType Type { get; set; }
+
+        [JsonPropertyName("svc_comment")]
+        public string Comment { get; set; }
     }
 }

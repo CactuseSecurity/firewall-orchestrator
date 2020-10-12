e3da22f624b8cd61feb75433c2c31bee76940529
Tim Purschke <tmp@cactus.de>
2020-10-12 19:17:19 +0200

    dealing with anonymous  user (empty username) and tenants

--- roles/lib/files/FWO_Logging/Log.cs
+++ roles/lib/files/FWO_Logging/Log.cs
@@ -61,7 +61,7 @@ namespace FWO.Logging
             string File = Path.Split('\\', '/').Last();
             ConsoleColor StandardBackgroundColor = Console.BackgroundColor;
             ConsoleColor StandardForegroundColor = Console.ForegroundColor;
-            WriteInColor($"{LogType} - {Title} ({File} in line {Line}: {Text}", StandardForegroundColor, StandardBackgroundColor, ForegroundColor, BackgroundColor);
+            WriteInColor($"{LogType} - {Title} ({File} in line {Line}: {Text})", StandardForegroundColor, StandardBackgroundColor, ForegroundColor, BackgroundColor);
             Console.WriteLine("");
         }
 

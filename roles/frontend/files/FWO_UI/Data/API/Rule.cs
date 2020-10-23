1aa43ffb1716917e2c7c72c80b69fe980eae31e5
NilsPur <nils@purschke.de>
2020-10-23 20:46:09 +0200

    remove test error and cleanup

--- roles/frontend/files/FWO_UI/Data/API/Rule.cs
+++ roles/frontend/files/FWO_UI/Data/API/Rule.cs
@@ -65,11 +65,6 @@ namespace FWO.Ui.Data.API
 
         [JsonPropertyName("rule_track")]
         public string Track { get; set; }
-
-        public string DisplayDestinationZone()
-        {
-            return DestinationZone?.Name;
-        }
     }
 }
 

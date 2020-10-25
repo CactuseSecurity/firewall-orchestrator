6e9388ffdb3ec0b467ca82ff880ddfe6ac8e0c9e
NilsPur <nils@purschke.de>
2020-10-25 18:43:13 +0100

    Fetch and display rule user
    -sidebar
    -rule table

--- roles/frontend/files/FWO_UI/Data/API/NetworkLocation.cs
+++ roles/frontend/files/FWO_UI/Data/API/NetworkLocation.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace FWO.Ui.Data.API
 {
-    public class NetworkObjectWrapper
+    public class NetworkLocation
     {
         [JsonPropertyName("object")]
-        public NetworkObject Content { get; set; }
+        public NetworkObject Object { get; set; }
+
+        [JsonPropertyName("usr")]
+        public NetworkUser User { get; set; }
     }
 }
--- roles/frontend/files/FWO_UI/Data/API/Rule.cs
+++ roles/frontend/files/FWO_UI/Data/API/Rule.cs
@@ -46,7 +46,7 @@ namespace FWO.Ui.Data.API
         public Zone SourceZone { get; set; }
 
         [JsonPropertyName("rule_froms")]
-        public NetworkObjectWrapper[] Froms { get; set; }
+        public NetworkLocation[] Froms { get; set; }
 
         [JsonPropertyName("rule_dst_neg")]
         public bool DestinationNegated { get; set; }
@@ -58,7 +58,7 @@ namespace FWO.Ui.Data.API
         public Zone DestinationZone { get; set; }
 
         [JsonPropertyName("rule_tos")]
-        public NetworkObjectWrapper[] Tos { get; set; }
+        public NetworkLocation[] Tos { get; set; }
 
         [JsonPropertyName("rule_action")]
         public string Action { get; set; }

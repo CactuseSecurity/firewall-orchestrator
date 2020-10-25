63e80c85c4adbd66f9d1e370d85d7280902e10c5
Tim Purschke <tmp@cactus.de>
2020-10-25 17:20:21 +0100

    moving to latest ansible in preparation of using postgresql modules from community

--- scripts/fworch-webhook-receiver.py
+++ scripts/fworch-webhook-receiver.py
@@ -77,11 +77,11 @@ def post_handler():
             os.system("cp " + tmp_git_dir + "/scripts/fworch-webhook-receiver.py " + root_dir)
             # might not work due to user rights:
             # os.system("systemctl restart fworch-webhook-receiver.service")
-         uninstall_cmd = "cd " + tmp_git_dir + "/firewall-orchestrator && ssh-agent bash -c 'ssh-add " + ssh_priv_key_file + " && " + \
+         uninstall_cmd = "cd " + tmp_git_dir + "/firewall-orchestrator && ssh-agent bash -c '" + \
             "ansible-playbook -i inventory site.yml -e \"installation_mode=uninstall\"" + "'"
          f.write('executing uninstall command: ' + uninstall_cmd + '\n')
          os.system(uninstall_cmd) # remove fworch
-         build_cmd = "cd " + tmp_git_dir + "/firewall-orchestrator && ssh-agent bash -c 'ssh-add " + ssh_priv_key_file + " && " + \
+         build_cmd = "cd " + tmp_git_dir + "/firewall-orchestrator && ssh-agent bash -c '" + \
             "ansible-playbook -i inventory site.yml -e \"testkeys=yes installation_mode=new\" --skip-tags \"test\"" + "'"
          f.write('executing build command: ' + build_cmd + '\n')
          os.system(build_cmd) # building fworch

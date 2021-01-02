54fa8db21f11e6d19475bba8872c5ee9fa06f121
NilsPur <nils@purschke.de>
2021-01-02 03:08:25 +0100

    Fix statistics report

--- roles/lib/files/FWO_API_Client/Data/ObjectStatistics.cs
+++ roles/lib/files/FWO_API_Client/Data/ObjectStatistics.cs
@@ -5,13 +5,13 @@ namespace FWO.Api.Data
     public class ObjectStatistics
     {
         [JsonPropertyName("aggregate")]
-        public ObjectAggregate ObjectAggregate { get; set; }
+        public ObjectAggregate ObjectAggregate { get; set; } = new ObjectAggregate();
     }
 
     public class ObjectAggregate
     {
         [JsonPropertyName("count")]
-        public int ObjectCount { get; set; }
+        public int ObjectCount { get; set; } = 0;
 
     }
 

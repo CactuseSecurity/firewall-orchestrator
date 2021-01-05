603e243e8718c706a39596997c8bed68b4436c42
Y4nnikH <60013090+Y4nnikH@users.noreply.github.com>
2021-01-05 10:14:33 +0100

    simple collapse all button

---
+++ roles/ui/files/FWO_UI/Data/CollapseState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FWO.Ui.Data
+{
+    public delegate void Notify(bool show);
+    public class CollapseState
+    {
+        public static event Notify OnCollapseAll;
+
+        public static void CollapseAll()
+        {
+            OnCollapseAll?.Invoke(false);
+        }
+    }
+}

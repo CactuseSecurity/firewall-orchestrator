38136051178900f6f11e9f6ac683cc97fd33cb9c
Tim Purschke <tmp@cactus.de>
2021-01-11 10:27:35 +0100

    fixing foreign key addition

--- roles/database/files/upgrade/5.1.06.sql
+++ roles/database/files/upgrade/5.1.06.sql
@@ -1,2 +1,6 @@
 
-Alter table "report_schedule_format" add foreign key ("report_schedule_id") references "report_schedule" ("report_schedule_id") on update restrict on delete cascade;
+ALTER TABLE "report_schedule_format" DROP CONSTRAINT IF EXISTS report_schedule_format_report_schedule_id_fkey; 
+
+Alter table if exists "report_schedule_format" 
+    add constraint report_schedule_format_report_schedule_id_fkey foreign key ("report_schedule_id") references "report_schedule" ("report_schedule_id") 
+    on update restrict on delete cascade;

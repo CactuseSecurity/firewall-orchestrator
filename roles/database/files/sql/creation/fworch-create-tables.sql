63527f8d3a2eecae2c058580d8ffdfc84ab58f9e
Tim Purschke <tmp@cactus.de>
2021-01-21 11:21:30 +0100

    report_template corrections

--- roles/database/files/sql/creation/fworch-create-tables.sql
+++ roles/database/files/sql/creation/fworch-create-tables.sql
@@ -886,7 +886,7 @@ Create table if not exists "report_schedule"
 	"report_schedule_id" BIGSERIAL,
 	"report_schedule_name" Varchar, --  NOT NULL Default "Report_"|"report_id"::VARCHAR,  -- user given name of a report
 	"report_template_id" Integer, --FK
-	"report_schedule_owner" Integer, --FK
+	"report_schedule_owner" Integer NOT NULL, --FK
 	"report_schedule_start_time" Timestamp NOT NULL,  -- if day is bigger than 28, simply use the 1st of the next month, 00:00 am
 	"report_schedule_repeat" Integer Not NULL Default 0, -- 0 do not repeat, 1 daily, 2 weekly, 3 monthly, 4 yearly 
 	"report_schedule_every" Integer Not NULL Default 1, -- x - every x days/weeks/months/years
---
+++ roles/database/files/upgrade/5.1.09.sql
@@ -0,0 +1,5 @@
+
+-- default report templates belong to user 0 
+UPDATE "report_template" SET "report_template_owner" = 0; -- defining all templates to be default templates
+-- make schedule owner mandatory
+Alter table "report_schedule" ALTER COLUMN "report_schedule_owner" SET NOT NULL;

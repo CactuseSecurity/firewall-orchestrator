fc23ef952eeb0232ce9f46e153a727705a9287dc
Tim Purschke <tmp@cactus.de>
2021-05-28 06:42:35 +0200

    adding missing port in txt table

--- roles/database/files/sql/creation/fworch-fill-stm.sql
+++ roles/database/files/sql/creation/fworch-fill-stm.sql
@@ -678,6 +678,8 @@ INSERT INTO txt VALUES ('debug_level',          'German', 	'Debug Stufe');
 INSERT INTO txt VALUES ('debug_level',          'English', 	'Debug Level');
 INSERT INTO txt VALUES ('device_type',          'German', 	'Gerätetyp');
 INSERT INTO txt VALUES ('device_type',          'English', 	'Device Type');
+INSERT INTO txt VALUES ('port',                 'German', 	'Port');
+INSERT INTO txt VALUES ('port',                 'English', 	'Port');
 INSERT INTO txt VALUES ('import_user',          'German', 	'Import Nutzer');
 INSERT INTO txt VALUES ('import_user',          'English', 	'Import User');
 INSERT INTO txt VALUES ('login_secret',         'German', 	'Privater Schlüssel');

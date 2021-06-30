c643d658af7186310ee639d5164015276a733563
Tim Purschke <tmp@cactus.de>
2021-06-30 08:47:23 +0200

    adding v4 advanced rollback script

--- roles/database/files/sql/idempotent/fworch-import.sql
+++ roles/database/files/sql/idempotent/fworch-import.sql
@@ -272,7 +272,7 @@ $$ LANGUAGE plpgsql;
 --
 CREATE OR REPLACE FUNCTION rollback_import_of_mgm(INTEGER) RETURNS VOID AS $$
 DECLARE
-	i_mgm_id ALIAS FOR $1; -- ID des zurueckzuruderndes Managements
+	i_mgm_id ALIAS FOR $1; -- ID of the management which needs to be rolled back
 	last_import_time RECORD;
 	i_cur_ctrl_id BIGINT;
 BEGIN
@@ -291,8 +291,81 @@ BEGIN
 		DELETE FROM object		WHERE obj_create=i_cur_ctrl_id;
 		DELETE FROM usr			WHERE user_create=i_cur_ctrl_id;
 		DELETE FROM rule		WHERE rule_create=i_cur_ctrl_id;
-		-- DELETE FROM rule_order		WHERE control_id=i_cur_ctrl_id; 		-- Loeschen der Regelreihenfolge
 		DELETE FROM import_control	WHERE control_id = i_cur_ctrl_id; 		-- abschliessend Loeschen des Control-Eintrags
+	ELSE  
+		RAISE NOTICE 'No running import found for management id %', i_mgm_id;
+	END IF;
+	RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- this function is only necessary for versions previous to 5.x
+CREATE OR REPLACE FUNCTION rollback_import_of_mgm_without_cascade(INTEGER) RETURNS VOID AS $$
+DECLARE
+	i_mgm_id ALIAS FOR $1; -- ID of the management which needs to be rolled back
+	last_import_time RECORD;
+	i_hanging_import INTEGER;
+	i_last_working_import INTEGER;
+BEGIN
+	SELECT INTO i_hanging_import MAX(control_id) FROM import_control WHERE mgm_id=i_mgm_id AND stop_time is NULL;
+	IF FOUND THEN
+		SELECT INTO i_last_working_import * FROM get_previous_import_id(i_hanging_import);
+		DELETE FROM import_service	WHERE control_id = i_hanging_import;
+		DELETE FROM import_object	WHERE control_id = i_hanging_import;
+		DELETE FROM import_rule		WHERE control_id = i_hanging_import;
+		DELETE FROM import_zone		WHERE control_id = i_hanging_import;
+		DELETE FROM import_user		WHERE control_id = i_hanging_import;
+
+		-- deleting all changelog records created during hanging import
+		DELETE FROM changelog_service	WHERE control_id = i_hanging_import;
+		DELETE FROM changelog_object	WHERE control_id = i_hanging_import;
+		DELETE FROM changelog_user		WHERE control_id = i_hanging_import;
+		DELETE FROM changelog_rule		WHERE control_id = i_hanging_import;
+
+		-- reverting last_seen to last working import:
+		UPDATE objgrp 		SET import_last_seen=i_last_working_import	WHERE import_last_seen=i_hanging_import;
+		UPDATE objgrp_flat 	SET import_last_seen=i_last_working_import	WHERE import_last_seen=i_hanging_import;
+		UPDATE svcgrp 		SET import_last_seen=i_last_working_import 	WHERE import_last_seen=i_hanging_import;
+		UPDATE svcgrp_flat 	SET import_last_seen=i_last_working_import	WHERE import_last_seen=i_hanging_import;
+		UPDATE usergrp 		SET import_last_seen=i_last_working_import 	WHERE import_last_seen=i_hanging_import;
+		UPDATE usergrp_flat SET import_last_seen=i_last_working_import 	WHERE import_last_seen=i_hanging_import;
+		UPDATE service 		SET svc_last_seen=i_last_working_import 	WHERE svc_last_seen=i_hanging_import;
+		UPDATE object 		SET obj_last_seen=i_last_working_import 	WHERE obj_last_seen=i_hanging_import;
+		UPDATE usr 			SET user_last_seen=i_last_working_import 	WHERE user_last_seen=i_hanging_import;
+		UPDATE rule_service SET rs_last_seen=i_last_working_import 		WHERE rs_last_seen=i_hanging_import;
+		UPDATE rule_from 	SET rf_last_seen=i_last_working_import 		WHERE rf_last_seen=i_hanging_import;
+		UPDATE rule_to 		SET rt_last_seen=i_last_working_import 		WHERE rt_last_seen=i_hanging_import;
+		UPDATE rule 		SET rule_last_seen=i_last_working_import 	WHERE rule_last_seen=i_hanging_import;
+
+		-- remove pointers to rules and objects created during hanging import:
+		DELETE FROM rule_service	WHERE rule_id IN (SELECT rule_id FROM rule where rule_create=i_hanging_import);
+		DELETE FROM rule_service 	WHERE svc_id IN (SELECT svc_id FROM service where svc_create=i_hanging_import);
+		DELETE FROM rule_from 		WHERE rule_id IN (SELECT rule_id FROM rule where rule_create=i_hanging_import);
+		DELETE FROM rule_from 		WHERE obj_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+		DELETE FROM rule_from 		WHERE user_id IN (SELECT user_id FROM usr where user_create=i_hanging_import);
+		DELETE FROM rule_to 		WHERE rule_id IN (SELECT rule_id FROM rule where rule_create=i_hanging_import);
+		DELETE FROM rule_to 		WHERE obj_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+		DELETE FROM rule_order 		WHERE rule_id IN (SELECT rule_id FROM rule where rule_create=i_hanging_import);
+		DELETE FROM objgrp 			WHERE objgrp_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+		DELETE FROM objgrp 			WHERE objgrp_member_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+		DELETE FROM objgrp_flat 	WHERE objgrp_flat_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+		DELETE FROM objgrp_flat 	WHERE objgrp_flat_member_id IN (SELECT obj_id FROM object where obj_create=i_hanging_import);
+
+		DELETE FROM objgrp 			WHERE import_created=i_hanging_import;
+		DELETE FROM objgrp_flat 	WHERE import_created=i_hanging_import;
+		DELETE FROM svcgrp 			WHERE import_created=i_hanging_import;
+		DELETE FROM svcgrp_flat 	WHERE import_created=i_hanging_import;
+		DELETE FROM usergrp 		WHERE import_created=i_hanging_import;
+		DELETE FROM usergrp_flat 	WHERE import_created=i_hanging_import;
+
+		-- deleting all records created during hanging import
+		DELETE FROM service				WHERE svc_create=i_hanging_import;
+		DELETE FROM object				WHERE obj_create=i_hanging_import;
+		DELETE FROM usr					WHERE user_create=i_hanging_import;
+		DELETE FROM rule				WHERE rule_create=i_hanging_import;
+		DELETE FROM rule_order			WHERE control_id=i_hanging_import;
+		DELETE FROM import_control		WHERE control_id = i_hanging_import; 	-- final deletion of import_control entry
 	ELSE  
 		RAISE NOTICE 'No running import found for management id %', i_mgm_id;
 	END IF;

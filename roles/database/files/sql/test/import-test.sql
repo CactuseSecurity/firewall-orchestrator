d4f9abbdeb08d6641a65738149120605f7f9b556
Tim Purschke <tmp@cactus.de>
2021-07-30 19:53:59 +0200

    .

--- roles/database/files/sql/test/import-test.sql
+++ roles/database/files/sql/test/import-test.sql
@@ -2,12 +2,13 @@ CREATE EXTENSION IF NOT EXISTS pgtap;
 
 BEGIN;
 
-CREATE OR REPLACE FUNCTION hdb_catalog.testschema()
-RETURNS SETOF TEXT LANGUAGE plpgsql AS $$
-BEGIN
-    RETURN NEXT has_table( 'hdb_action_log' );
-    RETURN NEXT has_table( 'hdb_metadata' );
-    RETURN NEXT has_table( 'hdb_version' );
-END;
-$$;
-SELECT * FROM runtests('hdb_catalog'::name);
+-- cannot test hasura before API was installed
+-- CREATE OR REPLACE FUNCTION hdb_catalog.testschema()
+-- RETURNS SETOF TEXT LANGUAGE plpgsql AS $$
+-- BEGIN
+--     RETURN NEXT has_table( 'hdb_action_log' );
+--     RETURN NEXT has_table( 'hdb_metadata' );
+--     RETURN NEXT has_table( 'hdb_version' );
+-- END;
+-- $$;
+-- SELECT * FROM runtests('hdb_catalog'::name);

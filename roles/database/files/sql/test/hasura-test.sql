8fae3885d7fc933fd188182daab087dbf3e38698
Tim Purschke <tmp@cactus.de>
2021-08-04 11:42:06 +0200

    adding working hasura unit test

--- roles/database/files/sql/test/hasura-test.sql
+++ roles/database/files/sql/test/hasura-test.sql
@@ -5,18 +5,26 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS pgtap;
 
-CREATE OR REPLACE FUNCTION hdb_catalog.test_1_schema()
+-- CREATE OR REPLACE FUNCTION hdb_catalog.test_1_hdb_catalog_schema()
+-- RETURNS SETOF TEXT LANGUAGE plpgsql AS $$
+-- BEGIN
+--     RETURN NEXT has_table( 'hdb_catalog.hdb_action_log' );
+--     RETURN NEXT has_table( 'hdb_catalog.hdb_metadata' );
+--     RETURN NEXT has_table( 'hdb_catalog.hdb_version' );
+-- END;
+-- $$;
+
+CREATE OR REPLACE FUNCTION hdb_catalog.test_2_hdb_catalog_data()
 RETURNS SETOF TEXT LANGUAGE plpgsql AS $$
 BEGIN
-    RETURN NEXT has_table( 'hdb_action_log' );
-    RETURN NEXT has_table( 'hdb_metadata' );
-    RETURN NEXT has_table( 'hdb_version' );
+    RETURN NEXT results_eq('SELECT cast((select COUNT(*) FROM hdb_catalog.hdb_metadata) as integer)', 'SELECT cast (1 as integer)', 'there should be exactly one metadata entry');
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION public.shutdown_1() RETURNS VOID LANGUAGE plpgsql AS $$
+CREATE OR REPLACE FUNCTION hdb_catalog.shutdown_1() RETURNS VOID LANGUAGE plpgsql AS $$
 BEGIN
-    drop function if exists test_1_schema();
+    drop function if exists hdb_catalog.test_1_hdb_catalog_schema();
+    drop function if exists hdb_catalog.test_2_hdb_catalog_data();
 END;
 $$;
 

fb5c01cce1960836c40fc4a4e3bdac3806ab32ee
Tim Purschke <tmp@cactus.de>
2021-10-27 11:36:10 +0200

    more api doc fixing

--- roles/middleware/files/FWO.Middleware.Server/Startup.cs
+++ roles/middleware/files/FWO.Middleware.Server/Startup.cs
@@ -114,7 +114,7 @@ namespace FWO.Middleware
 
             app.UseSwagger();
             // app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "FWO.Middleware v1"); });
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/umgmt/v1/swagger.json", "FWO.Middleware v1"); });
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "FWO.Middleware v1"); });
 
             //app.UseHttpsRedirection();
 

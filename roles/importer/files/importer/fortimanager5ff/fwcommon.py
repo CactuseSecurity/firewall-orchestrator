607c30cd4b74f606d2c359ba7e8945224c6a5bd6
abarz722 <achim.barz@gmx.de>
2021-11-17 12:23:59 +0100

    fix merge error

--- roles/importer/files/importer/fortimanager5ff/fwcommon.py
+++ roles/importer/files/importer/fortimanager5ff/fwcommon.py
@@ -68,7 +68,6 @@ def get_config(config2import, current_import_id, base_dir, mgm_details, secret_f
             fmgr_service.normalize_svcobjects(raw_config, config2import, current_import_id)
             fmgr_rule.normalize_access_rules(raw_config, config2import, current_import_id)
             fmgr_rule.normalize_nat_rules(raw_config, config2import, current_import_id)
-            fmgr_service.normalize_svcobjects(raw_config, config2import, current_import_id)
 
     getter.logout(fm_api_url, sid, ssl_verification='',proxy_string='', debug=debug_level)
     if (debug_level>=2):

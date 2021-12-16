3bad45f21cd0cad535a4ed9e283ff5ecc56baf8e
Tim Purschke <tmp@cactus.de>
2021-12-16 11:46:08 +0100

    docs

--- roles/importer/files/importer/import-mgm.py
+++ roles/importer/files/importer/import-mgm.py
@@ -26,7 +26,8 @@ parser.add_argument('-c', '--clear', metavar='clear_management',
 parser.add_argument('-f', '--force', action='store_true', default=False,
                     help='If set the import will be attempted without checking for changes before')
 parser.add_argument('-d', '--debug', metavar='debug_level', default='0',
-                    help='Debug Level: 0=off, 1=send debug to console, 2=send debug to file, 3=save noramlized config file; 4=additionally save native config file; default=0')
+                    help='Debug Level: 0=off, 1=send debug to console, 2=send debug to file, 3=save noramlized config file; 4=additionally save native config file; default=0. \n' +\
+                        'config files are saved to $FWORCH/tmp/import dir')
 parser.add_argument('-x', '--proxy', metavar='proxy_string',
                     help='proxy server string to use, e.g. http://1.2.3.4:8080')
 parser.add_argument('-s', '--ssl', metavar='ssl_verification_mode', default='',

34f3efa1ca74517187ccd260de1c9b7c8c678e76
Tim Purschke <tmp@cactus.de>
2021-12-23 21:17:16 +0100

    cont.

--- roles/api/files/scripts/fwo-export-itsecorg-devices.py
+++ roles/api/files/scripts/fwo-export-itsecorg-devices.py
@@ -4,39 +4,41 @@
 create csv from itsecorg postgresql (input files for this script):
 
 create .pgpass with content (chmod 600)
-hostname:port:database:username:password
-localhost:5432:isodb:dbadmin:xxx
+    hostname:port:database:username:password
+    localhost:5432:isodb:dbadmin:xxx
 
 create exp-mgm.sql:
-
-COPY (
-    SELECT mgm_id, mgm_name, ssh_hostname, ssh_port, ssh_private_key, ssh_public_key,
-        ssh_user, dev_typ_id, config_path, do_not_import, force_initial_import, hide_in_gui,
-        importer_hostname, mgm_comment, debug_level, mgm_create, mgm_update, last_import_md5_complete_config
-    FROM management
-    WHERE NOT do_not_import AND NOT hide_in_gui AND mgm_name NOT ILIKE '%zzz___%'
-) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
+    COPY (
+        SELECT mgm_id, mgm_name, ssh_hostname, ssh_port, ssh_private_key, ssh_public_key,
+            ssh_user, dev_typ_id, config_path, do_not_import, force_initial_import, hide_in_gui,
+            importer_hostname, mgm_comment, debug_level, mgm_create, mgm_update, last_import_md5_complete_config
+        FROM management
+        WHERE NOT do_not_import AND NOT hide_in_gui AND mgm_name NOT ILIKE '%zzz___%'
+    ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
 create exp-dev.sql for FWO:
-
-COPY (
-    SELECT dev_id, dev_name, dev_typ_id, mgm_id, local_rulebase_name, global_rulebase_name, package_name, 
-        dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
-    FROM device
-    WHERE NOT do_not_import AND NOT hide_in_gui
-) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
+    COPY (
+        SELECT dev_id, dev_name, dev_typ_id, mgm_id, local_rulebase_name, global_rulebase_name, package_name, 
+            dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
+        FROM device
+        WHERE NOT do_not_import AND NOT hide_in_gui
+    ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
 create exp-dev.sql for itsecorg:
+    COPY (
+        SELECT dev_id, dev_name, dev_typ_id, mgm_id, dev_rulebase,
+            dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
+        FROM device
+        WHERE NOT do_not_import AND NOT hide_in_gui
+    ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
-COPY (
-    SELECT dev_id, dev_name, dev_typ_id, mgm_id, rulebase_name, global_rulebase_name,
-        dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
-    FROM device
-    WHERE NOT do_not_import AND NOT hide_in_gui
-) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
+run csv export for fworch:
+    psql -U dbadmin -h localhost -d fworchdb -c "\i exp-mgm.sql" >mgm.csv
+    psql -U dbadmin -h localhost -d fworchdb -c "\i exp-dev.sql" >dev.csv
 
-psql -U dbadmin -h localhost -d isodb -c "\i exp-mgm.sql" >mgm.sql
-psql -U dbadmin -h localhost -d isodb -c "\i exp-dev.sql" >dev.sql
+run csv export for itsecorg:
+    psql -U dbadmin -h localhost -d isodb -c "\i exp-mgm.sql" >mgm.csv
+    psql -U dbadmin -h localhost -d isodb -c "\i exp-dev.sql" >dev.csv
 
 """
 
@@ -46,12 +48,12 @@ base_dir = "/usr/local/fworch"
 importer_base_dir = base_dir + '/importer'
 sys.path.append(importer_base_dir)
 
-
 parser = argparse.ArgumentParser(
-    description="Export fworch configuration into encrypted json file\nsample; synopsis for ex- and import: fwo-export-config.py -o/tmp/fworch-config.graphql; <move to freshly installed FWO system without demo data>; fwo-execute-graphql.py -i/tmp/fworch-config.graphql")
+    description="convert database csv export of device data into graphql code for import into fworch\nsample; synopsis: fwo-convert-csv-dev-data.py -m mgm.csv -d dev.csv -o /tmp/fworch-dev-data.graphql; <can be imported into FWORCH>")
 parser.add_argument('-m', '--mgm_file', metavar='management_csv_file', required=True, help='filename to read management csv from')
 parser.add_argument('-d', '--dev_file', metavar='device_csv_file', required=True, help='filename to read device csv from')
 parser.add_argument('-o', '--outfile', metavar='output_file', required=True, help='filename to write output in json format to')
+parser.add_argument('-s', '--source', metavar='source_format', default='itsecorg', help='reading data from "itsecorg" (default) or "fworch" source')
 
 
 def convert_csv2graphql(csv_in, keys, types):
@@ -101,10 +103,17 @@ mgm_keys = ('mgm_id', 'mgm_name', 'ssh_hostname', 'ssh_port', 'ssh_private_key',
 mgm_types = ('int', 'string', 'string', 'int', 'string', 'string', 'string', 
     'int', 'string', 'bool', 'bool', 'bool', 'string', 'string',
     'int', 'string', 'string', 'string' )
-dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'local_rulebase_name', 'global_rulebase_name', 'package_name',
-    'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
-dev_types = ('int', 'string', 'int', 'int', 'string', 'string', 'string',
-    'string', 'bool', 'bool', 'bool', 'string', 'string')
+
+if args.source == 'fworch':
+    dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'local_rulebase_name', 'global_rulebase_name', 'package_name',
+        'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
+    dev_types = ('int', 'string', 'int', 'int', 'string', 'string', 'string',
+        'string', 'bool', 'bool', 'bool', 'string', 'string')
+elif args.source == 'itsecorg':
+    dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'dev_rulebase',
+        'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
+    dev_types = ('int', 'string', 'int', 'int', 'string',
+        'string', 'bool', 'bool', 'bool', 'string', 'string')
 
 with open(args.mgm_file) as mgmDataFile:
     mgmCSV = csv.reader(mgmDataFile)

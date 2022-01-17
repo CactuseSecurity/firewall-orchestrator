7343eb73c880d0405d07a91b2f9fbc199bfcf641
Tim Purschke <tmp@cactus.de>
2022-01-17 07:24:04 +0100

    Develop Stabilizing role recovery, fixing issue with api service, clean-up importer (#1451)
    * fixing legacy import
    
    * fix fortiadom only py per default
    
    * removing multi domain manager from import status
    
    * fix ipaddress bug (all nulled)
    
    * reverting essential api directory
    
    * changing api svc working dir
    
    * fix
    
    * remove api handler
    
    * make backup recovery more robust
    ignorning missing dir
    
    * fix2
    
    * fix3
    
    * reverting handlers
    
    * cosmetics
    
    * start 5.6.2
    cleaning up legacy vs. api importer
    
    * renaming legacy importer
    
    * fixing upgrade scheme
    upgrades will now also be run if the version does not change
    allows for multiple runs of (fixing) upgrades
    prerequisite: upgrade scripts need to be idempotent!
    
    * also removing old svc file
    
    * minor fixes
    
    * fix svc removal
    
    * fix2
    
    * fix3

---
+++ roles/database/files/upgrade/5.6.2.sql
@@ -0,0 +1,1 @@
+insert into stm_dev_typ (dev_typ_id,dev_typ_name,dev_typ_version,dev_typ_manufacturer,dev_typ_predef_svc) VALUES (4,'FortiGateStandalone','5ff','Fortinet','') ON Conflict Do Nothing;

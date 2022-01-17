be124ad1bcf276922d180ff5e482629982bcb161
Yannik Hayn <yannik@hayn.de>
2022-01-17 13:46:10 +0100

    wip(report-ui): better scroll into view for rsb objects

---
+++ roles/ui/files/FWO.UI/wwwroot/js/scrollIntoView.js
@@ -0,0 +1,5 @@
+﻿
+function scrollIntoRSBView(htmlObjId) {
+  let obj = document.getElementById(htmlObjId);
+  document.getElementById("rsb").scrollTop = obj.offsetTop - window.innerHeight / 2 + obj.getBoundingClientRect().height / 2;
+}

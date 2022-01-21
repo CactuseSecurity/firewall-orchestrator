ea1f97c9df392cb8b869affbb8ef74ce7df734b3
Tim Purschke <tmp@cactus.de>
2022-01-21 12:28:50 +0100

    make importer-api shutdown gracefully

--- roles/importer/files/importer/common.py
+++ roles/importer/files/importer/common.py
@@ -1,12 +1,172 @@
 import logging
+import sys, os, time, datetime
+import json, requests, requests.packages, argparse
+import importlib, logging, socket
+from pathlib import Path
+base_dir = "/usr/local/fworch"
+importer_base_dir = base_dir + '/importer'
+sys.path.append(importer_base_dir)
+import common, fwo_api
 
+full_config_size_limit = 5000000 # native configs greater than 5 MB will not stored in DB
+config2import_size_limit = 10000000 # native configs greater than 10 MB will be delted from import_config table after import
 csv_delimiter = '%'
 list_delimiter = '|'
 line_delimiter = "\n"
 apostrophe = "\""
 section_header_uids=[]
 nat_postfix = '_NatNwObj'
-# nat_postfix = '_nat'
+
+
+def import_management(mgm_id=None, ssl='off', debug_level=0, proxy='', in_file=None, limit=150, force=False):
+    error_count = 0
+    change_count = 0
+    importer_user_name = 'importer'  # todo: move to config file?
+    fwo_config_filename = base_dir + '/etc/fworch.json'
+    importer_pwd_file = base_dir + '/etc/secrets/importer_pwd'
+    import_tmp_path = base_dir + '/tmp/import'
+    change_count = 0
+    error_string = ''
+    start_time = int(time.time())
+
+    if ssl == '' or ssl == 'off':
+        requests.packages.urllib3.disable_warnings()  # suppress ssl warnings only
+    common.set_log_level(log_level=debug_level, debug_level=debug_level)
+    # read fwo config (API URLs)
+    with open(fwo_config_filename, "r") as fwo_config:
+        fwo_config = json.loads(fwo_config.read())
+    user_management_api_base_url = fwo_config['middleware_uri']
+    fwo_api_base_url = fwo_config['api_uri']
+
+    # authenticate to get JWT
+    with open(importer_pwd_file, 'r') as file:
+        importer_pwd = file.read().replace('\n', '')
+    if proxy is not None:
+        proxy = { "http_proxy": proxy, "https_proxy": proxy }
+    else:
+        proxy = None
+
+    jwt = fwo_api.login(importer_user_name, importer_pwd, user_management_api_base_url,
+                            ssl_verification=ssl, proxy=proxy)
+
+    # get mgm_details (fw-type, port, ip, user credentials):
+    mgm_details = fwo_api.get_mgm_details(fwo_api_base_url, jwt, {"mgmId": int(mgm_id)})
+
+    package_list = []
+    for dev in mgm_details['devices']:
+        package_list.append(dev['package_name'])
+
+    # only run if this is the correct import module
+    if mgm_details['importerHostname'] != socket.gethostname():
+        logging.debug(
+            "we are not responsible for importing this management - so resting")
+        sys.exit(0)
+
+    # set import lock
+    current_import_id = fwo_api.lock_import(
+        fwo_api_base_url, jwt, {"mgmId": int(mgm_id)})
+    if current_import_id == -1:
+        logging.error("error while setting import lock for management id " +
+                    str(mgm_id) + ", import already running?")
+        return 1 # sys.exit(1)
+
+    logging.info("start import of management " + str(mgm_id) +
+                ", import_id=" + str(current_import_id))
+
+    Path(import_tmp_path).mkdir(parents=True,
+                                exist_ok=True)  # make sure tmp path exists
+
+    full_config_json = {}
+    config2import = {}
+    rulebase_string = ''
+    for device in mgm_details['devices']:
+        rulebase_string += device['local_rulebase_name'] + ','
+    rulebase_string = rulebase_string[:-1]  # remove final comma
+
+    if in_file is not None:    # read native config from file
+        with open(in_file, 'r') as json_file:
+            full_config_json = json.load(json_file)
+
+    secret_filename = base_dir + '/tmp/import/mgm_id_' + \
+        str(mgm_id) + '_secret.txt'
+    with open(secret_filename, "w") as secret:  # write pwd to disk to avoid passing it as parameter
+        secret.write(mgm_details['secret'])
+
+    # pick product-specific importer:
+    fw_module_name = mgm_details['deviceType']['name'].lower().replace(
+        ' ', '') + mgm_details['deviceType']['version']+'.fwcommon'
+    fw_module = importlib.import_module(fw_module_name)
+
+    # get config from FW API and write config to json file "config_filename"
+    get_config_response = fw_module.get_config(
+        config2import, full_config_json,  current_import_id, mgm_details, debug_level, 
+            ssl_verification=ssl, proxy=proxy, limit=limit, force=force)
+
+    logging.debug("import_mgm.py: get_config completed, now writing debug config json files")
+
+    if debug_level>2:   # debugging: writing config to json file
+        normalized_config_filename = import_tmp_path + '/mgm_id_' + \
+            str(mgm_id) + '_config_normalized.json'
+        with open(normalized_config_filename, "w") as json_data:
+            json_data.write(json.dumps(config2import, indent=2))
+
+        if debug_level>3:
+            full_native_config_filename = import_tmp_path + '/mgm_id_' + \
+                str(mgm_id) + '_config_native.json'
+            with open(full_native_config_filename, "w") as json_data:  # create empty config file
+                json_data.write(json.dumps(full_config_json, indent=2))
+
+    # if no changes were found, we get get_config_response==512 and we skip everything else without errors
+    # todo: re-structure this to make it more logical/readable
+
+    if get_config_response == 1:
+        error_count += get_config_response
+    elif get_config_response == 0:
+        # now we import the config via API:
+        error_count += fwo_api.import_json_config(fwo_api_base_url, jwt, mgm_id, {
+            "importId": current_import_id, "mgmId": mgm_id, "config": config2import})
+
+        # checking for errors during imort
+        error_string_from_imp_control = fwo_api.get_error_string_from_imp_control(
+            fwo_api_base_url, jwt, {"importId": current_import_id})
+
+        if error_string_from_imp_control != None and error_string_from_imp_control != [{'import_errors': None}]:
+            error_count += 1
+            error_string += str(error_string_from_imp_control)
+        # todo: if no objects found at all: at least throw a warning
+
+        change_count = fwo_api.count_changes_per_import(fwo_api_base_url, jwt, current_import_id)
+        full_config_size = sys.getsizeof(json.dumps(full_config_json))
+        config2import_size = sys.getsizeof(json.dumps(config2import))
+
+        logging.debug("full_config size: " + str(full_config_size) + " bytes, config2import size: " + str(config2import_size) + " bytes")
+
+        if (change_count > 0 or error_count > 0) and full_config_size < full_config_size_limit:  # store full config in case of change or error
+            error_count += fwo_api.store_full_json_config(fwo_api_base_url, jwt, args.mgm_id, {
+                "importId": current_import_id, "mgmId": args.mgm_id, "config": full_config_json})
+
+    # CLEANUP: delete configs of imports without changes (if no error occured)
+    if change_count == 0 and error_count == 0 and get_config_response < 2:
+        error_count += fwo_api.delete_json_config(fwo_api_base_url, jwt, {"importId": current_import_id})
+        error_count += fwo_api.delete_import(fwo_api_base_url, jwt, current_import_id)
+    if change_count != 0 and config2import_size > config2import_size_limit:
+        error_count += fwo_api.delete_json_config(fwo_api_base_url, jwt, {"importId": current_import_id})
+    if os.path.exists(secret_filename):
+        os.remove(secret_filename)
+
+    # finalize import by unlocking it
+    error_count += fwo_api.unlock_import(fwo_api_base_url, jwt, int(
+        mgm_id), datetime.datetime.now().isoformat(), current_import_id, error_count, change_count)
+
+    logging.info("import_mgm.py: import no. " + str(current_import_id) +
+        " for management " + mgm_details['name'] + ' (id=' + str(mgm_id) + ")" +
+        str(" threw errors," if error_count else " successful,") + 
+        " change_count: " + str(change_count) +
+        ", duration: " + str(int(time.time()) - start_time) + "s")
+    if len(error_string) > 0:
+        print("ERRORS: " + error_string)
+
+    return error_count
 
 
 def set_log_level(log_level, debug_level):
--- roles/importer/files/importer/import-main-loop.py
+++ roles/importer/files/importer/import-main-loop.py
@@ -3,68 +3,94 @@
 #   run import loop every x seconds (adjust sleep time per management depending on the change frequency )
 #      import_mgm.py
 
-import os, argparse, sys
-import time, json, logging, requests
-base_dir = "/usr/local/fworch"
-importer_base_dir = base_dir + '/importer'
-sys.path.append(importer_base_dir)
-import fwo_api, common
+import signal
+import argparse
+import sys
+import time
+import json
+import logging
+import requests
+import common, fwo_api
 
-parser = argparse.ArgumentParser(
-    description='Run import loop across all managements to read configuration from FW managements via API calls')
-parser.add_argument('-i', '--interval', metavar='interval_time',
-                    default=90, help='time in seconds to sleep between import loops')
-parser.add_argument('-d', '--debug', metavar='debug_level', default='0',
-                    help='Debug Level: 0=off, 1=send debug to console, 2=send debug to file, 3=keep temporary config files; default=0')
-parser.add_argument('-x', '--proxy', metavar='proxy_string',
-                    help='proxy server string to use, e.g. http://1.2.3.4:8080')
-parser.add_argument('-s', '--ssl', metavar='ssl_verification_mode', default='',
-                    help='[ca]certfile, if value not set, ssl check is off"; default=empty/off')
-parser.add_argument('-l', '--limit', metavar='api_limit', default='150',
-                    help='The maximal number of returned results per HTTPS Connection; default=150')
+# https://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
+class GracefulKiller:
+    kill_now = False
 
-args = parser.parse_args()
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-requests.packages.urllib3.disable_warnings()  # suppress ssl warnings only
+    def exit_gracefully(self, *args):
+        self.kill_now = True
 
-importer_user_name = 'importer'  # todo: move to config file?
-fwo_config_filename = base_dir + '/etc/fworch.json'
-importer_pwd_file = base_dir + '/etc/secrets/importer_pwd'
-debug_level = int(args.debug)
-common.set_log_level(log_level=debug_level, debug_level=debug_level)
 
-# read fwo config (API URLs)
-with open(fwo_config_filename, "r") as fwo_config:
-    fwo_config = json.loads(fwo_config.read())
-user_management_api_base_url = fwo_config['middleware_uri']
-fwo_api_base_url = fwo_config['api_uri']
+if __name__ == '__main__':
+    base_dir = "/usr/local/fworch"
+    importer_base_dir = base_dir + '/importer'
+    sys.path.append(importer_base_dir)
+    parser = argparse.ArgumentParser(
+        description='Run import loop across all managements to read configuration from FW managements via API calls')
+    parser.add_argument('-i', '--interval', metavar='interval_time',
+                        default=90, help='time in seconds to sleep between import loops')
+    parser.add_argument('-d', '--debug', metavar='debug_level', default='0',
+                        help='Debug Level: 0=off, 1=send debug to console, 2=send debug to file, 3=keep temporary config files; default=0')
+    parser.add_argument('-x', '--proxy', metavar='proxy_string',
+                        help='proxy server string to use, e.g. http://1.2.3.4:8080')
+    parser.add_argument('-s', '--ssl', metavar='ssl_verification_mode', default='',
+                        help='[ca]certfile, if value not set, ssl check is off"; default=empty/off')
+    parser.add_argument('-l', '--limit', metavar='api_limit', default='150',
+                        help='The maximal number of returned results per HTTPS Connection; default=150')
 
-if 'ssl' in args and args.ssl!='':
-    ssl_string = ' -s ' + str(args.ssl) + ' '
-else:
-    ssl_string = ''
+    args = parser.parse_args()
 
-while True:
-    # authenticate to get JWT
-    with open(importer_pwd_file, 'r') as file:
-        importer_pwd = file.read().replace('\n', '')
-    jwt = fwo_api.login(importer_user_name, importer_pwd, user_management_api_base_url, ssl_verification=args.ssl, proxy=args.proxy)
-    mgm_ids = fwo_api.get_mgm_ids(fwo_api_base_url, jwt, {})
-    api_fetch_limit = fwo_api.get_config_value(fwo_api_base_url, jwt, key='fwApiElementsPerFetch')
-    if api_fetch_limit == None:
-        api_fetch_limit = '150'
-        
-    for mgm_id in mgm_ids:
-        id = str(mgm_id['id'])
-        # getting a new JWT in case the old one is not valid anymore after a long previous import
-        jwt = fwo_api.login(importer_user_name, importer_pwd, user_management_api_base_url, ssl_verification=args.ssl, proxy=args.proxy)
-        mgm_details = fwo_api.get_mgm_details(fwo_api_base_url, jwt, {"mgmId": id})
-        if mgm_details["deviceType"]["id"] in (9,11):  # only handle CPR8x and fortiManager
-            cmd = importer_base_dir + '/import-mgm.py -m ' + id + ssl_string + ' -d ' + str(debug_level) + ' -l ' + str(api_fetch_limit)
-            if 'proxy' in args and args.proxy != None:
-                cmd += ' -x ' + str(args.proxy)
-            logging.info("import_main_loop.py: starting import cmd " + cmd) 
-            os.system(cmd)
-    logging.info("import_main_loop.py: sleeping between loops for " +
-          str(args.interval) + " seconds")
-    time.sleep(int(args.interval))
+    requests.packages.urllib3.disable_warnings()  # suppress ssl warnings only
+
+    importer_user_name = 'importer'  # todo: move to config file?
+    fwo_config_filename = base_dir + '/etc/fworch.json'
+    importer_pwd_file = base_dir + '/etc/secrets/importer_pwd'
+    debug_level = int(args.debug)
+    common.set_log_level(log_level=debug_level, debug_level=debug_level)
+
+    # read fwo config (API URLs)
+    with open(fwo_config_filename, "r") as fwo_config:
+        fwo_config = json.loads(fwo_config.read())
+    user_management_api_base_url = fwo_config['middleware_uri']
+    fwo_api_base_url = fwo_config['api_uri']
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        # authenticate to get JWT
+        with open(importer_pwd_file, 'r') as file:
+            importer_pwd = file.read().replace('\n', '')
+        jwt = fwo_api.login(importer_user_name, importer_pwd,
+                            user_management_api_base_url, ssl_verification=args.ssl, proxy=args.proxy)
+
+        mgm_ids = fwo_api.get_mgm_ids(fwo_api_base_url, jwt, {})
+        api_fetch_limit = fwo_api.get_config_value(
+            fwo_api_base_url, jwt, key='fwApiElementsPerFetch')
+        if api_fetch_limit == None:
+            api_fetch_limit = '150'
+
+        for mgm_id in mgm_ids:
+            if killer.kill_now:
+                break
+            id = str(mgm_id['id'])
+            logging.info("import_main_loop.py: starting import of mgm_id=" + id)
+
+            # getting a new JWT in case the old one is not valid anymore after a long previous import
+            jwt = fwo_api.login(importer_user_name, importer_pwd,
+                                user_management_api_base_url, ssl_verification=args.ssl, proxy=args.proxy)
+            mgm_details = fwo_api.get_mgm_details(
+                fwo_api_base_url, jwt, {"mgmId": id})
+            if mgm_details["deviceType"]["id"] in (9, 11):  # only handle CPR8x and fortiManager
+                try:
+                    common.import_management(mgm_id=id, ssl=args.ssl, debug_level=debug_level, limit=api_fetch_limit)
+                except Exception:
+                    print("caught error")
+        logging.info("import_main_loop.py: sleeping between loops for " +
+                     str(args.interval) + " seconds")
+        counter=0
+        while counter < int(args.interval) and not killer.kill_now:
+            time.sleep(1)
+            counter += 1
+
+    print("importer-api was killed gracefully.")

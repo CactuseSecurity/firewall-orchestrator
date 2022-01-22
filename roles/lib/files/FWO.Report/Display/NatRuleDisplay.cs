a8eff98408b88e80b52c1f776cbb3188484d1701
Yannik Hayn <yannik@hayn.de>
2022-01-23 00:50:57 +0100

    feat(ui rsb): add translated objects to nat rule details
    also kinda fixes linking in /certification -> will work if we decide to add an report tab to rsb there

--- roles/lib/files/FWO.Report/Display/NatRuleDisplay.cs
+++ roles/lib/files/FWO.Report/Display/NatRuleDisplay.cs
@@ -9,7 +9,7 @@ namespace FWO.Ui.Display
         public NatRuleDisplay(UserConfig userConfig) : base(userConfig)
         {}
 
-        public string DisplayTranslatedSource(Rule rule, string style = "")
+        public string DisplayTranslatedSource(Rule rule, string style = "", bool recert = false)
         {
             result = new StringBuilder();
 
@@ -30,6 +30,8 @@ namespace FWO.Ui.Display
                 else
                     symbol = "oi oi-monitor";
 
+                string page = recert ? "certification" : "report";
+
                 if (source.User != null)
                     result.AppendLine($"<span class=\"oi oi-people\">&nbsp;</span><a href=\"report#report-m{rule.MgmtId}-user{source.User.Id}\" target=\"_top\" style=\"{style}\">{source.User.Name}</a>@");
                 result.Append($"<span class=\"{symbol}\">&nbsp;</span><a href=\"report#report-m{rule.MgmtId}-nwobj{source.Object.Id}\" target=\"_top\" style=\"{style}\">{source.Object.Name}</a>");
@@ -47,7 +49,7 @@ namespace FWO.Ui.Display
             return result.ToString();
         }
 
-        public string DisplayTranslatedDestination(Rule rule, string style = "")
+        public string DisplayTranslatedDestination(Rule rule, string style = "", bool recert = false)
         {
             result = new StringBuilder();
 
@@ -68,6 +70,8 @@ namespace FWO.Ui.Display
                 else
                     symbol = "oi oi-monitor";
 
+                string page = recert ? "certification" : "report";
+
                 result.Append($"<span class=\"{symbol}\">&nbsp;</span><a href=\"report#report-m{rule.MgmtId}-nwobj{destination.Object.Id}\" target=\"_top\" style=\"{style}\">{destination.Object.Name}</a>");
                 result.Append(destination.Object.IP != null ? $" ({destination.Object.IP})" : "");
                 result.AppendLine("<br>");
@@ -83,7 +87,7 @@ namespace FWO.Ui.Display
             return result.ToString();
         }
 
-        public string DisplayTranslatedService(Rule rule, string style = "")
+        public string DisplayTranslatedService(Rule rule, string style = "", bool recert = false)
         {
             result = new StringBuilder();
 
@@ -101,6 +105,8 @@ namespace FWO.Ui.Display
                     symbol = "oi oi-list-rich";
                 else
                     symbol = "oi oi-wrench";
+
+                string page = recert ? "certification" : "report";
 
                 result.Append($"<span class=\"{symbol}\">&nbsp;</span><a href=\"report#report-m{rule.MgmtId}-svc{service.Content.Id}\" target=\"_top\" style=\"{style}\">{service.Content.Name}</a>");
 

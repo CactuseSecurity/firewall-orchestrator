624bd7b17d9156c18e0525c247f3f34dd3d80a27
Tim Purschke <tmp@cactus.de>
2022-01-24 17:51:35 +0100

    fix query

--- roles/api/files/hdb_catalog-grants.sql
+++
@@ -1,4 +0,0 @@
-
-GRANT USAGE ON SCHEMA hdb_catalog TO dbbackupusers;
-Grant select on ALL TABLES in SCHEMA hdb_catalog to group dbbackupusers;
-ALTER DEFAULT PRIVILEGES IN SCHEMA hdb_catalog GRANT SELECT ON TABLES TO group dbbackupusers;

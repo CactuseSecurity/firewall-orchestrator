18499f25266e47b3924524e471d68b4059c33a55
Tim Purschke <tmp@cactus.de>
2022-01-24 17:36:17 +0100

    hasura schema grants fix

--- roles/database/files/sql/idempotent/fworch-grants.sql
+++ roles/database/files/sql/idempotent/fworch-grants.sql
@@ -1,14 +1,9 @@
 
 -- settings backup permissions
-
-GRANT USAGE ON SCHEMA hdb_catalog TO dbbackupusers;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO group "dbbackupusers";
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO group "dbbackupusers";
 Grant select on ALL TABLES in SCHEMA public to group dbbackupusers;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO group dbbackupusers;
-Grant select on ALL TABLES in SCHEMA hdb_catalog to group dbbackupusers;
-ALTER DEFAULT PRIVILEGES IN SCHEMA hdb_catalog GRANT SELECT ON TABLES TO group dbbackupusers;
-
 
 --  grants for all (implicit) sequences
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO group "secuadmins";

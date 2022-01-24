08185610c01a2253bda6d65567f9295fbea3f81b
Yannik Hayn <yannik@hayn.de>
2022-01-24 17:46:22 +0100

    feat(ui rsb): obj link improvements
    - fix unsmooth scrolling
    - links now fully reliable
    - add report tab in /certify -> links work there now also

--- roles/ui/files/FWO.UI/wwwroot/js/scrollIntoView.js
+++ roles/ui/files/FWO.UI/wwwroot/js/scrollIntoView.js
@@ -2,9 +2,10 @@
 function scrollIntoRSBView(htmlObjId) {
   let obj =  document.getElementById(htmlObjId)?.parentElement?.parentElement; // gets the tr element containing the obj
   if (!obj)
-    return;
+    return false;
   obj.scrollIntoView({behavior: "smooth", block: "center"});
   obj.classList.add("fade-bg");
   obj.classList.add("temp-highlight");
   setTimeout(() => obj.classList.remove("temp-highlight"), 800)
+  return obj.offsetParent !== null; // alement visible?
 }

40db3d8bf98a4c17fd8a5e4d39ea9d7e33253082
Tim Purschke <tmp@cactus.de>
2022-01-26 12:28:49 +0100

    fix migration script

--- roles/api/files/scripts/fwo-migrate-itsecorg-devices.py
+++ roles/api/files/scripts/fwo-migrate-itsecorg-devices.py
@@ -5,30 +5,28 @@
 This script exports all devices from an existing itsecorg installation.
 It allows for migrating fortigate ssh devices into fortimanager installations
 
-first add new fortimanager (via UI) and use its management id as parameter -f X
-
 create csv from itsecorg postgresql (input files for this script):
 
 create .pgpass with content (chmod 600)
-    hostname:port:database:username:password
+    # hostname:port:database:username:password
     localhost:5432:isodb:dbadmin:xxx
 
 create exp-mgm.sql:
     COPY (
         SELECT mgm_id, mgm_name, ssh_hostname, ssh_port, ssh_private_key, ssh_public_key,
             ssh_user, dev_typ_id, config_path, do_not_import, force_initial_import, hide_in_gui,
-            importer_hostname, mgm_comment, debug_level, mgm_create, mgm_update, last_import_md5_complete_config
+            importer_hostname, mgm_comment, mgm_create, mgm_update, last_import_md5_complete_config
         FROM management
         WHERE NOT do_not_import AND NOT hide_in_gui AND mgm_name NOT ILIKE '%zzz___%'
     ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
-create exp-dev.sql for FWO:
-    COPY (
-        SELECT dev_id, dev_name, dev_typ_id, mgm_id, local_rulebase_name, global_rulebase_name, package_name, 
-            dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
-        FROM device
-        WHERE NOT do_not_import AND NOT hide_in_gui
-    ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
+# create exp-dev.sql only if you want to migrate from one FWO to another one):
+#     COPY (
+#         SELECT dev_id, dev_name, dev_typ_id, mgm_id, local_rulebase_name, global_rulebase_name, package_name, 
+#             dev_comment, do_not_import, force_initial_import, hide_in_gui, dev_create, dev_update
+#         FROM device
+#         WHERE NOT do_not_import AND NOT hide_in_gui
+#     ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
 create exp-dev.sql for itsecorg:
     COPY (
@@ -38,13 +36,16 @@ create exp-dev.sql for itsecorg:
         WHERE NOT do_not_import AND NOT hide_in_gui
     ) TO STDOUT (FORMAT CSV, FORCE_QUOTE *);
 
-run csv export for fworch:
-    psql -U dbadmin -h localhost -d fworchdb -c "\i exp-mgm.sql" >mgm.csv
-    psql -U dbadmin -h localhost -d fworchdb -c "\i exp-dev.sql" >dev.csv
+# run csv export for fworch:
+#     psql -U dbadmin -h localhost -d fworchdb -c "\i exp-mgm.sql" >mgm.csv
+#     psql -U dbadmin -h localhost -d fworchdb -c "\i exp-dev.sql" >dev.csv
 
 run csv export for itsecorg:
     psql -U dbadmin -h localhost -d isodb -c "\i exp-mgm.sql" >mgm.csv
     psql -U dbadmin -h localhost -d isodb -c "\i exp-dev.sql" >dev.csv
+
+then run this script:
+    fwo-export-itsecorg-devices.py -
 
 """
 
@@ -60,7 +61,6 @@ parser.add_argument('-m', '--mgm_file', metavar='management_csv_file', required=
 parser.add_argument('-d', '--dev_file', metavar='device_csv_file', required=True, help='filename to read device csv from')
 parser.add_argument('-o', '--outfile', metavar='output_file', required=True, help='filename to write output in json format to')
 parser.add_argument('-s', '--source', metavar='source_format', default='itsecorg', help='reading data from "itsecorg" (default) or "fworch" source')
-parser.add_argument('-f', '--fortimanager', metavar='fortimanager_id', help='fortimanager management id')
 
 
 def convert_csv2graphql(csv_in, keys, types):
@@ -115,7 +115,7 @@ def migrateCsvDev(csv_in, input_type):
                         line.insert(5, dev_rulebase_name_ar[1]) # global rulebase name
                     else:
                         line.insert(5, None) # global rulebase name
-                    line.insert(6, None) # package
+                    line.insert(6, None) # package name - needs to be set manually
 
 
 args = parser.parse_args()
@@ -125,21 +125,21 @@ if len(sys.argv) == 1:
 
 mgm_keys = ('mgm_id', 'mgm_name', 'ssh_hostname', 'ssh_port', 'ssh_private_key', 'ssh_public_key', 'ssh_user', 
     'dev_typ_id', 'config_path', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'importer_hostname', 'mgm_comment', 
-    'debug_level', 'mgm_create', 'mgm_update', 'last_import_md5_complete_config')
+    'mgm_create', 'mgm_update', 'last_import_md5_complete_config')
 mgm_types = ('int', 'string', 'string', 'int', 'string', 'string', 'string', 
     'int', 'string', 'bool', 'bool', 'bool', 'string', 'string',
-    'int', 'string', 'string', 'string' )
+    'string', 'string', 'string' )
 
-if args.source == 'fworch':
-    dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'local_rulebase_name', 'global_rulebase_name', 'package_name',
-        'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
-    dev_types = ('int', 'string', 'int', 'int', 'string', 'string', 'string',
-        'string', 'bool', 'bool', 'bool', 'string', 'string')
-elif args.source == 'itsecorg':
-    dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'dev_rulebase',
-        'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
-    dev_types = ('int', 'string', 'int', 'int', 'string',
-        'string', 'bool', 'bool', 'bool', 'string', 'string')
+# if args.source == 'fworch':
+dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'local_rulebase_name', 'global_rulebase_name', 'package_name',
+    'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
+dev_types = ('int', 'string', 'int', 'int', 'string', 'string', 'string',
+    'string', 'bool', 'bool', 'bool', 'string', 'string')
+# elif args.source == 'itsecorg':
+#     dev_keys = ('dev_id', 'dev_name', 'dev_typ_id', 'mgm_id', 'dev_rulebase',
+#         'dev_comment', 'do_not_import', 'force_initial_import', 'hide_in_gui', 'dev_create', 'dev_update')
+#     dev_types = ('int', 'string', 'int', 'int', 'string',
+#         'string', 'bool', 'bool', 'bool', 'string', 'string')
 
 mgmList = list()
 with open(args.mgm_file) as mgmDataFile:

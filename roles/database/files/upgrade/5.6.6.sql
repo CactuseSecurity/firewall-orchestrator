38ca2204b451c984cc6278985b2253362581e0d7
abarz722 <achim.barz@gmx.de>
2022-02-18 11:08:57 +0100

    revert hasura metadata

--- roles/database/files/upgrade/5.6.6.sql
+++ roles/database/files/upgrade/5.6.6.sql
@@ -16,7 +16,7 @@ ALTER TABLE "log_data_issue" ADD COLUMN IF NOT EXISTS "user_id" INTEGER DEFAULT 
 ALTER TABLE "log_data_issue" DROP CONSTRAINT IF EXISTS "log_data_issue_uiuser_uiuser_id_fkey" CASCADE;
 Alter table "log_data_issue" add CONSTRAINT log_data_issue_uiuser_uiuser_id_fkey foreign key ("user_id") references "uiuser" ("uiuser_id") on update restrict on delete cascade;
 
-Create table "alert"
+Create table IF NOT EXISTS "alert"
 (
 	"alert_id" BIGSERIAL,
 	"ref_log_id" BIGINT,

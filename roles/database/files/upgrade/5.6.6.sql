b367eb5b7dc290dd355f2c3a12e5fa53b67a838e
Tim Purschke <tmp@cactus.de>
2022-02-22 07:34:02 +0100

    rule_to: rename usr.user_id fk relation

--- roles/database/files/upgrade/5.6.6.sql
+++ roles/database/files/upgrade/5.6.6.sql
@@ -6,6 +6,7 @@
 ALTER TABLE "rule_to" DROP CONSTRAINT if exists rule_to_pkey;
 ALTER table "rule_to" ADD column IF NOT EXISTS user_id BIGINT;
 ALTER table "rule_to" ADD column IF NOT EXISTS rule_to_id BIGSERIAL primary key;
+Alter table "rule_to" drop constraint if exists rule_to_user_id_fkey;
 Alter table "rule_to" drop constraint if exists rule_to_user_id_usr_user_id;
 Alter table "rule_to" add constraint rule_to_user_id_usr_user_id FOREIGN KEY 
     ("user_id") references "usr" ("user_id") on update restrict on delete cascade;

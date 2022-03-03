752f13c88c41117b14b0ab3d750251272552da3c
abarz722 <achim.barz@gmx.de>
2022-03-03 12:39:48 +0100

    in progess popup for several deletions

--- roles/database/files/sql/idempotent/fworch-texts.sql
+++ roles/database/files/sql/idempotent/fworch-texts.sql
@@ -63,8 +63,8 @@ INSERT INTO txt VALUES ('id',		            'German', 	'Id');
 INSERT INTO txt VALUES ('id',		            'English', 	'Id');
 INSERT INTO txt VALUES ('coming_soon',		    'German', 	'(demn&auml;chst)');
 INSERT INTO txt VALUES ('coming_soon',		    'English', 	'(coming soon)');
-INSERT INTO txt VALUES ('in_process',		    'German', 	'in Arbeit');
-INSERT INTO txt VALUES ('in_process',		    'English', 	'in process');
+INSERT INTO txt VALUES ('in_progress',		    'German', 	'in Arbeit');
+INSERT INTO txt VALUES ('in_progress',		    'English', 	'in progress');
 
 -- login
 INSERT INTO txt VALUES ('login', 				'German',	'Anmelden');

6b034a398edb385bf5e0c02e354652524c646d88
Tim Purschke <tmp@cactus.de>
2022-04-01 11:01:16 +0200

    removing dup filters this version works for cidr filter and rule IPs not for ranges in rules

--- roles/lib/files/FWO.Report.Filter/Ast/AstNodeFilterNetwork.cs
+++ roles/lib/files/FWO.Report.Filter/Ast/AstNodeFilterNetwork.cs
@@ -145,90 +145,35 @@ namespace FWO.Report.Filter.Ast
         {
             string filterIP = SanitizeIp(Value.Text);
             (string firstFilterIp, string lastFilterIp) = GetFirstAndLastIp(filterIP);
-            string ipFilterString;
-
-            if (firstFilterIp == lastFilterIp) // optimization, just need a single comparison if searching for single ip
-            {
-                string QueryVarName = $"{location}Ip" + query.parameterCounter++;
-                query.QueryVariables[QueryVarName] = firstFilterIp;
-                query.QueryParameters.Add($"${QueryVarName}: cidr! ");
-                // checking if single filter ip is part of a cidr subnet (or is a direct match for a single ip)
-                ipFilterString = $@" _or: [
-                            {{
-                                _and: 
-                                        [ 
-                                            {{ obj_ip: {{ _gte: ${QueryVarName} }} }}
-                                            {{ obj_ip: {{ _lte: ${QueryVarName} }} }}
-                                        ]
-                            }} 
-                            {{
-                                _and: 
-                                        [ 
-                                            {{ network_object_limits: {{ first_ip: {{ _lte: ${QueryVarName} }} }} }}
-                                            {{ network_object_limits: {{ last_ip: {{ _gte: ${QueryVarName} }} }} }}
-                                        ]
-                            }} 
-                            ]";
-            }
-            else // ip filter is a subnet with /xy
-            {
-                string QueryVarNameNet0 = $"{location}IpNet" + query.parameterCounter;
-                string QueryVarNameFirst1 = $"{location}IpLow" + query.parameterCounter;
-                string QueryVarNameLast2 = $"{location}IpHigh" + query.parameterCounter++;
-                query.QueryVariables[QueryVarNameNet0] = filterIP;
-                query.QueryVariables[QueryVarNameFirst1] = firstFilterIp;
-                query.QueryVariables[QueryVarNameLast2] = lastFilterIp;
-                query.QueryParameters.Add($"${QueryVarNameNet0}: cidr! ");
-                query.QueryParameters.Add($"${QueryVarNameFirst1}: cidr! ");
-                query.QueryParameters.Add($"${QueryVarNameLast2}: cidr! ");
-                // covering various cases: 
+            string QueryVarNameFirst1 = $"{location}IpLow" + query.parameterCounter;
+            string QueryVarNameLast2 = $"{location}IpHigh" + query.parameterCounter++;
+            query.QueryVariables[QueryVarNameFirst1] = firstFilterIp;
+            query.QueryVariables[QueryVarNameLast2] = lastFilterIp;
+            query.QueryParameters.Add($"${QueryVarNameFirst1}: cidr! ");
+            query.QueryParameters.Add($"${QueryVarNameLast2}: cidr! ");
+            // currently only working for pure CIDR objects,  covering the following cases: 
                 // 1 - current ip is fully contained in filter ip range
-                // 2 - current ip overlaps with lower boundary of filter ip range
-                // 3 - current ip overlaps with upper boundary of filter ip range
-                // 4 - current ip fully contains filter ip range - does not work
-                ipFilterString =
-                     $@" _or: [
-                            {{ obj_ip: {{ _eq: ${QueryVarNameNet0} }} }}
-                            {{ _and: 
-                                    [ 
-                                        {{ obj_ip: {{ _gte: ${QueryVarNameFirst1} }} }}
-                                        {{ obj_ip: {{ _lte: ${QueryVarNameLast2} }} }}
-                                    ]
-                            }}
-                            {{ _and: 
-                                    [ 
-                                        {{ obj_ip: {{ _lte: ${QueryVarNameFirst1} }} }}
-                                        {{ obj_ip: {{ _gte: ${QueryVarNameFirst1} }} }}
-                                    ]
-                            }} 
-                            {{ _and: 
-                                    [ 
-                                        {{ obj_ip: {{ _lte: ${QueryVarNameLast2} }} }}
-                                        {{ obj_ip: {{ _gte: ${QueryVarNameLast2} }} }}
-                                    ]
-                            }}
-                            {{ _and: 
-                                    [ 
-                                        {{ obj_ip: {{ _lte: ${QueryVarNameFirst1} }} }}
-                                        {{ obj_ip: {{ _gte: ${QueryVarNameLast2} }} }}
-                                    ]
-                            }}
-                            {{
-                                _and: 
-                                        [ 
-                                            {{ network_object_limits: {{ first_ip: {{ _lte: ${QueryVarNameFirst1} }} }} }}
-                                            {{ network_object_limits: {{ last_ip: {{ _gte: ${QueryVarNameLast2} }} }} }}
-                                        ]
-                            }} 
-                            {{
-                                _and: 
-                                        [ 
-                                            {{ network_object_limits: {{ first_ip: {{ _lte: ${QueryVarNameFirst1} }} }} }}
-                                            {{ network_object_limits: {{ last_ip: {{ _gte: ${QueryVarNameLast2} }} }} }}
-                                        ]
-                            }} 
-                        ]";
-            }
+                // 2 - current ip fully contains filter ip range - does not work
+            // not covering (relevant for ip in rule = range):
+                // 3 - current ip overlaps with lower boundary of filter ip range
+                // 4 - current ip overlaps with upper boundary of filter ip range
+            // might simply set all header IP addresses to 0.0.0.0/32 instead of 0.0.0.0/0 to filter them out
+            string ipFilterString =
+                    $@" _or: [
+                        {{ _and:
+                            [
+                                {{ obj_ip: {{ _lte: ${QueryVarNameFirst1} }} }}
+                                {{ obj_ip: {{ _gte: ${QueryVarNameLast2} }} }}
+                            ]
+                        }}
+                        {{
+                            _and:
+                            [
+                                {{ network_object_limits: {{ first_ip: {{ _lte: ${QueryVarNameFirst1} }} }} }}
+                                {{ network_object_limits: {{ last_ip: {{ _gte: ${QueryVarNameLast2} }} }} }}
+                            ]
+                        }}
+                    ]";
             query.ruleWhereStatement +=
                 $@" {locationTable}: 
                         {{ object: 

e70273ba86dd6ae5e07ffaf83d71c97bd6af21f4
Tim Purschke <tmp@cactus.de>
2022-04-22 13:41:13 +0200

    switch to sudo instal witout pipelining needed for ansible >=2.10 (debian11, ubuntu 22.04)

--- scripts/devsrv-manual-install.sh
+++ scripts/devsrv-manual-install.sh
@@ -2,5 +2,5 @@ cd
 rm -rf firewall-orchestrator
 ssh-agent bash -c 'ssh-add .ssh/id_github_deploy && git clone git@github.com:CactuseSecurity/firewall-orchestrator.git'
 cd firewall-orchestrator
-ansible-playbook site.yml -e "installation_mode=uninstall"
-ansible-playbook site.yml -e "installation_mode=new"
+sudo ansible-playbook site.yml -e "installation_mode=uninstall"
+sudo ansible-playbook site.yml -e "installation_mode=new"

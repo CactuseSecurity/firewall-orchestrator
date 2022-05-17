3f2fa95cb7ab92fcc49ddfddda9a1cd8f5caecc3
Nils <48684538+NilsPur@users.noreply.github.com>
2022-05-17 21:49:06 +0200

    permisson service init

---
+++ roles/ui/files/FWO.UI/Services/PermissionEventService.cs
@@ -0,0 +1,19 @@
+﻿using FWO.Api.Data;
+
+namespace FWO.Ui.Services
+{
+    public class PermissionEventService
+    {
+        public event EventHandler<UiUser>? OnPermissionChanged;
+
+        public PermissionEventService()
+        {
+
+        }
+
+        public void PermissionsChanged(UiUser user)
+        {
+            OnPermissionChanged?.Invoke(this, user);
+        }
+    }
+}

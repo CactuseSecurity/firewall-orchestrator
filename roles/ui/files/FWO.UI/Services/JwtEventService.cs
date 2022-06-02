2473dcca3ef229bb70a64fff5fba9113f7f0ccbe
Nils <48684538+NilsPur@users.noreply.github.com>
2022-06-02 19:34:59 +0200

    Relogin without state reset
    jwt expiry / role changes can be handled via relogin which doesnt lead to state reset

--- roles/ui/files/FWO.UI/Services/JwtEventService.cs
+++ roles/ui/files/FWO.UI/Services/JwtEventService.cs
@@ -6,9 +6,23 @@ namespace FWO.Ui.Services
     {
         public static event EventHandler<string>? OnPermissionChanged;
 
+        public static event EventHandler<string>? OnJwtAboutToExpire;
+
+        private static readonly Dictionary<string, Timer> jwtExpiryTimers = new Dictionary<string, Timer>();
+
         public static void PermissionsChanged(string userDn)
         {
             OnPermissionChanged?.Invoke(null, userDn);
+        }
+
+        public static void JwtAboutToExpire(string userDn)
+        {
+            OnJwtAboutToExpire?.Invoke(null, userDn);
+        }
+
+        public static void AddJwtTimer(string userDn, int time)
+        {
+            jwtExpiryTimers[userDn] = new Timer(_ => JwtAboutToExpire(userDn), null, time, time);
         }
     }
 }

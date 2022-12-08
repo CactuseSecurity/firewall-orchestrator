59533cb5de122d5b6373d027742a5dc420894af8
Tim Purschke <tmp@cactus.de>
2022-12-08 16:06:40 +0100

    first recert owner filter

--- roles/lib/files/FWO.Api.Client/Data/RecertFilter.cs
+++ roles/lib/files/FWO.Api.Client/Data/RecertFilter.cs
@@ -13,12 +13,15 @@ namespace FWO.Api.Data
         public bool RecertShowAnyMatch = false;
         public bool RecertSingleLinePerRule = false;
 
-        public RecertFilter(bool recertOverdueOnly = false, bool recertWithoutOwner = false, bool recertShowAnyMatch = false, bool recertSingleLinePerRule = false)
+        public FwoOwner RecertOwner = null;
+
+        public RecertFilter(bool recertOverdueOnly = false, bool recertWithoutOwner = false, bool recertShowAnyMatch = false, bool recertSingleLinePerRule = false, FwoOwner owner = null)
         {
             RecertOverdueOnly = recertOverdueOnly;
             RecertWithoutOwner = recertWithoutOwner;
             RecertShowAnyMatch = recertShowAnyMatch;
             RecertSingleLinePerRule = recertSingleLinePerRule;
+            RecertOwner = owner;
         }
     }
 }

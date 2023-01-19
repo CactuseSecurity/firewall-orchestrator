07249ed1447e988222274833f9f13cff35caf39f
Tim Purschke <tmp@cactus.de>
2023-01-19 18:28:28 +0100

    adding migration of recert data

--- roles/database/files/upgrade/6.1.3.sql
+++ roles/database/files/upgrade/6.1.3.sql
@@ -9,3 +9,19 @@ ALTER TABLE recertification ADD COLUMN IF NOT EXISTS next_recert_date Timestamp;
 --     recertified
 --   }
 --}
+
+CREATE OR REPLACE FUNCTION refresh_recert_entries () RETURNS VOID AS $$
+DECLARE
+    r_mgm RECORD;
+BEGIN
+    FOR r_mgm IN SELECT mgm_id FROM management WHERE NOT do_not_import
+    LOOP
+        PERFORM recert_refresh_per_management(r_mgm.mgm_id);
+    END LOOP;
+    RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT * FROM refresh_recert_entries ();
+DROP FUNCTION refresh_recert_entries();

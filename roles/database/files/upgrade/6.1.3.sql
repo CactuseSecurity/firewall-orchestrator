267282d557c8c72ef0cfd920e92e6f6c9230b95f
Tim Purschke <tmp@cactus.de>
2023-01-20 21:05:37 +0100

    adding upgrade for owner recert triggers

--- roles/database/files/upgrade/6.1.3.sql
+++ roles/database/files/upgrade/6.1.3.sql
@@ -1,14 +1,51 @@
 ALTER TABLE recertification ADD COLUMN IF NOT EXISTS next_recert_date Timestamp;
 
--- query getRecerts($ownerId: Int, $mgmId: Int) {
---   recert_get_one_owner_one_mgm(args: {i_mgm_id: $mgmId, i_owner_id: $ownerId}, limit: 10) {
---     owner_id
---     ip_match
---     rule_id
---     recert_date
---     recertified
---   }
---}
+-- creating triggers for owner changes:
+
+CREATE OR REPLACE FUNCTION owner_change_triggered ()
+    RETURNS TRIGGER
+    AS $BODY$
+BEGIN
+    PERFORM recert_refresh_per_owner(NEW.id);
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql
+VOLATILE
+COST 100;
+ALTER FUNCTION public.owner_change_triggered () OWNER TO fworch;
+
+
+DROP TRIGGER IF EXISTS owner_change ON owner CASCADE;
+
+CREATE TRIGGER owner_change
+    BEFORE INSERT OR UPDATE ON owner
+    FOR EACH ROW
+    EXECUTE PROCEDURE owner_change_triggered ();
+
+
+CREATE OR REPLACE FUNCTION owner_network_change_triggered ()
+    RETURNS TRIGGER
+    AS $BODY$
+BEGIN
+    PERFORM recert_refresh_per_owner(NEW.id);
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql
+VOLATILE
+COST 100;
+ALTER FUNCTION public.owner_network_change_triggered () OWNER TO fworch;
+
+DROP TRIGGER IF EXISTS owner_network_change ON owner_network CASCADE;
+
+CREATE TRIGGER owner_network_change
+    BEFORE INSERT OR UPDATE ON owner_network
+    FOR EACH ROW
+    EXECUTE PROCEDURE owner_network_change_triggered ();
+
+
+--- refreshing future recert entries:
 
 CREATE OR REPLACE FUNCTION refresh_recert_entries () RETURNS VOID AS $$
 DECLARE
@@ -25,3 +62,4 @@ $$ LANGUAGE plpgsql;
 
 SELECT * FROM refresh_recert_entries ();
 DROP FUNCTION refresh_recert_entries();
+

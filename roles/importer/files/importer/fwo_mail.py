a2482491f68d9c205a13a080b28fa27316a1c30b
Tim Purschke <tmp@cactus.de>
2023-07-04 19:31:14 +0200

    enhance fw-admin permissions - add sync in UI - fix missing ldap and tenant rights
    also adding import change mail exception output

--- roles/importer/files/importer/fwo_mail.py
+++ roles/importer/files/importer/fwo_mail.py
@@ -24,21 +24,25 @@ def send_mail(recipient_list, subject, body, fwo_config):
             smtp_server.starttls() #setting up to TLS connection
             smtp_server.ehlo() #calling the ehlo() again as encryption happens on calling startttls()
         if 'emailUser' in fwo_config and 'emailPassword' in fwo_config and fwo_config['emailUser']!="":
-            smtp_server.login(fwo_config['emailUser'],fwo_config['emailPassword']) #logging into out email id
+            smtp_server.login(fwo_config['emailUser'], fwo_config['emailPassword']) #logging into out email id
 
         #sending the mail by specifying the from and to address and the message
         smtp_server.send_message(msg)
         smtp_server.quit() #terminating the server
-    except:
+    except Exception as e:
         logger = getFwoLogger()
-        logger.error("error while sending import change notification email")
+        logger.error("error while sending import change notification email: " + str(e))
 
 
 def send_change_notification_mail(fwo_config, number_of_changes, mgm_name, mgm_id):
     if 'impChangeNotifyActive' in fwo_config and bool(fwo_config['impChangeNotifyActive']) and 'impChangeNotifyRecipients' in fwo_config:
+        body = ""
+        if 'impChangeNotifyBody' in fwo_config:
+            body += fwo_config['impChangeNotifyBody'] + ": "
+        body += str(number_of_changes) + ", Management: " + mgm_name + " (id=" + mgm_id + ")"
         send_mail(
             fwo_config['impChangeNotifyRecipients'].split(','),
             fwo_config['impChangeNotifySubject'] if 'impChangeNotifySubject' in fwo_config else "firewall orchestrator change notification",
-            fwo_config['impChangeNotifyBody'] + ": " if 'impChangeNotifyBody' in fwo_config else  str(number_of_changes) + ", Management: " + mgm_name + " (id=" + mgm_id + ")",
+            body,
             fwo_config
         )

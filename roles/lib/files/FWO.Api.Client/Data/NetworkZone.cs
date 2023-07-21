4709c1940f87353359d572626f17f64cd158639c
NilsPur <48684538+NilsPur@users.noreply.github.com>
2023-07-21 16:20:39 +0200

    fix network zone id for integration tests

--- roles/lib/files/FWO.Api.Client/Data/NetworkZone.cs
+++ roles/lib/files/FWO.Api.Client/Data/NetworkZone.cs
@@ -8,7 +8,7 @@ namespace FWO.Api.Data
     public class NetworkZone
     {
         [JsonProperty("zone_id"), JsonPropertyName("zone_id")]
-        public int Id { get; set; } = -1;
+        public int Id { get; set; }
 
         [JsonProperty("zone_name"), JsonPropertyName("zone_name")]
         public string Name { get; set; } = "";

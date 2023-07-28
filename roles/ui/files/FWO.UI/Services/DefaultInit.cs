c13fa10600b43f9fcf230f256421e70e8104e4bc
abarz722 <achim.barz@gmx.de>
2023-07-28 10:00:14 +0200

    edit template

--- roles/ui/files/FWO.UI/Services/DefaultInit.cs
+++ roles/ui/files/FWO.UI/Services/DefaultInit.cs
@@ -5,11 +5,11 @@ namespace FWO.Ui.Services
     public static class DefaultInit
     {
         public static void DoNothing(Exception? e, string t, string m, bool E) {}
-        public static async Task DoNothing() {}
-        public static async Task DoNothing(string s) {}
-        public static async Task DoNothing(RequestStatefulObject s) {}
-        public static async Task DoNothing(RequestReqTask r) {}
-        public static async Task DoNothing(RequestImplTask i) {}
+        public static async Task DoNothing() { await Task.CompletedTask; }
+        public static async Task DoNothing(string s) { await Task.CompletedTask; }
+        public static async Task DoNothing(RequestStatefulObject s) { await Task.CompletedTask; }
+        public static async Task DoNothing(RequestReqTask r) { await Task.CompletedTask; }
+        public static async Task DoNothing(RequestImplTask i) {await Task.CompletedTask; }
         public static bool DoNothingSync() { return false; }
     }
 }

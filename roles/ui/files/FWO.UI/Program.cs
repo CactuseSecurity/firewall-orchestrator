f2a6c6ef0557034bf3913ec4ef1823887f857f20
Nils <48684538+NilsPur@users.noreply.github.com>
2023-08-19 15:41:20 +0200

    blazor environment fixes
    Use production / development based on the build type instead of always using development. Do not show detailed errors in production mode. Use the custom error page in the production environment. Spelling mistake fix

--- roles/ui/files/FWO.UI/Program.cs
+++ roles/ui/files/FWO.UI/Program.cs
@@ -1,35 +1,105 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorTable;
+using FWO.Api.Client;
+using FWO.Config.Api;
+using FWO.Config.File;
 using FWO.Logging;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using FWO.Middleware.Client;
+using FWO.Ui.Auth;
+using FWO.Ui.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.Circuits;
+using RestSharp;
+using System.Diagnostics;
+using FWO.Ui;
 
-namespace FWO.Ui
+// Implicitly call static constructor so backround lock process is started
+// (static constructor is only called after class is used in any way)
+Log.WriteInfo("Startup", "Starting FWO UI Server...");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseStaticWebAssets();
+
+/// Add services to the container.
+#region Services
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddScoped<CircuitHandler, CircuitHandlerService>();
+
+string ApiUri = ConfigFile.ApiServerUri;
+string MiddlewareUri = ConfigFile.MiddlewareServerUri;
+string ProductVersion = ConfigFile.ProductVersion;
+
+builder.Services.AddScoped<ApiConnection>(_ => new GraphQlApiConnection(ApiUri));
+builder.Services.AddScoped<MiddlewareClient>(_ => new MiddlewareClient(MiddlewareUri));
+
+// Create "anonymous" (empty) jwt
+MiddlewareClient middlewareClient = new MiddlewareClient(MiddlewareUri);
+ApiConnection apiConn = new GraphQlApiConnection(ApiUri);
+
+RestResponse<string> createJWTResponse = middlewareClient.CreateInitialJWT().Result;
+bool connectionEstablished = createJWTResponse.IsSuccessful;
+int connectionAttemptsCount = 1;
+while (!connectionEstablished)
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Implicitly call static constructor so backround lock process is started
-            // (static constructor is only called after class is used in any way)
-            Log.WriteInfo("Startup", "Starting FWO UI Server...");
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-					webBuilder.UseStaticWebAssets();
-					webBuilder.UseStartup<Startup>();
-                });
-        }
-    }
+	Log.WriteError("Middleware Server Connection",
+	$"Error while authenticating as anonymous user from UI (Attempt {connectionAttemptsCount}), "
+	+ $"Uri: {createJWTResponse.ResponseUri?.AbsoluteUri}, "
+	+ $"HttpStatus: {createJWTResponse.StatusDescription}, "
+	+ $"Error: {createJWTResponse.ErrorMessage}");
+	Thread.Sleep(500 * connectionAttemptsCount++);
+	createJWTResponse = middlewareClient.CreateInitialJWT().Result;
+	connectionEstablished = createJWTResponse.IsSuccessful;
 }
+
+string jwt = createJWTResponse.Data ?? throw new NullReferenceException("Received empty jwt.");
+apiConn.SetAuthHeader(jwt);
+
+// Get all non-confidential configuration settings and add to a global service (for all users)
+GlobalConfig globalConfig = Task.Run(async () => await GlobalConfig.ConstructAsync(jwt)).Result;
+builder.Services.AddSingleton<GlobalConfig>(_ => globalConfig);
+builder.Services.AddScoped<UserConfig>(_ => new UserConfig(globalConfig));
+
+builder.Services.AddScoped(_ => new NetworkZoneService());
+builder.Services.AddScoped(_ => new DomEventService());
+
+builder.Services.AddBlazorTable();
+
+#endregion
+
+var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+#region HTTP Request Pipeline
+
+Log.WriteInfo("Environment", $"{app.Environment.ApplicationName} runs in {app.Environment.EnvironmentName} Mode.");
+
+if (app.Environment.IsDevelopment())
+{
+	Debug.WriteLine("TEST");
+	app.UseDeveloperExceptionPage();
+}
+else
+{
+	Debug.WriteLine("TEST2");
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	// app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+#endregion
+
+app.Run();

bc6c297c4094960f912ede49bd3fd59cc59b889b
Tim Purschke <tmp@cactus.de>
2023-09-27 12:19:54 +0200

    adding more simulated change options

--- roles/sample-data/files/config_changes/changeRule.py
+++ roles/sample-data/files/config_changes/changeRule.py
@@ -6,18 +6,7 @@ import string
 import os
 import sys
 import json
-
-
-maxElements = 5
-commentChangeId = "FWORCH: "
-anyObj =  {
-      "name": "all",
-      "q_origin_key": "all"
-   }
-config_path = sys.argv[1] if len(sys.argv) >= 2 else "/tmp/demo10_fOS.json"
-tempConfigFile = config_path + ".tmp"
-deleteElement = False
-changeSource = False
+import logging
 
 
 def randomOctet():
@@ -33,54 +22,114 @@ def randomUid():
     return s[:8] + '-' + s[8:12] + '-' + s[12:16] + '-' + s[16:20] + '-' + s[20:]
 
 
+# constants:
+maxElements = 5
+commentChangeId = "FWORCH: "
+
+# fortiOS specific:
+anyObj =  {
+      "name": "all",
+      "q_origin_key": "all"
+   }
+
+srcString = 'srcaddr'
+dstString = 'dstaddr'
+nwObjString = "nw_obj_firewall/address"
+deleteElement = False
+changeSource = False
+
+if len(sys.argv) == 2:
+    config_path = sys.argv[1]
+else:
+    logging.error('did not specify config file as parameter')
+    print ("syntax: changeRule.py configFileName")
+    exit(1)
+
+tempConfigFile = config_path + ".tmp"
+
 with open(config_path) as f:
     config = json.load(f)
 
+# fortiOs settings:
 rules = config['rules']['rules']
+
+
 numberOfRules = len(rules)
-numberOfRules = round(numberOfRules/4)    # only change the first quarter of the rules and keep the rest as is
+numberOfRules = round(numberOfRules/2)    # only change the first half of the rules and keep the rest as is
 
 pickedRuleNumber = random.randrange(0, numberOfRules)
 rule = rules[pickedRuleNumber]
 
 if random.randrange(0, 2)==0:
     changeSource = True
-    ruleSide = rule['srcaddr']
+    ruleSide = rule[srcString]
 else:
-    ruleSide = rule['dstaddr']
+    ruleSide = rule[dstString]
 
 if len(ruleSide)>=maxElements:
     deleteElement=True
     del ruleSide[len(ruleSide)-1]
 
-if not deleteElement:
-    newUid = randomUid()
-    newIp = randomIp()
+actionChoices = ['changeSrcOrDst', 'enDisable', 'reverseAction', 'reverseLogging']
 
-    # cannot add to any obj, so delete it first
-    if anyObj in ruleSide:
-        del ruleSide[0]
+actionChosen = actionChoices[random.randrange(0, len(actionChoices))]
 
-    nwObj = {
-            "name": newIp,
-            "q_origin_key": newIp,
-            "uuid": newUid,
-            "subnet": [newIp, 32],
-            "type": "ipmask",
-            "obj-type": "ip",
-            "comment": commentChangeId + "random ip added as simulated change",
-            "associated-interface": "",
-            "color": 0
-        }
-    config["nw_obj_firewall/address"].append(nwObj)
+if actionChosen == 'changeSrcOrDst':
+    if not deleteElement:
+        newUid = randomUid()
+        newIp = randomIp()
 
-    nwObjRef = {
-            "name": newIp,
-            "q_origin_key": newIp
-        }
-    ruleSide.append(nwObjRef)
+        # cannot add to any obj, so delete it first
+        if anyObj in ruleSide:
+            del ruleSide[0]
 
+        nwObj = {
+                "name": newIp,
+                "q_origin_key": newIp,
+                "uuid": newUid,
+                "subnet": [newIp, 32],
+                "type": "ipmask",
+                "obj-type": "ip",
+                "comment": commentChangeId + "random ip added as simulated change",
+                "associated-interface": "",
+                "color": 0
+            }
+        config[nwObjString].append(nwObj)
+
+        nwObjRef = {
+                "name": newIp,
+                "q_origin_key": newIp
+            }
+        ruleSide.append(nwObjRef)
+
+elif actionChosen == 'enDisable':
+    if rule['status'] == "enable":
+        rule['status'] = "disable"
+    else:
+        rule['status'] = "enable"
+elif actionChosen == 'reverseAction':
+    if rule['action'] == "accept":
+        rule['action'] = "deny"
+    else:
+        rule['action'] = "accept"
+elif actionChosen == 'reverseLogging':
+    if rule['logtraffic-start'] == "enable":
+        rule['logtraffic-start'] = "disable"
+    else:
+        rule['logtraffic-start'] = "enable"
+else:
+    print ("unknown action chosen: " + actionChosen )
 with open(tempConfigFile, 'w', encoding='utf-8') as f:
     json.dump(config, f, ensure_ascii=False, indent=4)
 
 os.rename(tempConfigFile, config_path)
+if changeSource:
+    sideString = 'source'
+else:
+    sideString = 'destination'
+
+logText = 'changeRule simulator: changed rule no. ' + str(pickedRuleNumber)+ ', changeType=' + actionChosen
+if actionChosen=='changeSrcOrDst':
+    logText += ', changed ' + sideString 
+#print (logText)
+logging.info(logText)

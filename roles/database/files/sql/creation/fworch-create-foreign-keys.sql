05b658ec82090dd3966d89c871f0bd6fab5de5cb
Tim Purschke <tmp@cactus.de>
2023-10-25 07:53:11 +0200

    revert fk rule2nwobject

--- roles/database/files/sql/creation/fworch-create-foreign-keys.sql
+++ roles/database/files/sql/creation/fworch-create-foreign-keys.sql
@@ -101,12 +101,6 @@ ALTER TABLE "rule"
 Alter table "rule" add constraint "rule_metadata_dev_id_rule_uid_f_key"
   foreign key ("dev_id", "rule_uid") references "rule_metadata" ("dev_id", "rule_uid") on update restrict on delete cascade;
 
-Alter table "rule2nwobject" add foreign key ("obj_id") references "object" ("obj_id") on update restrict on delete cascade;
-Alter table "rule2nwobject" add  foreign key ("create") references "import_control" ("control_id") on update restrict on delete cascade;
-Alter table "rule2nwobject" add  foreign key ("last_seen") references "import_control" ("control_id") on update restrict on delete cascade;
-Alter table "rule2nwobject" add  foreign key ("rule_id") references "rule" ("rule_id") on update restrict on delete cascade;
-Alter table "rule2nwobject" add  foreign key ("user_id") references "usr" ("user_id") on update restrict on delete cascade;
-
 Alter table "rule_from" add  foreign key ("obj_id") references "object" ("obj_id") on update restrict on delete cascade;
 Alter table "rule_from" add  foreign key ("rf_create") references "import_control" ("control_id") on update restrict on delete cascade;
 Alter table "rule_from" add  foreign key ("rf_last_seen") references "import_control" ("control_id") on update restrict on delete cascade;

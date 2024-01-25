8ebc0b4f7f15c445aad13b3bc9de60d43961b710
Tim Purschke <tmp@cactus.de>
2024-01-25 12:01:29 +0100

    venv doc fix

--- scripts/install-ansible-from-venv.sh
+++ scripts/install-ansible-from-venv.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-# running this script needs access to pypi.org (either directly or through proxy) 
+# this script must be executed from within the repo directory using source as follows:
+# source scripts/install-ansible-from-venv.sh
+
+# for this you also need access to pypi.org (either directly or through proxy) 
 # for downloading ansible
 
 sudo apt install python3-venv

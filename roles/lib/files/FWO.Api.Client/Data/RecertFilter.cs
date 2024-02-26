7d705b57938d66901f503da24987932749ea3841
abarz722 <achim.barz@gmx.de>
2024-02-26 17:19:51 +0100

    new component recert param selection

--- roles/lib/files/FWO.Api.Client/Data/RecertFilter.cs
+++ roles/lib/files/FWO.Api.Client/Data/RecertFilter.cs
@@ -2,9 +2,19 @@ namespace FWO.Api.Data
 {
     public class RecertFilter
     {
-        public List<int> RecertOwnerList {get; set;} = new List<int>();
+        public List<int> RecertOwnerList {get; set;} = new ();
         public bool RecertShowAnyMatch {get; set;} = false;
         public int RecertificationDisplayPeriod = 0;
 
+        public RecertFilter()
+        {}
+
+        public RecertFilter(RecertFilter recertFilter)
+        {
+            RecertOwnerList = new(recertFilter.RecertOwnerList);
+            RecertShowAnyMatch = recertFilter.RecertShowAnyMatch;
+            RecertificationDisplayPeriod = recertFilter.RecertificationDisplayPeriod;
+
+        }
     }
 }

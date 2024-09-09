2220ea8e02cf7172b147651fc0e48028d61e2b7e
solidprogramming <Luca.Weidmann@gmx.de>
2024-09-09 14:36:55 +0200

    [~] Changed login UI for better responsiveness [~] Monitoring tables changed page size

--- roles/database/files/upgrade/8.3.2.sql
+++ roles/database/files/upgrade/8.3.2.sql
@@ -1,3 +1,1 @@
 insert into config (config_key, config_value, config_user) VALUES ('welcomeMessage', '', 0) ON CONFLICT DO NOTHING;
--- INSERT INTO txt VALUES ('H9054', 'German',  'Nachricht die auf der Anmeldeseite angezeigt werden soll.');
--- INSERT INTO txt VALUES ('H9054', 'English', 'Message that is displayed on Login Page');

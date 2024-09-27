a94bae7d41a20ebd3dc57986c2e9b54f7c3912af
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-09-27 05:09:47 +0200

    [~] ModellingConnectionHandler refresh after creating a new connection #2549

--- roles/ui/files/FWO.UI/Services/ModellingConnectionHandler.cs
+++ roles/ui/files/FWO.UI/Services/ModellingConnectionHandler.cs
@@ -1011,6 +1011,7 @@ namespace FWO.Ui.Services
                     if(AddMode)
                     {
                         await AddConnectionToDb();
+                        await ReInit();
                     }
                     else
                     {

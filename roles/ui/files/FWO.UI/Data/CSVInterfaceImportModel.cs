d4d4897d979c172adb618a7dcbfda92dc4c92c1c
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-10-09 11:46:36 +0200

    [-] Removed Appserver import at manualappserver [+] Added Appserver import at modelling settings WIP  #2582

---
+++ roles/ui/files/FWO.UI/Data/CSVInterfaceImportModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FWO.Ui.Data
+{
+    public class CSVInterfaceImportModel
+    {
+        public string? AppID { get; set; }
+        public string? AppServerTyp { get; set; }
+        public string? AppIPRange { get; set; }
+    }
+}

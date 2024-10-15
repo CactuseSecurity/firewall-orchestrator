25244e0409cc3d80143bc059a1d9c22f25b68993
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-10-15 14:41:54 +0200

    [+] add missing FK connection #2591

---
+++ roles/database/files/upgrade/8.4.1.sql
@@ -0,0 +1,2 @@
+ALTER TABLE modelling.connection DROP CONSTRAINT IF EXISTS modelling_connection_proposed_app_id_foreign_key;
+ALTER TABLE modelling.connection ADD CONSTRAINT modelling_connection_proposed_app_id_foreign_key FOREIGN KEY (proposed_app_id) REFERENCES owner(id) ON UPDATE RESTRICT ON DELETE CASCADE;

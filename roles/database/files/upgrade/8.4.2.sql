d56f3f0e3cefd450f6881299f1df2678b38a9dee
abarz722 <achim.barz@gmx.de>
2024-10-19 07:03:01 +0200

    add enum value to database

--- roles/database/files/upgrade/8.4.2.sql
+++ roles/database/files/upgrade/8.4.2.sql
@@ -52,3 +52,5 @@ insert into config (config_key, config_value, config_user) VALUES ('externalRequ
 insert into config (config_key, config_value, config_user) VALUES ('externalRequestStartAt', '00:00:00', 0) ON CONFLICT DO NOTHING;
 insert into config (config_key, config_value, config_user) VALUES ('modRolloutResolveServiceGroups', 'true', 0) ON CONFLICT DO NOTHING;
 insert into config (config_key, config_value, config_user) VALUES ('modRolloutBundleTasks', 'false', 0) ON CONFLICT DO NOTHING;
+
+ALTER TYPE action_enum ADD VALUE IF NOT EXISTS 'unchanged';

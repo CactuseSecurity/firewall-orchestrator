a7f3ccc96cece1d595b3185ffe5906dc028be6a4
alf-cactus <alf@cactus.de>
2024-11-20 09:33:52 +0100

    remove duplicate color line

--- roles/database/files/upgrade/8.5.2.sql
+++ roles/database/files/upgrade/8.5.2.sql
@@ -2,4 +2,3 @@ insert into stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4') ON
 insert into stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed') ON CONFLICT DO NOTHING;
 insert into stm_color (color_name, color_rgb) VALUES ('olive', '617d28') ON CONFLICT DO NOTHING;
 insert into stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00') ON CONFLICT DO NOTHING;
-insert into stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00') ON CONFLICT DO NOTHING;

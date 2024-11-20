c417c600065e2c20743d3f42d19341bc9064c8f6
alf-cactus <alf@cactus.de>
2024-11-20 09:30:47 +0100

    add missing colors

---
+++ roles/database/files/upgrade/8.5.2.sql
@@ -0,0 +1,5 @@
+insert into stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4') ON CONFLICT DO NOTHING;
+insert into stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed') ON CONFLICT DO NOTHING;
+insert into stm_color (color_name, color_rgb) VALUES ('olive', '617d28') ON CONFLICT DO NOTHING;
+insert into stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00') ON CONFLICT DO NOTHING;
+insert into stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00') ON CONFLICT DO NOTHING;

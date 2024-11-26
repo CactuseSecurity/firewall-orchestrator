58a37841f220a7c9c7ea326493901f3ee8eb8b68
Tim Purschke <tmp@cactus.de>
2024-11-26 19:06:13 +0100

    default owner ldap internal fix

--- roles/database/files/upgrade/8.5.2.sql
+++ roles/database/files/upgrade/8.5.2.sql
@@ -2,5 +2,5 @@
 drop trigger IF exists refresh_view_rule_with_owner_delete_trigger ON recertification CASCADE;
 
 insert into config (config_key, config_value, config_user) VALUES ('ownerLdapId', '1', 0) ON CONFLICT DO NOTHING;
-insert into config (config_key, config_value, config_user) VALUES ('ownerLdapGroupNames', 'ModellerGroup_@@ExternalAppId@@', 0) ON CONFLICT DO NOTHING;
+insert into config (config_key, config_value, config_user) VALUES ('ownerLdapGroupNames', 'cn=ModellerGroup_@@ExternalAppId@@,ou=groupOfUniqueNames,dc=fworch,dc=internal', 0) ON CONFLICT DO NOTHING;
 insert into config (config_key, config_value, config_user) VALUES ('manageOwnerLdapGroups', 'true', 0) ON CONFLICT DO NOTHING;

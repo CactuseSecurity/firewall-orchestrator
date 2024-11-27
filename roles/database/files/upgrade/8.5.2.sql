7ac2b8e032b038ed4d0d2dfed181eeed17a275f6
alf-cactus <alf@cactus.de>
2024-11-27 15:02:43 +0100

    database syntax

--- roles/database/files/upgrade/8.5.2.sql
+++ roles/database/files/upgrade/8.5.2.sql
@@ -1,14 +1,14 @@
 DO $do$ BEGIN
-    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='crete blue') THEN
-        INSERT INTO stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4')
+    IF NOT EXISTS (SELECT color_name FROM stm_color WHERE color_name='crete blue') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4');
     END IF;
-    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='state blue') THEN
-        INSERT INTO stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed')
+    IF NOT EXISTS (SELECT color_name FROM stm_color WHERE color_name='state blue') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed');
     END IF;
-    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='olive') THEN
-        INSERT INTO stm_color (color_name, color_rgb) VALUES ('olive', '617d28')
+    IF NOT EXISTS (SELECT color_name FROM stm_color WHERE color_name='olive') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('olive', '617d28');
     END IF;
-    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='dark gold') THEN
-        INSERT INTO stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00')
+    IF NOT EXISTS (SELECT color_name FROM stm_color WHERE color_name='dark gold') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00');
     END IF;
 END $do$;

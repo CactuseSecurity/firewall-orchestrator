d15ac397deff2b5761c968a759090bb985e2b5b2
alf-cactus <alf@cactus.de>
2024-11-27 14:07:50 +0100

    8.5.2 update with stm_color

--- roles/database/files/upgrade/8.5.2.sql
+++ roles/database/files/upgrade/8.5.2.sql
@@ -1,4 +1,14 @@
-insert into stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4') ON CONFLICT DO NOTHING;
-insert into stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed') ON CONFLICT DO NOTHING;
-insert into stm_color (color_name, color_rgb) VALUES ('olive', '617d28') ON CONFLICT DO NOTHING;
-insert into stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00') ON CONFLICT DO NOTHING;
+DO $do$ BEGIN
+    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='crete blue') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('crete blue', '485cd4')
+    END IF;
+    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='state blue') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('state blue', 'a186ed')
+    END IF;
+    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='olive') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('olive', '617d28')
+    END IF;
+    IF EXISTS (SELECT color_name FROM stm_color WHERE color_name='dark gold') THEN
+        INSERT INTO stm_color (color_name, color_rgb) VALUES ('dark gold', 'cdad00')
+    END IF;
+END $do$;

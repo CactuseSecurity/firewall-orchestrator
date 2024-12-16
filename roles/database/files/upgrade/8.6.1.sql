14f2e3a8c3dee0b2117d1f9d2f98d60fcefddeff
Tim Purschke <tmp@cactus.de>
2024-12-16 17:17:41 +0100

    fix constraint

--- roles/database/files/upgrade/8.6.1.sql
+++ roles/database/files/upgrade/8.6.1.sql
@@ -1,3 +1,3 @@
 ALTER TABLE owner_network DROP CONSTRAINT IF EXISTS owner_network_ip_unique;
-ALTER TABLE owner_network ADD CONSTRAINT owner_network_ip_unique UNIQUE (owner_id, ip, ip_end, import_source) REFERENCES owner(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+ALTER TABLE owner_network ADD CONSTRAINT owner_network_ip_unique UNIQUE (owner_id, ip, ip_end, import_source);
 ALTER TABLE ext_request ADD COLUMN IF NOT EXISTS locked boolean default false;

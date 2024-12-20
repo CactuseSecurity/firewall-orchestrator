ef1137a8617017c02d79abfdadc86f9aa36597b7
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-12-20 13:33:48 +0100

    .

--- roles/lib/files/FWO.Report/ReportBase.cs
+++ roles/lib/files/FWO.Report/ReportBase.cs
@@ -247,7 +247,7 @@ namespace FWO.Report
                 browserFetcher = new();
             }
 
-            InstalledBrowser? brw = await browserFetcher.DownloadAsync()
+            InstalledBrowser? brw = await browserFetcher.DownloadAsync();
 
 
             using IBrowser? browser = await Puppeteer.LaunchAsync(new LaunchOptions

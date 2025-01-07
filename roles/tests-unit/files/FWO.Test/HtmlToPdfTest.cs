c42e1c02f8162ea03c1d6b24c0ab442dcb98ca1b
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2025-01-07 12:16:11 +0100

    .

--- roles/tests-unit/files/FWO.Test/HtmlToPdfTest.cs
+++ roles/tests-unit/files/FWO.Test/HtmlToPdfTest.cs
@@ -19,7 +19,7 @@ namespace FWO.Test
         {
             var isGitHubActions = Environment.GetEnvironmentVariable("GITHUB_ACTIONS") == "true";
 
-            Log.WriteInfo("Test Log", $"Is githib actions: {Environment.GetEnvironmentVariable("GITHUB_ACTIONS")}");
+            Log.WriteInfo("Test Log", $"Is githib actions: {isGitHubActions}");
 
             // the PDF generation with puppeteer is currently not working in GitHub Actions
             if (!isGitHubActions)

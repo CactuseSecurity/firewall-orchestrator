c063d3c6596e1c520187a88e698993c3fa92c429
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2025-02-06 10:01:16 +0100

    .

--- roles/tests-unit/files/FWO.Test/HtmlToPdfTest.cs
+++ roles/tests-unit/files/FWO.Test/HtmlToPdfTest.cs
@@ -23,69 +23,65 @@ namespace FWO.Test
 
             string? isGitHubActions = Environment.GetEnvironmentVariable("RUNNING_ON_GITHUB_ACTIONS");
 
-            // the PDF generation with puppeteer is currently not working in GitHub Actions
-            if (isGitHubActions != null)
+
+            if (File.Exists(FilePath))
+                File.Delete(FilePath);
+
+            OperatingSystem? os = Environment.OSVersion;
+
+            Log.WriteInfo("Test Log", $"OS: {os}");
+
+            string path = "";
+            BrowserFetcher? browserFetcher = default;
+
+            switch (os.Platform)
             {
-                if (File.Exists(FilePath))
-                    File.Delete(FilePath);
-
-                OperatingSystem? os = Environment.OSVersion;
-
-                Log.WriteInfo("Test Log", $"OS: {os}");
-
-                string path = "";
-                BrowserFetcher? browserFetcher = default;
-
-                switch (os.Platform)
-                {
-                    case PlatformID.Win32NT:
-                        browserFetcher = new();
-                        break;
-                    case PlatformID.Unix:
-                        path = ChromeBinPathLinux;
-                        browserFetcher = new(new BrowserFetcherOptions { Path = path, Platform = Platform.Linux, Browser = SupportedBrowser.Chrome });
-                        break;
-                    default:
+                case PlatformID.Win32NT:
+                    browserFetcher = new();
                     break;
-                }
-
-                InstalledBrowser? brw = await browserFetcher.DownloadAsync(BrowserTag.Stable);
-
-                using IBrowser? browser = await Puppeteer.LaunchAsync(new LaunchOptions
-                {
-                    ExecutablePath = isGitHubActions!=null ? "/usr/bin/chromium-browser" : brw.GetExecutablePath(),
-                    Headless = true,
-                    DumpIO = isGitHubActions!=null ? true : false, // Enables debug logs
-                    //Args = isGitHubActions!=null ?
-                    //    new[] { "--no-sandbox", "--database=/tmp", "--disable-setuid-sandbox" }
-                    //    : [] // No additional arguments locally
-                });
-
-                try
-                {
-                    await TryCreatePDF(browser, PaperFormat.A0);
-                    await TryCreatePDF(browser, PaperFormat.A1);
-                    await TryCreatePDF(browser, PaperFormat.A2);
-                    await TryCreatePDF(browser, PaperFormat.A3);
-                    await TryCreatePDF(browser, PaperFormat.A4);
-                    await TryCreatePDF(browser, PaperFormat.A5);
-                    await TryCreatePDF(browser, PaperFormat.A6);
-
-                    await TryCreatePDF(browser, PaperFormat.Ledger);
-                    await TryCreatePDF(browser, PaperFormat.Legal);
-                    await TryCreatePDF(browser, PaperFormat.Letter);
-                    await TryCreatePDF(browser, PaperFormat.Tabloid);
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-                finally
-                {
-                    await browser.CloseAsync();
-                }
+                case PlatformID.Unix:
+                    path = ChromeBinPathLinux;
+                    browserFetcher = new(new BrowserFetcherOptions { Path = path, Platform = Platform.Linux, Browser = SupportedBrowser.Chrome });
+                    break;
+                default:
+                    break;
             }
 
+            InstalledBrowser? brw = await browserFetcher.DownloadAsync(BrowserTag.Stable);
+
+            using IBrowser? browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                ExecutablePath = isGitHubActions != null ? "/usr/bin/chromium-browser" : brw.GetExecutablePath(),
+                Headless = true,
+                DumpIO = isGitHubActions != null ? true : false, // Enables debug logs
+                                                                 //Args = isGitHubActions!=null ?
+                                                                 //    new[] { "--no-sandbox", "--database=/tmp", "--disable-setuid-sandbox" }
+                                                                 //    : [] // No additional arguments locally
+            });
+
+            try
+            {
+                await TryCreatePDF(browser, PaperFormat.A0);
+                await TryCreatePDF(browser, PaperFormat.A1);
+                await TryCreatePDF(browser, PaperFormat.A2);
+                await TryCreatePDF(browser, PaperFormat.A3);
+                await TryCreatePDF(browser, PaperFormat.A4);
+                await TryCreatePDF(browser, PaperFormat.A5);
+                await TryCreatePDF(browser, PaperFormat.A6);
+
+                await TryCreatePDF(browser, PaperFormat.Ledger);
+                await TryCreatePDF(browser, PaperFormat.Legal);
+                await TryCreatePDF(browser, PaperFormat.Letter);
+                await TryCreatePDF(browser, PaperFormat.Tabloid);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await browser.CloseAsync();
+            }
         }
 
         private static bool IsValidHTML(string html)
@@ -124,7 +120,7 @@ namespace FWO.Test
             catch (Exception)
             {
                 throw new Exception("This paper kind is currently not supported. Please choose another one or \"Custom\" for a custom size.");
-            }            
+            }
         }
 
         [OneTimeTearDown]

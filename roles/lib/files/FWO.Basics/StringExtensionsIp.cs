688719c91977bee01a26ad8bca880f119973b0a1
Robin-Smets <robinsmets@outlook.de>
2025-02-12 18:27:06 +0100

    #2861: Changed Ordering Logic in App Role builder to be based on IPRange objects

--- roles/lib/files/FWO.Basics/StringExtensionsIp.cs
+++ roles/lib/files/FWO.Basics/StringExtensionsIp.cs
@@ -210,4 +210,3 @@ namespace FWO.Basics
 
     }
 }
-
--- roles/ui/files/FWO.UI/Data/OrderMode.cs
+++ roles/ui/files/FWO.UI/Data/OrderMode.cs
@@ -1,9 +1,11 @@
 namespace FWO.Ui.Data
 {
+    /// <summary>
+    /// Defines the mode that should be used to order a collection.
+    /// </summary>
     public enum OrderMode
     {
         Asc,
         Desc        
     }
-
 }

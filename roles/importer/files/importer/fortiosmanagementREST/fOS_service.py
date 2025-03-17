0d5cc592c6089b4a849d709e8500802b92e9cd5a
Tim Purschke <tmp@cactus.de>
2025-03-17 15:28:57 +0100

    importer-py changes rollback etc.

--- roles/importer/files/importer/fortiosmanagementREST/fOS_service.py
+++ roles/importer/files/importer/fortiosmanagementREST/fOS_service.py
@@ -117,19 +117,19 @@ def extractSinglePortRange(port_range):
     port_end = port
 
     # open ranges (not found so far in data)
-    pattern = re.compile('^\>(\d+)$')
+    pattern = re.compile(r'^\>(\d+)$')
     match = pattern.match(port)
     if match:
         port = str(int(match.group()[1:]) + 1)
         port_end = str(65535)
-    pattern = re.compile('^\<(\d+)$')
+    pattern = re.compile(r'^\<(\d+)$')
     match = pattern.match(port)
     if match:
         port = str(1)
         port_end = str(int(match.group()[1:]) - 1)
 
     # split ranges
-    pattern = re.compile('^(\d+)\-(\d+)$')
+    pattern = re.compile(r'^(\d+)\-(\d+)$')
     match = pattern.match(port)
     if match:
         port, port_end = match.group().split('-')

ec8139048c8a97cae2db88661cbb495d2397cc7e
Tim Purschke <tmp@cactus.de>
2025-04-01 10:37:12 +0200

    fix long module path

--- roles/importer/files/importer/dummyroutermanagement1/fwcommon.py
+++ roles/importer/files/importer/dummyroutermanagement1/fwcommon.py
@@ -6,7 +6,7 @@ sys.path.append(importer_base_dir + '/dummyrouter')
 from curses import raw
 from fwo_log import getFwoLogger
 import fwo_globals
-from roles.importer.files.importer.model_controllers.interface_controller import Interface
+from model_controllers.interface_controller import Interface
 from model_controllers.route_controller import Route
 import json, requests, requests.packages
 from datetime import datetime

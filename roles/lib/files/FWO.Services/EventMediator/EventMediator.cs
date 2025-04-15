748694a6b9dadc7b33f612bd5e40bfb492e9a9db
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2025-04-15 18:04:42 +0200

    [+] Some comments [+] EventMediator Unsubscribe #3145

--- roles/lib/files/FWO.Services/EventMediator/EventMediator.cs
+++ roles/lib/files/FWO.Services/EventMediator/EventMediator.cs
@@ -1,25 +1,43 @@
-using FWO.Data;
 using FWO.Services.EventMediator.Interfaces;
 
 namespace FWO.Services.EventMediator;
 
 public class EventMediator : IEventMediator
 {
-    private readonly Dictionary<Type, List<Action<IEvent>>> _handlers = [];
+    private readonly Dictionary<Type, Dictionary<string, List<Action<IEvent>>>> _handlers = [];
 
-    public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : class, IEvent
+    /// <summary>
+    /// Adds the handler as an invokeable action.
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    /// <param name="name"></param>
+    /// <param name="handler"></param>
+    public void Subscribe<TEvent>(string name, Action<TEvent> handler) where TEvent : class, IEvent
     {
         if(!_handlers.ContainsKey(typeof(TEvent)))
         {
             _handlers[typeof(TEvent)] = [];
         }
 
-        _handlers[typeof(TEvent)].Add(e => handler((TEvent)e));
+        if(!_handlers[typeof(TEvent)].TryGetValue(name, out List<Action<IEvent>>? value))
+        {
+            value = [];
+            _handlers[typeof(TEvent)][name] = value;
+        }
+
+        value.Add(e => handler((TEvent)e));
     }
 
-    public void Publish<TEvent>(TEvent @event)  where TEvent : class, IEvent
+    /// <summary>
+    /// If matching subscription handler was found it will be invoked.
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    /// <param name="name"></param>
+    /// <param name="event"></param>
+    public void Publish<TEvent>(string name, TEvent @event) where TEvent : class, IEvent
     {
-        if(_handlers.TryGetValue(typeof(TEvent), out List<Action<IEvent>>? handlers))
+        if(_handlers.TryGetValue(typeof(TEvent), out Dictionary<string, List<Action<IEvent>>>? actions) 
+            && actions.TryGetValue(name, out List<Action<IEvent>>? handlers))
         {
             foreach(Action<IEvent> handler in handlers)
             {
@@ -28,9 +46,20 @@ public class EventMediator : IEventMediator
         }
     }
 
-    public void Unsubscribe<TEvent>(TEvent @event)
+    /// <summary>
+    /// Unsubscribe of ALL events of the given type matching the name.
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    /// <param name="event"></param>
+    /// <returns>True/False if remove was successfull</returns>
+    public bool Unsubscribe<TEvent>(string name) where TEvent : class, IEvent
     {
+        if(_handlers.ContainsKey(typeof(TEvent)))
+        {
+           return _handlers.Remove(typeof(TEvent));
+        }
 
+        return false;
     }
 }
 
--- roles/lib/files/FWO.Services/EventMediator/Interfaces/IEventMediator.cs
+++ roles/lib/files/FWO.Services/EventMediator/Interfaces/IEventMediator.cs
@@ -2,7 +2,8 @@ namespace FWO.Services.EventMediator.Interfaces;
 
 public interface IEventMediator
 {
-    void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : class, IEvent;
-    void Publish<TEvent>(TEvent @event) where TEvent : class, IEvent;
+    void Subscribe<TEvent>(string name, Action<TEvent> handler) where TEvent : class, IEvent;
+    void Publish<TEvent>(string name, TEvent @event) where TEvent : class, IEvent;
+    bool Unsubscribe<TEvent>(string name) where TEvent : class, IEvent;
 }
 

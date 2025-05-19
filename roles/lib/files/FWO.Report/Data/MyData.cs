fcc4e967c1e48aa7c91da18311f0c2582d070a14
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2025-05-19 12:56:23 +0200

    .

--- roles/lib/files/FWO.Report/Data/MyData.cs
+++ roles/lib/files/FWO.Report/Data/MyData.cs
@@ -1,11 +1,6 @@
 namespace FWO.Report.Data
 {
-    public class MyData
-    {
-        public string Name { get; set; } = "";
-        public HeaderType HeaderType { get; set; } = HeaderType.None;
-    }
-
+ 
     public enum HeaderType
     {
         None,

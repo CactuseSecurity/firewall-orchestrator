eeae820d14e7ef9653ca7c62c39b462dc8664d29
Robin-Smets <robinsmets@outlook.de>
2025-05-19 11:51:52 +0200

    issue 3170 - made existing implementation of chunked call generic

--- roles/importer/files/importer/fwo_api_oo.py
+++ roles/importer/files/importer/fwo_api_oo.py
@@ -4,26 +4,30 @@ import json
 import traceback
 import copy
 import time
+import re
 
 import fwo_globals
+
 from fwo_log import getFwoLogger
 from fwo_const import fwo_api_http_import_timeout, api_call_chunk_size
 from fwo_exceptions import FwoApiServiceUnavailable, FwoApiTimeout
-from fwo_base import ChunkableVariableType
+
 
 # this class is used for making calls to the FWO API (will supersede fwo_api.py)
 class FwoApi():
     
     FwoApiUrl: str
     FwoJwt: str
+    query_info: dict
 
 
     def __init__(self, ApiUri, Jwt):
         self.FwoApiUrl = ApiUri
         self.FwoJwt = Jwt
+        self.query_info = {}
 
 
-    def call(self, query, queryVariables="", chunkable_variable="", query_name="", return_object_name="", debug_level=0):
+    def call(self, query, queryVariables="", debug_level=0, analyze_payload=False):
         """
             The standard FWO API call.
         """
@@ -36,8 +40,9 @@ class FwoApi():
         }
         full_query = {"query": query, "variables": queryVariables}
         logger = getFwoLogger(debug_level=debug_level)
-        is_chunked_call, chunkable_variable_type, total_elements = self._check_is_chunkable_call(queryVariables, chunkable_variable)
-        started = time.time()
+
+        if analyze_payload:
+            self._analyze_payload(query, queryVariables)
 
         with requests.Session() as session:
             if fwo_globals.verify_certs is None:    # only for first FWO API call (getting info on cert verification)
@@ -47,10 +52,13 @@ class FwoApi():
             session.headers = request_headers
 
             try: 
-                if is_chunked_call:
-                    return_object = self._call_chunked(session, query, queryVariables, chunkable_variable, query_name, return_object_name, debug_level, chunkable_variable_type, total_elements)
+                if analyze_payload and self.query_info["chunking_info"]["needs_chunking"]:
+                    started = time.time()
+                    return_object = self._call_chunked(session, query, queryVariables, debug_level)
                     elapsed_time = time.time() - started
-                    logger.debug(f"Chunked API call ({query_name}) processed in {elapsed_time:.4f} s.")
+                    affected_rows = sum(obj["affected_rows"] for obj in return_object["data"].values())
+                    logger.debug(f"Chunked API call ({self.query_info["query_name"]}) processed in {elapsed_time:.4f} s. Affected rows: {affected_rows}.")
+                    self.query_info = {}
                 else:
                     return_object = self._post_query(session, full_query)
 
@@ -81,81 +89,152 @@ class FwoApi():
                 raise FwoApiTimeout(f"FWO API HTTP error 502 (might have reached timeout of {int(fwo_api_http_import_timeout)/60} minutes)")
         raise exception
 
-
-    def _check_is_chunkable_call(self, query_variables, chunkable_variable):
+    def _analyze_payload(self, query, query_variables):
         """
-            Evaluates the type of the given chunkable_variable and the need for chunking (depending on the number of elements within the chunkable variable).
+            Analyzes the query and the query_variables and writes the findings to 'self.query_info'.
         """
 
-        is_chunkable_call = False
-        chunkable_variable_type = ChunkableVariableType.DEFAULT
-        total_elements = 0
+        query_string = query
+        query_length = len(query_string)
 
-        if chunkable_variable != "":
+        # Parse query type and name from query string.
 
-            # Check if call qualifies as standard chunked call.
+        query_string_split = query_string.strip().split()
+        query_type = query_string_split[0] if query_string_split[0] in ("query", "mutation", "subscription") else "unknown"
+        query_name_regex_result = re.match(r"[A-Za-z]+", query_string_split[1])
+        query_name = query_name_regex_result.group(0) if query_name_regex_result else ""
 
-            if isinstance(chunkable_variable, str):
-                chunkable_variable_type = ChunkableVariableType.STRING
-                total_elements = len(query_variables[chunkable_variable])
+        # Parse args from query string.       
+        
+        query_args = {}
+        query_string_substring = query_string.removeprefix(f"{query_type} {query_name}")
+        query_args_regex_result = re.match(r"\(([^)]*)\)", query_string_substring)
+        query_args_string = query_args_regex_result.group(1) if query_args_regex_result else ""
+        query_args_string_split = re.split(r'[,\n]', query_args_string)
+        
+        for query_args_string_split_element in query_args_string_split:
+            element_split = query_args_string_split_element.strip().split()
+            arg_name = element_split[0].removeprefix("$").removesuffix(":")
+            arg_type = element_split[1]
+            query_args[arg_name] = arg_type
 
-            # Check if call qualifies as chunked multi-table call.
+        # Parse chunking info.
 
-            elif isinstance(chunkable_variable, list) and all(isinstance(item, tuple) for item in chunkable_variable):
-                chunkable_variable_type = ChunkableVariableType.LIST_OF_TUPLES
-                referenced_update_args = chunkable_variable[0][1]
-                total_elements = len(referenced_update_args)
-            
-            else:
-                chunkable_variable_type = ChunkableVariableType.UNKNOWN
-            
-            if total_elements > api_call_chunk_size:
-                is_chunkable_call = True
+        needs_chunking, adjusted_chunk_size, list_elements_length, chunkable_variables = self._get_chunking_info(query_variables)
+        chunking_info = {
+            "needs_chunking": needs_chunking,
+            "adjusted_chunk_size": adjusted_chunk_size,
+            "chunkable_variables": chunkable_variables,
+            "total_elements": list_elements_length
+        }
+        
+        # Save information in field query_info.
 
-        return is_chunkable_call, chunkable_variable_type, total_elements
+        self.query_info = {
+            "query_string": query_string,
+            "query_length": query_length,
+            "query_type": query_type,
+            "query_name": query_name,
+            "query_args": query_args,
+            "chunking_info": chunking_info
+        }
         
 
-    def _call_chunked(self, session, query, query_variables="", chunkable_variable="", query_name="", return_object_name="", debug_level=0, chunkable_variable_type=ChunkableVariableType.DEFAULT, total_elements=0):
+    def _get_chunking_info(self, query_variables: dict):
+
+        # Get all query variables of type list.
+
+        lists_in_query_variable = {
+            chunkable_variable_name: list_object 
+            for chunkable_variable_name, list_object in query_variables.items() 
+            if isinstance(list_object, list)
+        }
+
+        # If there is no list typed query variable there is nothing chunkable.
+
+        if not lists_in_query_variable or len(lists_in_query_variable.items()) == 0:
+            return False, 0, []
+        
+        list_elements_length = sum(
+            len(list_object) 
+            for list_object in lists_in_query_variable.values() 
+        )
+
+        # If the number of all elements is lower than the configured threshold, there is no need for chunking.
+
+        if list_elements_length < api_call_chunk_size:
+            return False, 0, 0, []
+        
+        # If there are more than one chunkable variable, the chunk_size has to be adjusted accordingly.
+
+        adjusted_chunk_size = int(api_call_chunk_size / len(lists_in_query_variable.items()))
+
+        return True, adjusted_chunk_size, list_elements_length, list(lists_in_query_variable.keys())
+        
+
+    def _call_chunked(self, session, query, query_variables="", debug_level=0):
         """
             Splits a defined query variable into chunks and posts the queries chunk by chunk.
         """
 
         chunk_number = 1
-        obj_count = 0
+        total_processed_elements = 0
         return_object = {}
-        chunked_query_variables = copy.deepcopy(query_variables)
         logger = getFwoLogger(debug_level=debug_level)
-        logger.debug(f"Processing chunked API call ({query_name})...")
+        logger.debug(f"Processing chunked API call ({self.query_info["query_name"]})...")
+
+        # Separate chunkable variables.
+
+        chunkable_variables = {
+            variable: list_object
+            for variable, list_object in query_variables.items()
+            if variable in list(self.query_info["chunking_info"]["chunkable_variables"])
+        }
 
         # Loops until all elements of the the query variable have been processed.
-        while(obj_count < chunk_number * api_call_chunk_size and obj_count < total_elements):
-            # Gets current chunk and sets it as query variable
-            chunk = []
+        while(total_processed_elements < self.query_info["chunking_info"]["total_elements"]):
+            
+            total_chunk_elements = 0
 
-            if chunkable_variable_type == ChunkableVariableType.STRING:
-                chunk = query_variables[chunkable_variable][obj_count : obj_count + api_call_chunk_size]
-                chunked_query_variables[chunkable_variable] = chunk
-            elif chunkable_variable_type == ChunkableVariableType.LIST_OF_TUPLES:
-                update_args = chunkable_variable[0][1]
-                chunk = update_args[obj_count : obj_count + api_call_chunk_size]
-                for key in chunked_query_variables.keys():
-                    chunked_query_variables[key] = chunk
-            else:
-                raise NotImplementedError()
+            # Gets current chunks slice borders.
+
+            slice_start = (chunk_number -1) * self.query_info["chunking_info"]["adjusted_chunk_size"]
+            slice_end = chunk_number * self.query_info["chunking_info"]["adjusted_chunk_size"]
+
+            # Gets current chunks.
+
+            chunks = {
+                variable: list_object[slice_start:slice_end]
+                for variable, list_object in chunkable_variables.items()
+            }
+
+            for variable, chunk in chunks.items():
+                query_variables[variable] = chunk
+                total_chunk_elements = len(chunk)
 
             # Post query.
-            response = self._post_query(session, {"query": query, "variables": chunked_query_variables})
+
+            response = self._post_query(session, {"query": query, "variables": query_variables})
+
             # Gather and merge returning data.
+
             if return_object == {}:
                 return_object = response
             else:
                 new_return = response
-                if chunkable_variable_type != ChunkableVariableType.LIST_OF_TUPLES:
-                    return_object["data"][return_object_name]["returning"].extend(new_return["data"][return_object_name]["returning"])
-                    return_object["data"][return_object_name]["affected_rows"] += new_return["data"][return_object_name]["affected_rows"]
+                for new_return_object_type, new_return_object in new_return["data"].items():
+                    if not isinstance(return_object["data"].get(new_return_object_type), dict):
+                        return_object["data"][new_return_object_type] = {}
+                        return_object["data"][new_return_object_type]["affected_rows"] = 0
+                        return_object["data"][new_return_object_type]["returning"] = []
+                    return_object["data"][new_return_object_type]["affected_rows"] += new_return_object["affected_rows"]
+                    if "returning" in return_object["data"][new_return_object_type].keys():
+                        return_object["data"][new_return_object_type]["returning"].extend(new_return_object["returning"])
+
             # Log current state of the process and increment variables.
-            obj_count += len(chunk)
-            logger.debug(f"Chunk nr: {chunk_number}; Total nr of processed elements: {obj_count}")
+
+            total_processed_elements += total_chunk_elements
+            logger.debug(f"{chunk_number}: {total_processed_elements}/{self.query_info["chunking_info"]["total_elements"]}")
             chunk_number += 1
 
         return return_object

84700723c20bf23ae4a50afdae1514dfd9d5bca2
Robin-Smets <robinsmets@outlook.de>
2025-06-20 16:54:59 +0200

    issue 3355 - implemented BuildRuleTree_WithArraysFromSetUp_CreatesCorrectStructure

--- roles/tests-unit/files/FWO.Test/RuleTreeBuilderTest.cs
+++ roles/tests-unit/files/FWO.Test/RuleTreeBuilderTest.cs
@@ -13,6 +13,7 @@ namespace FWO.Test
         private RuleTreeBuilder _ruleTreeBuilder = default!;
         private RulebaseLink[] _rulebaseLinks = default!;
         private RulebaseReport[] _rulebases = default!;
+        private RuleTreeItem _control_tree = default!;
 
         [OneTimeSetUp]
         public void SetUpTestClass()
@@ -26,7 +27,7 @@ namespace FWO.Test
                     IsInitial = true,
                     IsGlobal = false,
                     IsSection = false
-                }                
+                }
             ];
 
             _rulebases =
@@ -41,8 +42,43 @@ namespace FWO.Test
                         new Rule { Id = 2, Uid = "rule-1.2", RulebaseId = 1 },
                         new Rule { Id = 3, Uid = "rule-1.3", RulebaseId = 1 }
                     }
-                }              
+                }
             ];
+
+            _control_tree = new RuleTreeItem
+            {
+                IsRoot = true,
+                Children = new List<Basics.ITreeItem<Rule>>
+                {
+                    new RuleTreeItem
+                    {
+                        Header = "Ordered Layer",
+                        Children = new List<Basics.ITreeItem<Rule>>
+                        {
+                            new RuleTreeItem
+                            {
+                                Identifier = "Rule (ID/UID): 1/rule-1.1",
+                                Data = new Rule { Id = 1, Uid = "rule-1.1", RulebaseId = 1 },
+                                Position = new List<int>{1,1}
+                            },
+                            new RuleTreeItem
+                            {
+                                Identifier = "Rule (ID/UID): 2/rule-1.2",
+                                Data = new Rule { Id = 2, Uid = "rule-1.2", RulebaseId = 1 },
+                                Position = new List<int>{1,2}
+                            },
+                            new RuleTreeItem
+                            {
+                                Identifier = "Rule (ID/UID): 3/rule-1.3",
+                                Data = new Rule { Id = 3, Uid = "rule-1.3", RulebaseId = 1 },
+                                Position = new List<int>{1,3}
+                            }
+                        }
+                    }
+                }
+            };
+
+            
 
 
         }
@@ -68,8 +104,8 @@ namespace FWO.Test
 
             Assert.That(queue is null);
         }
-        
-                [Test]
+
+        [Test]
         public void BuildRulebaseLinkQueue_WithArraysFromSetUp_ReturnsQueue()
         {
             // Arrange
@@ -81,8 +117,20 @@ namespace FWO.Test
             queue = _ruleTreeBuilder.BuildRulebaseLinkQueue(_rulebaseLinks, _rulebases);
 
             // Assert
-            
+
             Assert.That(queue is Queue<(RulebaseLink, RulebaseReport)>);
+        }
+        
+        [Test]
+        public void BuildRuleTree_WithArraysFromSetUp_CreatesCorrectStructure()
+        {
+            // Act
+
+            _ruleTreeBuilder.BuildRulebaseLinkQueue(_rulebaseLinks, _rulebases);
+            _ruleTreeBuilder.BuildRuleTree();
+
+            // Assert
+            Assert.That(_ruleTreeBuilder.RuleTree.ToJson(), Is.EqualTo(_control_tree.ToJson()));
         }
 
     }

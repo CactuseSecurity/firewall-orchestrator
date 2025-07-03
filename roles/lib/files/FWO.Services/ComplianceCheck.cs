777643213779e2291f2bb62f21a9a211f353d031
abarz722 <achim.barz@gmx.de>
2025-07-03 16:25:37 +0200

    Compliance Check: Scheduler, settings, move razor code

---
+++ roles/lib/files/FWO.Services/ComplianceCheck.cs
@@ -0,0 +1,139 @@
+﻿using FWO.Basics;
+using FWO.Config.Api;
+using FWO.Data;
+using NetTools;
+
+
+namespace FWO.Services
+{
+    public class ComplianceCheck(UserConfig userConfig)
+    {
+		ComplianceNetworkZone[] NetworkZones = [];
+
+		public async Task CheckAll()
+		{
+
+		}
+
+		public List<(ComplianceNetworkZone, ComplianceNetworkZone)> CheckIpRangeInputCompliance(IPAddressRange? sourceIpRange, IPAddressRange? destinationIpRange, ComplianceNetworkZone[] networkZones)
+		{
+			NetworkZones = networkZones;
+			List<(ComplianceNetworkZone, ComplianceNetworkZone)> forbiddenCommunicationsOutput = [];
+			if (sourceIpRange != null && destinationIpRange != null)
+			{
+				CheckCompliance
+				(
+					[sourceIpRange],
+					[destinationIpRange],
+					out forbiddenCommunicationsOutput
+				);
+			}
+			return forbiddenCommunicationsOutput;
+		}
+
+		public bool CheckRuleCompliance(Rule rule, out List<(ComplianceNetworkZone, ComplianceNetworkZone)> forbiddenCommunication)
+		{
+			List<IPAddressRange> froms = [];
+			List<IPAddressRange> tos = [];
+
+			foreach (NetworkLocation networkLocation in rule.Froms)
+			{
+				// Determine all source ip ranges
+				froms.AddRange(ParseIpRange(networkLocation.Object));
+			}
+			foreach (NetworkLocation networkLocation in rule.Tos)
+			{
+				// Determine all destination ip ranges
+				tos.AddRange(ParseIpRange(networkLocation.Object));
+			}
+
+			return CheckCompliance(froms, tos, out forbiddenCommunication);
+		}
+
+		private bool CheckCompliance(List<IPAddressRange> source, List<IPAddressRange> destination, out List<(ComplianceNetworkZone, ComplianceNetworkZone)> forbiddenCommunication)
+		{
+			// Determine all matching source zones
+			List<ComplianceNetworkZone> sourceZones = DetermineZones(source);
+
+			// Determine all macthing destination zones
+			List<ComplianceNetworkZone> destinationZones = DetermineZones(destination);
+
+			forbiddenCommunication = [];
+
+			foreach (ComplianceNetworkZone sourceZone in sourceZones)
+			{
+				foreach (ComplianceNetworkZone destinationZone in destinationZones)
+				{
+					if (!sourceZone.CommunicationAllowedTo(destinationZone))
+					{
+						forbiddenCommunication.Add((sourceZone, destinationZone));
+					}
+				}
+			}
+
+			return forbiddenCommunication.Count == 0;
+		}
+
+
+		private List<ComplianceNetworkZone> DetermineZones(List<IPAddressRange> ranges)
+		{
+			List<ComplianceNetworkZone> result = [];
+			List<List<IPAddressRange>> unseenIpAddressRanges = [];
+
+			for (int i = 0; i < ranges.Count; i++)
+			{
+				unseenIpAddressRanges.Add(
+                [
+                    new(ranges[i].Begin, ranges[i].End)
+				]);
+			}
+
+			foreach (ComplianceNetworkZone zone in NetworkZones.Where(z => z.OverlapExists(ranges, unseenIpAddressRanges)))
+			{
+				result.Add(zone);
+			}
+
+			// Get ip ranges that are not in any zone
+			List<IPAddressRange> undefinedIpRanges = [.. unseenIpAddressRanges.SelectMany(x => x)];
+			if (undefinedIpRanges.Count > 0)
+			{
+				result.Add
+				(
+					new ComplianceNetworkZone()
+					{
+						Name = userConfig.GetText("internet_local_zone"),
+					}
+				);
+			}
+
+			return result;
+		}
+
+		private static List<IPAddressRange> ParseIpRange(NetworkObject networkObject)
+		{
+			List<IPAddressRange> ranges = [];
+
+			if (networkObject.Type == new NetworkObjectType() { Name = ObjectType.IPRange })
+			{
+				ranges.Add(IPAddressRange.Parse($"{networkObject.IP}-{networkObject.IpEnd}"));
+			}
+			else if (networkObject.Type != new NetworkObjectType() { Name = ObjectType.Group })
+			{
+				for (int j = 0; j < networkObject.ObjectGroupFlats.Length; j++)
+				{
+					if (networkObject.ObjectGroupFlats[j].Object != null)
+					{
+						ranges.AddRange(ParseIpRange(networkObject.ObjectGroupFlats[j].Object!));
+					}
+				}
+			}
+			else
+			{
+				// CIDR notation or single (host) IP can be parsed directly
+				ranges.Add(IPAddressRange.Parse(networkObject.IP));
+			}
+
+			return ranges;
+		}
+    }
+}

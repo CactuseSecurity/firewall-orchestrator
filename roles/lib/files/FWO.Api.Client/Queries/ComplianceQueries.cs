a864a7e3429f9ddbe63e1c02f675b62bfde97969
Robin-Smets <robinsmets@outlook.de>
2025-07-21 11:30:25 +0200

    issue 3454 - add get and add one api calls for policy and criteria

--- roles/lib/files/FWO.Api.Client/Queries/ComplianceQueries.cs
+++ roles/lib/files/FWO.Api.Client/Queries/ComplianceQueries.cs
@@ -4,11 +4,17 @@ namespace FWO.Api.Client.Queries
 {
     public class ComplianceQueries : Queries
     {
+        public static readonly string getCriteria;
+
         public static readonly string addNetworkZone;
         public static readonly string deleteNetworkZone;
         public static readonly string getNetworkZones;
         public static readonly string updateNetworkZones;
+
         public static readonly string modifyNetworkZoneCommunication;
+
+        public static readonly string getPolicies;
+
         public static readonly string addViolations;
         public static readonly string getViolations;
 
@@ -16,11 +22,17 @@ namespace FWO.Api.Client.Queries
         {
             try
             {
+                getCriteria = File.ReadAllText(QueryPath + "compliance/getCriteria.graphql");
+
                 addNetworkZone = File.ReadAllText(QueryPath + "compliance/addNetworkZone.graphql");
                 deleteNetworkZone = File.ReadAllText(QueryPath + "compliance/deleteNetworkZone.graphql");
                 getNetworkZones = File.ReadAllText(QueryPath + "compliance/getNetworkZones.graphql");
                 updateNetworkZones = File.ReadAllText(QueryPath + "compliance/updateNetworkZone.graphql");
+
                 modifyNetworkZoneCommunication = File.ReadAllText(QueryPath + "compliance/updateNetworkZoneCommunication.graphql");
+
+                getPolicies = File.ReadAllText(QueryPath + "compliance/getPolicies.graphql");
+                
                 addViolations = File.ReadAllText(QueryPath + "compliance/addViolations.graphql");
                 getViolations = File.ReadAllText(QueryPath + "compliance/getViolations.graphql");
             }

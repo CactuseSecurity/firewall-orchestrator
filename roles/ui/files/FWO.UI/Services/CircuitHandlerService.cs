a4b7b13f4e13888aa03af206626c76bf83cd3e2c
Administrator <Administrator@WIN-QSHSLKQBFPH>
2025-08-18 12:39:41 +0200

    [+] Neue Felder und Methoden zur Behandlung von Benutzer-Sitzungsschließungen. [+] Erstelle `UserSessionClosedEvent` in `UserSessionClosedEvent.cs`. [+] Erstelle `UserSessionClosedEventArgs` in `UserSessionClosedEventArgs.cs`. [+] Integriere `IEventMediator` in `MainLayout.razor`. [+] Abonniere `UserSessionClosedEvent` und implementiere `OnUserSessionClosedEvent`. [+] Aktualisiere `Dispose`-Methode in `MainLayout.razor`.

--- roles/ui/files/FWO.UI/Services/CircuitHandlerService.cs
+++ roles/ui/files/FWO.UI/Services/CircuitHandlerService.cs
@@ -1,25 +1,33 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Server.Circuits;
 using FWO.Logging;
-using FWO.Basics;
 using FWO.Data;
-using System.Collections.Concurrent;
+using FWO.Services.EventMediator.Interfaces;
+using FWO.Services.EventMediator.Events;
 
 namespace FWO.Ui.Services
 {
-    public class CircuitHandlerService : CircuitHandler
+    public class CircuitHandlerService(IEventMediator eventMediator) : CircuitHandler
     {
         public UiUser? User { get; set; }
+
+        private readonly UserSessionClosedEvent OnUserSessionClosed = new();
 
         public override Task OnCircuitClosedAsync(Circuit circuit, CancellationToken cancellationToken)
         {
             if (User != null)
             {
                 Log.WriteAudit($"Session of \"{User.Name}\" closed", $"Session of user \"{User.Name}\" (last logged in) with DN: \"{User.Dn}\" was closed.");
+
+                OnUserSessionClosed.EventArgs = new UserSessionClosedEventArgs
+                {
+                    UserDn = User.Dn,
+                    UserName = User.Name,
+                };
+
+                eventMediator.Publish(nameof(CircuitHandlerService), OnUserSessionClosed);
             }
+
             return base.OnCircuitClosedAsync(circuit, cancellationToken);
         }
-	}
+    }
 }

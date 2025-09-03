ef88b7f154128b0e8dec9aae130279c28be2dcbe
abarz722 <achim.barz@gmx.de>
2025-09-03 19:03:22 +0200

    fix some review issues

--- roles/lib/files/FWO.Services/ModellingVarianceAnalysis.cs
+++ roles/lib/files/FWO.Services/ModellingVarianceAnalysis.cs
@@ -88,11 +88,12 @@ namespace FWO.Services
             return true;
         }
 
-        public async Task<ModellingVarianceResult> AnalyseRulesVsModelledConnections(List<ModellingConnection> connections, ModellingFilter modellingFilter, bool fullAnalysis = true)
+        public async Task<ModellingVarianceResult> AnalyseRulesVsModelledConnections(List<ModellingConnection> connections,
+            ModellingFilter modellingFilter, bool fullAnalysis = true, bool ignoreGroups = false)
         {
             await InitManagements();
             varianceResult = new() { Managements = RelevantManagements };
-            if(ruleRecognitionOption.NwSeparateGroupAnalysis && fullAnalysis)
+            if(ruleRecognitionOption.NwSeparateGroupAnalysis && fullAnalysis && !ignoreGroups)
             {
                 await GetNwObjectsProductionState();
                 PreAnalyseAllAppRoles(connections);

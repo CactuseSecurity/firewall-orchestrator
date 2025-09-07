950ceed6623e3f21b6be46e9d450f1489085a5e8
Tim Purschke <tmp@cactus.de>
2025-09-07 17:31:15 +0200

    sonar fix complexity

--- roles/importer/files/importer/fortiadom5ff/fmgr_getter.py
+++ roles/importer/files/importer/fortiadom5ff/fmgr_getter.py
@@ -106,6 +106,12 @@ def update_config_with_fortinet_api_call(config_json, sid, api_base_url, api_pat
         if len(result)<limit:
             returned_new_objects = False
 
+    parse_special_fortinet_api_results(result_name, full_result)
+
+    config_json.append({'type': result_name, 'data': full_result})
+
+
+def parse_special_fortinet_api_results(result_name, full_result):
     if result_name == 'nw_obj_global_firewall/internet-service-basic':
         if len(full_result)>0 and 'response' in full_result[0] and 'results' in full_result[0]['response']: 
             full_result = full_result[0]['response']['results']
@@ -113,7 +119,6 @@ def update_config_with_fortinet_api_call(config_json, sid, api_base_url, api_pat
             logger = getFwoLogger()
             logger.warning(f"did not get expected results for {result_name} - setting to empty list")
             full_result = []
-    config_json.append({'type': result_name, 'data': full_result})
 
 
 def fortinet_api_call(sid, api_base_url, api_path, payload={}, method='get'):

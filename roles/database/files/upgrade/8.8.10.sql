836094d61cd25e2db4608f11290d8f06cda66ab9
abarz722 <achim.barz@gmx.de>
2025-09-08 09:02:26 +0200

    add constraint + full text filter

--- roles/database/files/upgrade/8.8.10.sql
+++ roles/database/files/upgrade/8.8.10.sql
@@ -1,1 +1,4 @@
 alter table report add column if not exists read_only Boolean default FALSE;
+
+alter table owner_recertification drop constraint if exists owner_recertification_owner_foreign_key;
+ALTER TABLE owner_recertification ADD CONSTRAINT owner_recertification_owner_foreign_key FOREIGN KEY (owner_id) REFERENCES owner(id) ON UPDATE RESTRICT ON DELETE CASCADE;

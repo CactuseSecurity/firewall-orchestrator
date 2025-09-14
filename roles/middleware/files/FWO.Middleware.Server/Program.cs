9cbdaa4b75201e99f746d30718639b4436ace794
Elias Kolbenschlag <ekolbenschlag@online.de>
2025-09-14 16:42:45 +0200

    #3544 Fixed warnings in Program.cs
    Recommendation is to use top level rout registration in projects for .NET 6+, but if a custom PipelineStructure or other explicit ordering is necessary. otherwise replace lines 170-173 with app.MapControllers()

--- roles/middleware/files/FWO.Middleware.Server/Program.cs
+++ roles/middleware/files/FWO.Middleware.Server/Program.cs
@@ -166,9 +166,11 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+#pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 
 app.Run();

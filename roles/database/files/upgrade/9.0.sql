94ad77f14b35d5b1430fbe9330559771142c13f3
Yannik Hayn <yannik@hayn.de>
2025-09-29 17:37:17 +0200

    feat(db): add cisco asa dev typ

--- roles/database/files/upgrade/9.0.sql
+++ roles/database/files/upgrade/9.0.sql
@@ -1514,3 +1514,6 @@ ON CONFLICT (config_key, config_user) DO NOTHING;
 -- ALTER TABLE "rule_from" DROP COLUMN IF EXISTS "rf_last_seen";
 -- ALTER TABLE "rule_to" DROP COLUMN IF EXISTS "rt_last_seen";
 -- ALTER TABLE "rule_service" DROP COLUMN IF EXISTS "rs_last_seen";
+
+insert into stm_dev_typ (dev_typ_id,dev_typ_name,dev_typ_version,dev_typ_manufacturer,dev_typ_predef_svc,dev_typ_is_multi_mgmt,dev_typ_is_mgmt,is_pure_routing_device)
+    VALUES (28,'Cisco Asa','9','Cisco','',false,true,false);

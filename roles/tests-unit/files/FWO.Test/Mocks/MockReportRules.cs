9ac0b55efe72933f49227fb64227073cb13960b5
Robin-Smets <robinsmets@outlook.de>
2025-10-21 09:04:10 +0200

    issue 3786 - implemented MockReportRules

--- roles/tests-unit/files/FWO.Test/Mocks/MockReportRules.cs
+++ roles/tests-unit/files/FWO.Test/Mocks/MockReportRules.cs
@@ -1,115 +1,57 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
-using FWO.Services.RuleTreeBuilder;
+using FWO.Api.Client;
+using FWO.Basics;
+using FWO.Config.Api;
 using FWO.Data;
 using FWO.Data.Report;
-using FWO.Test.Mocks;
+using FWO.Report;
+using FWO.Report.Filter;
+using FWO.Services.RuleTreeBuilder;
 
-namespace FWO.Test
+namespace FWO.Test.Mocks
 {
-    [TestFixture]
-    internal class RuleTreeBuilderTest
+    public class MockReportRules : ReportRules
     {
-        private RuleTreeBuilder _ruleTreeBuilder = default!;
-        private MockReportRules _reportRules = default!;
-        private RulebaseLink[] _rulebaseLinks = default!;
-        private RulebaseReport[] _rulebases = default!;
-        private RuleTreeItem _control_tree = default!;
+        private int _rulebaseId = 0;
+        private int _ruleId = 0;
 
-        [OneTimeSetUp]
-        public void SetUpTestClass()
+        public RuleTreeItem ControlTree = new();
+
+        public MockReportRules(DynGraphqlQuery query, UserConfig userConfig, ReportType reportType, Func<List<ManagementReport>>? setupFunc = null) : base(query, userConfig, reportType)
         {
-            _rulebaseLinks =
-            [
-                new RulebaseLink
-                {
-                    NextRulebaseId = 1,
-                    LinkType = 2,
-                    IsInitial = true
-                },
-                new RulebaseLink
-                {
-                    NextRulebaseId = 2,
-                    LinkType = 2
-                },
-                new RulebaseLink
-                {
-                    FromRulebaseId = 2,
-                    NextRulebaseId = 3,
-                    LinkType = 4,
-                    IsSection = true
-                },
-                new RulebaseLink
-                {
-                    FromRulebaseId = 3,
-                    NextRulebaseId = 4,
-                    LinkType = 4,
-                    IsSection = true
-                },
-                new RulebaseLink
-                {
-                    FromRuleId = 8,
-                    FromRulebaseId = 4,
-                    NextRulebaseId = 5,
-                    LinkType = 3
-                }
-            ];
+            if (setupFunc == null)
+            {
+                setupFunc = SetupSingleManagementReportEmpty;
+            }
 
-            _rulebases =
-            [
-                new RulebaseReport
-                {
-                    Id = 1,
-                    Name = "Ordered layer with rules",
-                    Rules = new[]
-                    {
-                        new Rule { Id = 1, Uid = "rule-1.1", RulebaseId = 1 },
-                        new Rule { Id = 2, Uid = "rule-1.2", RulebaseId = 1 },
-                        new Rule { Id = 3, Uid = "rule-1.3", RulebaseId = 1 }
-                    }
-                },
-                new RulebaseReport
-                {
-                    Id = 2,
-                    Name = "Ordered layer with sections",
-                },
-                new RulebaseReport
-                {
-                    Id = 3,
-                    Name = "First section in ordered layer",
-                    Rules = new[]
-                    {
-                        new Rule { Id = 4, Uid = "rule-2.1", RulebaseId = 3 },
-                        new Rule { Id = 5, Uid = "rule-2.2", RulebaseId = 3 },
-                        new Rule { Id = 6, Uid = "rule-2.3", RulebaseId = 3 }
-                    }
-                },
-                new RulebaseReport
-                {
-                    Id = 4,
-                    Name = "Section with inline layer",
-                    Rules = new[]
-                    {
-                        new Rule { Id = 7, Uid = "rule-2.4", RulebaseId = 4 },
-                        new Rule { Id = 8, Uid = "rule-2.5", RulebaseId = 4 },
-                        new Rule { Id = 9, Uid = "rule-2.6", RulebaseId = 4 }
-                    }
-                },
-                new RulebaseReport
-                {
-                    Id = 5,
-                    Name = "Inline layer",
-                    Rules = new[]
-                    {
-                        new Rule { Id = 10, Uid = "rule-2.5.1", RulebaseId = 5 },
-                        new Rule { Id = 11, Uid = "rule-2.5.2", RulebaseId = 5 },
-                        new Rule { Id = 12, Uid = "rule-2.5.3", RulebaseId = 5 }
-                    }
-                }
-            ];
+            List<ManagementReport> managementReports = setupFunc();
 
-            _control_tree = new RuleTreeItem
+            foreach (var managementReport in managementReports)
+            {
+                ReportData.ManagementData.Add(managementReport);
+            }
+
+        }
+
+        public List<ManagementReport> SetupSingleManagementReportEmpty()
+        {
+            List<ManagementReport> reports = new()
+            {
+                new ManagementReport
+                {
+                    Rulebases = [],
+                    Devices =
+                    [
+                        CreateDeviceReport()
+                    ]
+                }
+            };
+
+            return reports;
+        }
+
+        public List<ManagementReport> SetupSingleManagementReportBasic()
+        {
+            ControlTree = new RuleTreeItem
             {
                 IsRoot = true,
                 Children = new List<Basics.ITreeItem<Rule>>
@@ -220,58 +162,114 @@ namespace FWO.Test
                     }
                 }
             };
+
+            return new()
+            {
+                new ManagementReport
+                {
+                    Rulebases =
+                    [
+                        CreateRulebaseReport(rulebaseName: "Ordered layer with rules", numberOfRules: 3),
+                        CreateRulebaseReport(rulebaseName: "Ordered layer with sections", numberOfRules: 0),
+                        CreateRulebaseReport(rulebaseName: "First section in ordered layer", numberOfRules: 3),
+                        CreateRulebaseReport(rulebaseName: "Section with inline layer", numberOfRules: 3),
+                        CreateRulebaseReport(rulebaseName: "Inline layer", numberOfRules: 3)
+                    ],
+                    Devices =
+                    [
+                        CreateDeviceReport(rulebaseLinks:
+                        [
+                            new RulebaseLink
+                            {
+                                NextRulebaseId = 1,
+                                LinkType = 2,
+                                IsInitial = true
+                            },
+                            new RulebaseLink
+                            {
+                                NextRulebaseId = 2,
+                                LinkType = 2
+                            },
+                            new RulebaseLink
+                            {
+                                FromRulebaseId = 2,
+                                NextRulebaseId = 3,
+                                LinkType = 4,
+                                IsSection = true
+                            },
+                            new RulebaseLink
+                            {
+                                FromRulebaseId = 3,
+                                NextRulebaseId = 4,
+                                LinkType = 4,
+                                IsSection = true
+                            },
+                            new RulebaseLink
+                            {
+                                FromRuleId = 8,
+                                FromRulebaseId = 4,
+                                NextRulebaseId = 5,
+                                LinkType = 3
+                            }
+                        ])
+                    ]
+                }
+            };
         }
 
-        [SetUp]
-        public void SetUpTestMethod()
+        private RulebaseReport CreateRulebaseReport(string rulebaseName = "", int numberOfRules = 0)
         {
-            _ruleTreeBuilder = new RuleTreeBuilder();
-            _reportRules = new MockReportRules(new(""), new(), Basics.ReportType.Rules);
-            _rulebaseLinks = _reportRules.ReportData.ManagementData.First().Devices.First().RulebaseLinks;
-            _rulebases = _reportRules.ReportData.ManagementData.First().Rulebases;
-        }
+            _rulebaseId++;
 
-        [Test]
-        public void BuildRulebaseLinkQueue_WithEmptyArraysAsArgs_ReturnsNull()
-        {
-            // Arrange
+            if (rulebaseName == "")
+            {
+                rulebaseName = $"Mock Rulebase {_rulebaseId}";
+            }
 
-            Queue<(RulebaseLink, RulebaseReport)>? queue;
+            List<Rule> rules = new();
 
-            // Act
+            if (numberOfRules > 0)
+            {
+                for (int i = 1; i <= numberOfRules; i++)
+                {
+                    _ruleId++;
+                    rules.Add(new Rule
+                    {
+                        Id = _ruleId,
+                        Uid = $"rule-{_rulebaseId}.{_ruleId}",
+                        RulebaseId = _rulebaseId,
+                        Name = $"Mock Rule {_ruleId}"
+                    });
+                }
+            }
 
-            queue = _ruleTreeBuilder.BuildRulebaseLinkQueue(_rulebaseLinks, _rulebases);
-
-            // Assert
-
-            Assert.That(queue is null);
-        }
-
-        [Test]
-        public void BuildRulebaseLinkQueue_BasicSetup_Succeeds()
-        {
-            // Arrange
-
-            Queue<(RulebaseLink, RulebaseReport)>? queue;
-            SetUpTestBySetupFunc()
-
-            // Act
-
-            queue = _ruleTreeBuilder.BuildRulebaseLinkQueue(_rulebaseLinks, _rulebases);
-            _ruleTreeBuilder.BuildRuleTree();
-
-            // Assert
-            Assert.That(_ruleTreeBuilder.RuleTree.ToJson(), Is.EqualTo(_reportRules.ControlTree.ToJson()));
-        }
-
-        private void SetUpTestBySetupFunc(Func<List<ManagementReport>> setupFunc)
-        {
-            _reportRules = new MockReportRules(new(""), new(), Basics.ReportType.Rules, setupFunc);
-            _rulebaseLinks = _reportRules.ReportData.ManagementData.First().Devices.First().RulebaseLinks;
-            _rulebases = _reportRules.ReportData.ManagementData.First().Rulebases;
+            return new RulebaseReport
+            {
+                Id = _rulebaseId,
+                Name = rulebaseName,
+                Rules = rules.ToArray()
+            };
         }
         
+        private DeviceReport CreateDeviceReport(int deviceId = 0, string deviceName = "", List<RulebaseLink>? rulebaseLinks = null)
+        {
+            if (deviceId == 0)
+            {
+                deviceId = 1;
+            }
 
+            if (deviceName == "")
+            {
+                deviceName = $"Mock Device {deviceId}";
+            }
 
+            return new DeviceReport
+            {
+                Id = deviceId,
+                Uid = $"device-{deviceId}",
+                Name = deviceName,
+                RulebaseLinks = rulebaseLinks?.ToArray() ?? []
+            };
+        }
     }
 }

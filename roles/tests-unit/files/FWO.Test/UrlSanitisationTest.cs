d6d26b4b2565d80bdbb306be730069d333b34d56
Tim Purschke <tmp@cactus.de>
2025-10-29 07:24:34 +0100

    moving tests to namespace FWO.Test

--- roles/tests-unit/files/FWO.Test/UrlSanitisationTest.cs
+++ roles/tests-unit/files/FWO.Test/UrlSanitisationTest.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 
-namespace UrlSanitizerTests
+namespace FWO.Test
 {
     [TestFixture]
     public class UrlSanitizerTests

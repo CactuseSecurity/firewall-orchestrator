34986179e72c36dc4ef46f5be4bda3313c22886e
Yannik Hayn <yannik@hayn.de>
2025-11-05 12:46:17 +0100

    fix(importer): installon None instead of emptystring

--- roles/importer/files/importer/ciscoasa9/asa_rule.py
+++ roles/importer/files/importer/ciscoasa9/asa_rule.py
@@ -131,7 +131,7 @@ def create_rule_from_acl_entry(access_list_name: str, entry: AccessListEntry,
         rule_svc_refs=svc_ref,
         rule_action=RuleAction.ACCEPT if entry.action == "permit" else RuleAction.DROP,
         rule_track=RuleTrack.NONE,
-        rule_installon="", #gateway_uid, TODO: commented out for now to avoid duplication issues
+        rule_installon=None, #gateway_uid, TODO: commented out for now to avoid duplication issues
         rule_time="",
         rule_name=access_list_name,
         rule_uid=rule_uid,

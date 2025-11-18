9960481d9171905edd25b69701ec5f8fdb9df726
Robin-Smets <robinsmets@outlook.de>
2025-11-18 14:04:15 +0100

    implemented ComplianceCheckTestFixture

--- roles/tests-unit/files/FWO.Test/ComplianceCheckTest.cs
+++ roles/tests-unit/files/FWO.Test/ComplianceCheckTest.cs
@@ -1,11 +1,10 @@
-using System.Net;
-using FWO.Api.Client;
 using FWO.Api.Client.Queries;
 using FWO.Basics;
 using FWO.Compliance;
 using FWO.Config.Api;
 using FWO.Data;
 using FWO.Logging;
+using FWO.Test.Fixtures;
 using FWO.Test.Mocks;
 using NetTools;
 using NSubstitute;
@@ -14,7 +13,7 @@ using NUnit.Framework;
 namespace FWO.Test
 {
     [TestFixture]
-    internal class ComplianceCheckTest
+    internal class ComplianceCheckTest : ComplianceCheckTestFixture
     {
         #region Configuration
 
@@ -88,7 +87,7 @@ namespace FWO.Test
 
             if (createPolicy)
             {
-                _policy = CreatePolicy();
+                _policy = CreatePolicy(ForbiddenServiceUid);
                 _complianceCheck.Policy = _policy;
 
                 _apiConnection.AsSub()
@@ -112,7 +111,7 @@ namespace FWO.Test
 
                 _apiConnection.AsSub()
                     .SendQueryAsync<List<Rule>>(RuleQueries.getRulesForSelectedManagements)
-                    .Returns(CreateRulesForComplianceCheckTest(setupNoViolations));
+                    .Returns(CreateRulesForComplianceCheckTest(setupNoViolations, ForbiddenServiceUid));
             }
 
             return Task.CompletedTask;
@@ -337,241 +336,6 @@ namespace FWO.Test
             Assert.That(result.Count == 1);
             Assert.That(result.First().Begin.ToString() == networkObject.IP.StripOffNetmask());
             Assert.That(result.First().End.ToString() == networkObject.IpEnd.StripOffNetmask());
-        }
-
-        #endregion
-
-        #region Test-data-generation
-
-        private List<Rule>[] BuildFixedRuleChunksParallel(int numberOfChunks, int numberOfRulesPerChunk, int startRuleId = 1, int? maxDegreeOfParallelism = null)
-        {
-            if (numberOfChunks <= 0) throw new ArgumentOutOfRangeException(nameof(numberOfChunks));
-            if (numberOfRulesPerChunk < 0) throw new ArgumentOutOfRangeException(nameof(numberOfRulesPerChunk));
-
-            var ruleChunks = new List<Rule>[numberOfChunks];
-
-            Parallel.For(
-                0, numberOfChunks,
-                new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism ?? Environment.ProcessorCount },
-                i =>
-                {
-                    var list = new List<Rule>(numberOfRulesPerChunk);
-                    int baseId = startRuleId + i * numberOfRulesPerChunk;
-
-                    for (int j = 0; j < numberOfRulesPerChunk; j++)
-                    {
-                        list.Add(CreateSimpleRule(baseId + j));
-                    }
-
-                    ruleChunks[i] = list;
-                });
-
-            return ruleChunks;
-        }
-
-        private List<Rule> CreateRulesForComplianceCheckTest(bool allCompliant = false)
-        {
-            if (allCompliant)
-            {
-                return new List<Rule>
-                {
-                    CreateSimpleRule(1),
-                    CreateSimpleRule(2),
-                    CreateSimpleRule(3),
-                    CreateSimpleRule(4),
-                    CreateSimpleRule(5)
-                };
-            }
-            else
-            {
-                Rule ruleNotAssessable = CreateSimpleRule(1);
-                ruleNotAssessable.Froms[0].Object.IP = "0.0.0.0/32";
-                ruleNotAssessable.Froms[0].Object.IpEnd = "255.255.255.255/32";
-
-                Rule ruleMatrixViolation = CreateSimpleRule(2, destinationHigh: true);
-
-                Rule ruleMatrixViolationAutoCalcInternet = CreateSimpleRule(3);
-                ruleMatrixViolationAutoCalcInternet.Froms[0] = new NetworkLocation(
-                    new NetworkUser(),
-                    CreateNetworkObject(3, "source", ObjectType.IPRange) // will be in Internet zone
-                );
-
-                Rule ruleForbiddenService = CreateSimpleRule(4);
-                ruleForbiddenService.Services[0].Content.Uid = ForbiddenServiceUid;
-
-
-                Rule ruleCompliant = CreateSimpleRule(5);
-
-                return new List<Rule>
-                {
-                    ruleNotAssessable,
-                    ruleMatrixViolation,
-                    ruleMatrixViolationAutoCalcInternet,
-                    ruleForbiddenService,
-                    ruleCompliant
-                };
-            }
-        }
-
-        private Rule CreateSimpleRule(int ruleID, bool sourceHigh = false, bool destinationHigh = false)
-        {
-            Rule rule = new Rule
-            {
-                Id = ruleID,
-                Action = "accept",
-                MgmtId = 1
-            };
-
-            NetworkUser user = new();
-            NetworkObject source = CreateNetworkObject(ruleID, "source", ObjectType.IPRange, sourceHigh);
-            NetworkObject destination = CreateNetworkObject(ruleID, "destination", ObjectType.IPRange, destinationHigh);
-            ServiceWrapper service = new();
-            service.Content.Uid = $"service-uid-{ruleID}";
-
-            rule.Froms = [new(user, source)];
-            rule.Tos = [new(user, destination)];
-            rule.Services = [service];
-
-            return rule;
-        }
-
-        private NetworkObject CreateNetworkObject(int ruleId, string uidPrefix, string objectTypeName, bool? highIpRange = null)
-        {
-            NetworkObject networkObject = new();
-
-            if (highIpRange != null)
-            {
-                if (highIpRange.Value)
-                {
-                    networkObject.IP = "193.0.0.0/32";
-                    networkObject.IpEnd = "198.0.0.0/32";
-                }
-                else
-                {
-                    networkObject.IP = "128.0.0.0/32";
-                    networkObject.IpEnd = "168.0.0.0/32";
-                }
-            }
-            else // for setting up objects with IPs that are undefined, but not part of a reserved range
-            {
-                networkObject.IP = "3.0.0.0/32";
-                networkObject.IpEnd = "4.0.0.0/32";
-            }
-
-            networkObject.Name = $"{uidPrefix}-uid-rule{ruleId}";
-            networkObject.Type.Name = objectTypeName;
-
-            return networkObject;
-        }
-
-        private CompliancePolicy CreatePolicy()
-        {
-            CompliancePolicy policy = new();
-            ComplianceCriterionWrapper serviceCriterion = new();
-            serviceCriterion.Content.CriterionType = nameof(CriterionType.ForbiddenService);
-            serviceCriterion.Content.Content = ForbiddenServiceUid;
-            ComplianceCriterionWrapper matrixCriterion = new();
-            matrixCriterion.Content.CriterionType = nameof(CriterionType.Matrix);
-            ComplianceCriterionWrapper assessabilityCriterion = new();
-            assessabilityCriterion.Content.CriterionType = nameof(CriterionType.Assessability);
-            policy.Criteria.AddRange([serviceCriterion, matrixCriterion, assessabilityCriterion]);
-            return policy;
-        }
-
-        private List<ComplianceNetworkZone> CreateNetworkZones(bool createInternetZone, bool createUndefinedInternalZone)
-        {
-            List<ComplianceNetworkZone> networkZones = new()
-            {
-                new()
-                {
-                    Id = 1,
-                    CriterionId = 1,
-                    Name = "128-168 Zone",
-                    IdString = "zone_1",
-                    IPRanges =
-                    [
-                        new IPAddressRange(
-                            IPAddress.Parse("128.0.0.0"),
-                            IPAddress.Parse("168.0.0.0")
-                        )
-                    ]
-                },
-                new()
-                {
-                    Id = 2,
-                    CriterionId = 1,
-                    Name = "193-198 Zone",
-                    IdString = "zone_2",
-                    IPRanges =
-                    [
-                        new IPAddressRange(
-                            IPAddress.Parse("193.0.0.0"),
-                            IPAddress.Parse("198.0.0.0")
-                        )
-                    ]
-                }
-            };
-
-            if (createInternetZone)
-            {
-                networkZones.Add(
-                    new()
-                    {
-                        Id = 3,
-                        CriterionId = 1,
-                        Name = "Auto-calculated Internet Zone",
-                        IdString = "AUTO_CALCULATED_ZONE_INTERNET",
-                        IsAutoCalculatedInternetZone = true,
-                        IPRanges =
-                        [
-                            new IPAddressRange(
-                                IPAddress.Parse("0.0.0.0"),
-                                IPAddress.Parse("9.255.255.255")
-                            ),
-                            new IPAddressRange(
-                                IPAddress.Parse("11.0.0.0"),
-                                IPAddress.Parse("127.255.255.255")
-                            ),
-                            new IPAddressRange(
-                                IPAddress.Parse("168.0.0.1"),
-                                IPAddress.Parse("192.255.255.255")
-                            ),
-                            new IPAddressRange(
-                                IPAddress.Parse("198.0.0.1"),
-                                IPAddress.Parse("255.255.255.255")
-                            )
-                        ]
-                    }
-                ); 
-            }
-
-            if (createUndefinedInternalZone)
-            {
-                networkZones.Add(
-                    new()
-                    {
-                        Id = 4,
-                        CriterionId = 1,
-                        Name = "Auto-calculated Undefined-Interal Zone",
-                        IdString = "AUTO_CALCULATED_ZONE_UNDEFINED_INTERNAL",
-                        IsAutoCalculatedUndefinedInternalZone = true,
-                        IPRanges =
-                        [
-                            new IPAddressRange(
-                                IPAddress.Parse("10.0.0.0"),
-                                IPAddress.Parse("10.255.255.255")
-                            )
-                        ]
-                    }
-                ); 
-            }
-
-            foreach (ComplianceNetworkZone zone in networkZones.Where(zone => !zone.IsAutoCalculatedUndefinedInternalZone).ToList())
-            {
-                zone.AllowedCommunicationDestinations = [zone];
-            }
-
-            return networkZones;
         }
 
         #endregion

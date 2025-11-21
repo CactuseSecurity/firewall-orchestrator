cdc844f8b074dbf6065ed131df9ce52fb821f936
Tim Purschke <tmp@cactus.de>
2025-11-21 17:54:06 +0100

    cleanup area ip import scripts

--- scripts/customizing/area_ip_data_import/convertNwObjDataFromGit.py
+++ scripts/customizing/area_ip_data_import/convertNwObjDataFromGit.py
@@ -16,9 +16,11 @@ import os
 from pathlib import Path
 import git  # apt install python3-git # or: pip install git
 import csv
+import urllib.parse
+
 
 defaultConfigFilename = "/usr/local/fworch/etc/secrets/customizingConfig.json"
-ipamGitRepoTargetDir = "ipamRepo"
+ipamGitRepoTargetDir = "/usr/local/fworch/etc/ipamRepo"
 
 
 def getLogger(debug_level_in=0):
@@ -37,11 +39,11 @@ def getLogger(debug_level_in=0):
     logging.basicConfig(format=logformat, datefmt="%Y-%m-%dT%H:%M:%S%z", level=llevel)
     logger.setLevel(llevel)
 
-    #set log level for noisy requests/connectionpool module to WARNING: 
+    #set log level for noisy requests/connectionpool module to WARNING:
     connection_log = logging.getLogger("urllib3.connectionpool")
     connection_log.setLevel(logging.WARNING)
     connection_log.propagate = True
-    
+
     if debug_level>8:
         logger.debug ("debug_level=" + str(debug_level) )
     return logger
@@ -89,11 +91,11 @@ def extractSocketInfo(asset, services):
 
 
 def generatePublicIPv4NetworksAsInternetArea():
-    internetSubnets = ['0.0.0.0/5', '8.0.0.0/7', '11.0.0.0/8', '12.0.0.0/6', '16.0.0.0/4', '32.0.0.0/3', '64.0.0.0/2', 
-                       '128.0.0.0/3', '160.0.0.0/5', '168.0.0.0/6', '172.0.0.0/12', '172.32.0.0/11', '172.64.0.0/10', 
-                       '172.128.0.0/9', '173.0.0.0/8', '174.0.0.0/7', '176.0.0.0/4', '192.0.0.0/9', '192.128.0.0/11', 
-                       '192.160.0.0/13', '192.169.0.0/16', '192.170.0.0/15', '192.172.0.0/14', '192.176.0.0/12', 
-                       '192.192.0.0/10', '193.0.0.0/8', '194.0.0.0/7', '196.0.0.0/6', '200.0.0.0/5', '208.0.0.0/4', 
+    internetSubnets = ['0.0.0.0/5', '8.0.0.0/7', '11.0.0.0/8', '12.0.0.0/6', '16.0.0.0/4', '32.0.0.0/3', '64.0.0.0/2',
+                       '128.0.0.0/3', '160.0.0.0/5', '168.0.0.0/6', '172.0.0.0/12', '172.32.0.0/11', '172.64.0.0/10',
+                       '172.128.0.0/9', '173.0.0.0/8', '174.0.0.0/7', '176.0.0.0/4', '192.0.0.0/9', '192.128.0.0/11',
+                       '192.160.0.0/13', '192.169.0.0/16', '192.170.0.0/15', '192.172.0.0/14', '192.176.0.0/12',
+                       '192.192.0.0/10', '193.0.0.0/8', '194.0.0.0/7', '196.0.0.0/6', '200.0.0.0/5', '208.0.0.0/4',
                        '224.0.0.0/3']
     internetDicts = []
     for net in internetSubnets:
@@ -101,7 +103,7 @@ def generatePublicIPv4NetworksAsInternetArea():
     return internetDicts
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Read configuration from FW management via API calls')
     parser.add_argument('-c', '--config', default=defaultConfigFilename,
@@ -121,14 +123,14 @@ if __name__ == "__main__":
     ipamGitPassword = readConfig(args.config, ['ipamGitPassword'])[0]
 
     try:
-        # get cmdb repo
+        # get ipam repo
         if os.path.exists(ipamGitRepoTargetDir):
             # If the repository already exists, open it and perform a pull
             repo = git.Repo(ipamGitRepoTargetDir)
             origin = repo.remotes.origin
             origin.pull()
         else:
-            repoUrl = "https://" + ipamGitUser + ":" + ipamGitPassword + "@" + ipamGitRepo
+            repoUrl = "https://" + ipamGitUser + ":" + urllib.parse.quote(ipamGitPassword, safe='') + "@" + ipamGitRepo
             repo = git.Repo.clone_from(repoUrl, ipamGitRepoTargetDir)
     except:
         logger.error("error while trying to access git repo '" + ipamGitRepo + "', exception: " + str(traceback.format_exc()))
@@ -139,62 +141,83 @@ if __name__ == "__main__":
     subnetAr = []
 
     try:
-        with open(subnetDataFilename, 'r') as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                subnetAr.append(row)
+        with open(subnetDataFilename, newline='') as csvFile:
+            reader = csv.reader(csvFile)
+            subnetAr += list(reader)
     except:
-        logger.error("error while trying to read subnet csv file '" + subnetDataFilename + "', exception: " + str(traceback.format_exc()))
+        logger.error("error while trying to read csv file '" + csvFile + "', exception: " + str(traceback.format_exc()))
         sys.exit(1)
+
+    # try:
+    #     with open(subnetDataFilename, 'r') as file:
+    #         csv_reader = csv.DictReader(file)
+    #         for row in csv_reader:
+    #             subnetAr.append(row)
+    # except:
+    #     logger.error("error while trying to read subnet csv file '" + subnetDataFilename + "', exception: " + str(traceback.format_exc()))
+    #     sys.exit(1)
 
     normSubnetData = { "subnets": {}, "zones": {}, "areas": {} }
     snId = 0
 
+    nameColumn = 3
+    ipColumn = 0
+    maskColumn = 1
+    maxColumn = nameColumn
+    line = 0
     for subnet in subnetAr:
+        line += 1
         # ignore all "reserved" subnets whose name starts with "RES"
-        if not subnet['Subnetzname'].startswith('RES'):
-            naId = subnet['Subnetzname'][2:4]
-            subnetIp = subnet['Subnetzadresse']
-            netmask = subnet['Subnetzmaske']
-            cidr = str(ipaddress.ip_network(subnetIp + '/' + netmask))
-            
-            nameParts = subnet['Subnetzname'].split('.')
-            if len(nameParts)>1:
-                zoneName = nameParts[1]
-                if len(nameParts)>=3:
-                    subnetName = nameParts[2]
+        if len(subnet)<maxColumn+1:
+            logger.warning("line " + str(line) + ": ignoring malformed subnet " + str(subnet))
+        else:
+            if not subnet[nameColumn].startswith('RES'):
+                naId = subnet[nameColumn][2:4]
+                subnetIp = subnet[ipColumn]
+                netmask = subnet[maskColumn]
+                try:
+                    cidr = str(ipaddress.ip_network(subnetIp + '/' + netmask))
+                except:
+                    logger.warning('found line with unparsable IP: ' + subnetIp + '/' + netmask)
+                    continue
+
+                nameParts = subnet[nameColumn].split('.')
+                if len(nameParts)>1:
+                    zoneName = nameParts[1]
+                    if len(nameParts)>=3:
+                        subnetName = nameParts[2]
+                    else:
+                        subnetName = ""
                 else:
-                    subnetName = ""
-            else:
-                logger.warning("ignoring malformed network entry for net " + subnet['Subnetzadresse'] + ", subnetname: " + subnet['Subnetzname'])
-                continue
+                    logger.warning("line " + str(line) + ": ignoring malformed network entry for net " + subnet[ipColumn] + ", subnetname: " + subnet[nameColumn])
+                    continue
 
-            zoneNamePartsDots = nameParts[0].split('.')
+                zoneNamePartsDots = nameParts[0].split('.')
 
-            zoneNamePartsUnderscore = zoneNamePartsDots[0].split('_')
-            zoneId = zoneNamePartsUnderscore[0][2:7]
-            areaName = '_'.join(zoneNamePartsUnderscore[1:])
-            normSubnet = {
-                "na-id": naId,
-                "na-name": areaName,
-                "zone-id": zoneId,
-                "zone-name": zoneName,
-                "ip": cidr,
-                "name": subnetName
-            }
-            normSubnetData['subnets'].update({ snId: normSubnet})
-            snId += 1;
+                zoneNamePartsUnderscore = zoneNamePartsDots[0].split('_')
+                zoneId = zoneNamePartsUnderscore[0][2:7]
+                areaName = '_'.join(zoneNamePartsUnderscore[1:])
+                normSubnet = {
+                    "na-id": naId,
+                    "na-name": areaName,
+                    "zone-id": zoneId,
+                    "zone-name": zoneName,
+                    "ip": cidr,
+                    "name": subnetName
+                }
+                normSubnetData['subnets'].update({ snId: normSubnet})
+                snId += 1;
 
-            # filling areas
-            if not naId in normSubnetData['areas']:
-                normSubnetData['areas'].update({ naId: {"area-name": areaName, "area-id": naId, "subnets": [], "zones": [] }})
-            normSubnetData['areas'][naId]['subnets'].append({"ip": cidr, "name": subnetName })
-            normSubnetData['areas'][naId]['zones'].append({"zone-id": zoneId, "zone-name": zoneName })
+                # filling areas
+                if not naId in normSubnetData['areas']:
+                    normSubnetData['areas'].update({ naId: {"area-name": areaName, "area-id": naId, "subnets": [], "zones": [] }})
+                normSubnetData['areas'][naId]['subnets'].append({"ip": cidr, "name": subnetName })
+                normSubnetData['areas'][naId]['zones'].append({"zone-id": zoneId, "zone-name": zoneName })
 
-            # filling zones
-            if not zoneId in normSubnetData['zones']:
-                normSubnetData['zones'].update({ zoneId: { "zone-name": zoneName, "subnets": [] }})
-            normSubnetData['zones'][zoneId]['subnets'].append({"ip": cidr, "name": subnetName })
+                # filling zones
+                if not zoneId in normSubnetData['zones']:
+                    normSubnetData['zones'].update({ zoneId: { "zone-name": zoneName, "subnets": [] }})
+                normSubnetData['zones'][zoneId]['subnets'].append({"ip": cidr, "name": subnetName })
 
     # transform output
     transfSubnetData = { "areas": [] }
@@ -208,7 +231,7 @@ if __name__ == "__main__":
     transfSubnetData['areas'].append( {
         'name': 'Internet',
         'id_string': 'NA00',
-        'subnets': generatePublicIPv4NetworksAsInternetArea() } )        
+        'subnets': generatePublicIPv4NetworksAsInternetArea() } )
     # open: what about ipv6 addresses?
     # open: what about the companies own public ip addresses - should they be excluded here?
 

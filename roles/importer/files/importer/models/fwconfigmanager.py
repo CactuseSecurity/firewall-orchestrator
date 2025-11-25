4c8b617942b7bcbb8b329111e81b95e7c3fad9f6
Lennart Schmidt <lennart.schmidt@gmx.de>
2025-11-25 22:35:46 +0100

    fix: Models

--- roles/importer/files/importer/models/fwconfigmanager.py
+++ roles/importer/files/importer/models/fwconfigmanager.py
@@ -1,17 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from models.fwconfig_normalized import FwConfigNormalized
 
 class FwConfigManager(BaseModel):
-    manager_uid: str
-    manager_name: str
-    is_super_manager: bool = False
-    domain_uid: str
-    domain_name: str
-    sub_manager_ids: list[int] = []
-    configs: list[FwConfigNormalized] = []
-
+    manager_uid: str = Field(description="Unique identifier string of the management", alias="ManagerUid")
+    manager_name: str = Field(description="Name of the management", alias="ManagerName")
+    is_super_manager: bool = Field(description="Indicates if the management is a super manager", alias="IsSuperManager", default=False)
+    domain_uid: str = Field(description="Domain UID", alias="DomainUid")
+    domain_name: str = Field(description="Domain name", alias="DomainName")
+    sub_manager_ids: list[int] = Field(default=[], description="List of sub-manager IDs", alias="SubManagerIds")
+    configs: list[FwConfigNormalized] = Field(default=[], description="List of normalized firewall configurations", alias="Configs")
 
     model_config = {
         "arbitrary_types_allowed": True
-    }
+    } 
     

184cceb5dad2374d02ebcc2745e54942e91b34ff
Lennart Schmidt <lennart.schmidt@gmx.de>
2025-11-25 22:41:44 +0100

    fix: config model

--- roles/importer/files/importer/models/fworch_config.py
+++ roles/importer/files/importer/models/fworch_config.py
@@ -2,11 +2,9 @@
     the configuraton of a firewall orchestrator itself
     as read from the global config file including FWO URI
 """
-from pydantic import Field, BaseModel
 
-
-class FworchConfig(BaseModel):
-    fwo_api_url: str = Field(description="Firewall Orchestrator API URL", alias="FwoApiUrl")
-    fwo_user_mgmt_api_uri: str | None = Field(description="Firewall Orchestrator User Management API URI", alias="FwoUserMgmtApiUri")
-    api_fetch_size: int = Field(description="Number of items to fetch per API call", alias="ApiFetchSize", default=500)
-    importer_password: str | None = Field(description="Password for the importer to authenticate with FWO", alias="ImporterPassword")
+class FworchConfig:
+    fwo_api_url: str
+    fwo_user_mgmt_api_uri: str | None
+    api_fetch_size: int
+    importer_password: str | None

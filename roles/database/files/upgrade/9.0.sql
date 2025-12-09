a42cba871edb7be5f323b817e36880815656cbb1
cd <cd@cactus.de>
2025-12-09 09:29:59 +0100

    delete rule_metadata has NO entrie in rule

--- roles/database/files/upgrade/9.0.sql
+++ roles/database/files/upgrade/9.0.sql
@@ -1895,9 +1895,15 @@ BEGIN
     WHERE r.rule_uid IS NULL;
 
     IF missing_uids IS NOT NULL THEN
-        RAISE EXCEPTION 'Missing rule(s): %', missing_uids;
-    END IF;
-	
+        RAISE NOTICE 'Missing rule(s): %', missing_uids;
+		DELETE FROM rule_metadata
+			WHERE rule_uid IN (
+				SELECT rm.rule_uid
+				FROM rule_metadata rm
+				LEFT JOIN rule r ON rm.rule_uid = r.rule_uid
+				WHERE r.rule_uid IS NULL
+		);		
+    END IF;	
 	
     -- Constraints droppen
     ALTER TABLE rule DROP CONSTRAINT IF EXISTS rule_metadatum;

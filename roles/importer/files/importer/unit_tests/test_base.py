c74572e2e35cc217ab295202f5ce11a47dfafa19
Erik Prescher <prescher-erik@web.de>
2025-12-11 16:05:53 +0100

    chore: more tests

--- roles/importer/files/importer/unit_tests/test_base.py
+++ roles/importer/files/importer/unit_tests/test_base.py
@@ -3,6 +3,7 @@ import unittest.mock
 import pytest
 from fwo_api import FwoApi
 from fwo_api_call import FwoApiCall
+from fwo_exceptions import FwoImporterError
 from model_controllers.fwconfig_import_gateway import FwConfigImportGateway
 from model_controllers.import_state_controller import ImportStateController
 from model_controllers.management_controller import ManagementController
@@ -40,6 +41,7 @@ def fwconfig_import_gateway(
     global_state.previous_global_config = FwConfigNormalized()
 
     service_provider = ServiceProvider()
+    service_provider.reset()
     service_provider.register(Services.GLOBAL_STATE, lambda: global_state, Lifetime.SINGLETON)
     return FwConfigImportGateway()
 
@@ -97,3 +99,50 @@ def test_fwconfig_import_gateway_init(
     assert fwconfig_import_gateway.get_rb_link_controller().rb_links[0].from_rulebase_id == from_rulebase_id
     assert fwconfig_import_gateway.get_rb_link_controller().rb_links[0].to_rulebase_id == to_rulebase_id
     assert fwconfig_import_gateway.get_rb_link_controller().rb_links[0].link_type == link_type
+
+
+def test_fwconfig_import_gateway_init_no_links(
+    fwconfig_import_gateway: FwConfigImportGateway,
+    api_call: FwoApiCall,
+    mocker: MockerFixture,
+):
+    api_call.call = unittest.mock.Mock(return_value={"data": {"rulebase_link": []}})
+
+    get_graphql_code = mocker.patch.object(FwoApi, "get_graphql_code", return_value="lol")
+
+    import_state = fwconfig_import_gateway.get_global_state().import_state.state
+
+    import_state.gateway_map = {}
+    get_graphql_code.assert_not_called()
+
+    fwconfig_import_gateway.update_gateway_diffs()
+    assert fwconfig_import_gateway.get_rb_link_controller().rb_links is not None
+    assert len(fwconfig_import_gateway.get_rb_link_controller().rb_links) == 0
+
+
+def test_fwconfig_import_gateway_init_no_gateway_ids(
+    fwconfig_import_gateway: FwConfigImportGateway,
+    api_call: FwoApiCall,
+    mocker: MockerFixture,
+):
+    api_call.call = unittest.mock.Mock(return_value={"data": {"rulebase_link": []}})
+
+    get_graphql_code = mocker.patch.object(FwoApi, "get_graphql_code", return_value="lol")
+
+    import_state = fwconfig_import_gateway.get_global_state().import_state.state
+
+    import_state.gateway_map = {1: {}}
+
+    fwconfig_import_gateway.update_gateway_diffs()
+    get_graphql_code.assert_not_called()
+    assert fwconfig_import_gateway.get_rb_link_controller().rb_links is not None
+    assert len(fwconfig_import_gateway.get_rb_link_controller().rb_links) == 0
+
+
+def test_fwconfig_import_gateway_update_rulebase_link_diffs_no_configs(
+    fwconfig_import_gateway: FwConfigImportGateway,
+):
+    fwconfig_import_gateway.get_global_state().normalized_config = None
+    with pytest.raises(FwoImporterError) as excinfo:
+        fwconfig_import_gateway.update_rulebase_link_diffs()
+    assert "normalized_config is None in update_rulebase_link_diffs" in str(excinfo.value)

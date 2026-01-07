51d55a426a6a0fec45ee9a10d34960b6d86e6042
Erik Prescher <prescher-erik@web.de>
2026-01-07 12:34:48 +0100

    wip: all tests implemented, but the rule_numerics are wrong and therefore the tests are failing

--- roles/importer/files/importer/unit_tests/test_fwconfig_import_ruleorder.py
+++ roles/importer/files/importer/unit_tests/test_fwconfig_import_ruleorder.py
@@ -163,8 +163,12 @@ def test_initialize_on_consecutive_insertions(
     global_state: GlobalState,
     rule_order_service: RuleOrderService,
     fwconfig_builder: FwConfigBuilder,
+    config_tuple: tuple[FwConfigNormalized, str],
 ):
     # Arrange
+    config, _ = config_tuple
+    global_state.normalized_config = config
+    global_state.previous_config = copy.deepcopy(config)
 
     assert global_state.normalized_config is not None
     rulebase = global_state.normalized_config.rulebases[0]
@@ -256,15 +260,19 @@ def test_initialize_on_consecutive_insertions(
 
 
 def test_initialize_on_move_across_rulebases(
-    global_state: GlobalState,
     rule_order_service: RuleOrderService,
     fwconfig_builder: FwConfigBuilder,
     fwconfig_import_rule: FwConfigImportRule,
+    config_tuple: tuple[FwConfigNormalized, str],
 ):
     # Arrange
+    config, _ = config_tuple
+    fwconfig_import_rule.normalized_config = config
+
     assert fwconfig_import_rule.normalized_config is not None
     source_rulebase = fwconfig_import_rule.normalized_config.rulebases[0]
     source_rulebase_uids = list(source_rulebase.rules.keys())
+    print("cshitrne", source_rulebase_uids)
     target_rulebase = fwconfig_import_rule.normalized_config.rulebases[1]
     target_rulebase_uids = list(target_rulebase.rules.keys())
 
@@ -290,4 +298,51 @@ def test_initialize_on_move_across_rulebases(
     assert rule is not None
     assert rule.rule_num_numeric == RULE_NUM_NUMERIC_STEPS / 2, (
         f"Moved rule_num_numeric is {fwconfig_import_rule.normalized_config.rulebases[1].rules[deleted_rule.rule_uid].rule_num_numeric}, expected {RULE_NUM_NUMERIC_STEPS / 2}"
+    )
+
+
+def test_update_rulebase_diffs_on_moves_to_beginning_middle_and_end_of_rulebase(
+    rule_order_service: RuleOrderService,
+    fwconfig_import_rule: FwConfigImportRule,
+    config_tuple: tuple[FwConfigNormalized, str],
+):
+    # Arrange
+    config, _ = config_tuple
+    fwconfig_import_rule.normalized_config = config
+    rulebase = fwconfig_import_rule.normalized_config.rulebases[0]
+    rule_uids = list(rulebase.rules.keys())
+
+    beginning_rule_uid = _move_rule_in_config(
+        fwconfig_import_rule.normalized_config, rulebase.uid, 5, 0, rule_uids
+    )  # Move to beginning
+    middle_rule_uid = _move_rule_in_config(
+        fwconfig_import_rule.normalized_config, rulebase.uid, 1, 4, rule_uids
+    )  # Move to middle
+    end_rule_uid = _move_rule_in_config(
+        fwconfig_import_rule.normalized_config, rulebase.uid, 2, 9, rule_uids
+    )  # Move to end
+
+    # Act
+
+    rule_order_service.update_rule_order_diffs()
+
+    # Assert
+    assert beginning_rule_uid is not None
+    beginning_rule = _get_rule(fwconfig_import_rule.normalized_config, 0, beginning_rule_uid)
+    assert beginning_rule is not None
+    assert beginning_rule.rule_num_numeric == RULE_NUM_NUMERIC_STEPS / 2, (
+        f"Beginning moved rule_num_numeric is {fwconfig_import_rule.normalized_config.rulebases[0].rules[beginning_rule_uid].rule_num_numeric}, expected {RULE_NUM_NUMERIC_STEPS / 2}"
+    )
+
+    assert middle_rule_uid is not None
+    middle_rule = _get_rule(fwconfig_import_rule.normalized_config, 0, middle_rule_uid)
+    assert middle_rule is not None
+    assert middle_rule.rule_num_numeric == 4608, (
+        f"Middle moved rule_num_numeric is {fwconfig_import_rule.normalized_config.rulebases[0].rules[middle_rule_uid].rule_num_numeric}, expected 4608"
+    )
+    assert end_rule_uid is not None
+    end_rule = _get_rule(fwconfig_import_rule.normalized_config, 0, end_rule_uid)
+    assert end_rule is not None
+    assert end_rule.rule_num_numeric == 11 * RULE_NUM_NUMERIC_STEPS, (
+        f"End moved rule_num_numeric is {fwconfig_import_rule.normalized_config.rulebases[0].rules[end_rule_uid].rule_num_numeric}, expected {11 * RULE_NUM_NUMERIC_STEPS}"
     )

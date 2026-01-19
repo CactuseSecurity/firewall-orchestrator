ada3c33e60f416eb1bbb96cd0112e1fe654077a5
Erik Prescher <prescher-erik@web.de>
2026-01-19 17:01:01 +0100

    wip: more tests

--- roles/importer/files/importer/unit_tests/test_fwconfig_import_consistency.py
+++ roles/importer/files/importer/unit_tests/test_fwconfig_import_consistency.py
@@ -18,6 +18,7 @@ def fw_config_import_object() -> FwConfigImportObject:
     fw_config_import_object.network_object_type_map = {"network": 1, "group": 2, "host": 3, "machine_range": 4}
     fw_config_import_object.service_object_type_map = {"simple": 1, "group": 2, "rpc": 3}
     fw_config_import_object.user_object_type_map = {"group": 1, "simple": 2}
+    fw_config_import_object.user_object_type_map = {"group": 1, "simple": 2}
     fw_config_import_object.get_user_obj_type_map = unittest.mock.Mock(return_value={"group": 1, "simple": 2})
     return fw_config_import_object
 
@@ -1108,3 +1109,42 @@ class TestCheckConsistencyServiceObjects:
 
         assert len(consistency_checker.issues) == 1
         assert consistency_checker.issues == {"unresolvableSvcObjRefs": ["InvalidService"]}
+
+
+class TestCheckUserObjectConsistency:
+    def test_check_user_object_consistency_valid(
+        self,
+        fwconfig_builder: FwConfigBuilder,
+        import_state_controller: ImportStateController,
+        fw_config_import_object: FwConfigImportObject,
+    ):
+        manager_controller = FwConfigManagerListController()
+        config, _ = fwconfig_builder.build_config(
+            network_object_count=10,
+            service_object_count=10,
+            rulebase_count=3,
+            rules_per_rulebase_count=10,
+            user_object_count=1,
+            user_group_object_count=2,
+            user_group_object_member_count=2,
+        )
+
+        manager = FwConfigManager(
+            manager_uid="mgr1",
+            is_super_manager=True,
+            sub_manager_ids=[],
+            configs=[config],
+            domain_name="",
+            domain_uid="",
+            manager_name="",
+        )
+        manager_controller.add_manager(manager)
+
+        consistency_checker = FwConfigImportCheckConsistency(
+            import_details=import_state_controller,
+            config_list=manager_controller,
+        )
+        consistency_checker.maps = fw_config_import_object
+        consistency_checker.check_user_object_consistency(config=manager_controller)
+
+        assert len(consistency_checker.issues) == 0

e494b5867da9633d10fd8f3c656bc4b281605f2c
Lennart Schmidt <lennart.schmidt@gmx.de>
2026-01-29 16:06:36 +0100

    fix: Remove "secrets"

--- roles/importer/files/importer/test/data/mock_objects.py
+++ roles/importer/files/importer/test/data/mock_objects.py
@@ -20,14 +20,14 @@ class MockObjectsFactory:
                         "hostname": "192.168.10.5",  # Needs to NOT have "://" to trigger decryption logic
                         "ssh_port": 22,
                         # Required for: api_call_result["data"]["management"][0]["import_credential"]["secret"]
-                        "import_credential": {"id": 45, "username": "api_admin", "secret": "encrypted_secret_string"},
+                        "import_credential": {"id": 45, "username": "api_admin", "secret": ""},
                         # Optional: The code iterates this if present
                         "subManagers": [
                             {
                                 "id": 101,
                                 "name": "CMA_London",
                                 "hostname": "192.168.10.6",
-                                "import_credential": {"id": 46, "secret": "encrypted_sub_secret"},
+                                "import_credential": {"id": 46, "secret": ""},
                             }
                         ],
                     }

a924378d3f6ec7f3e0b5db0b9a4fbcd9327ea935
abarz722 <achim.barz@gmx.de>
2026-02-20 13:56:02 +0100

    consolidate alert handling

--- roles/lib/files/FWO.Services/AlertHelper.cs
+++ roles/lib/files/FWO.Services/AlertHelper.cs
@@ -1,21 +1,17 @@
 using FWO.Api.Client;
 using FWO.Api.Client.Queries;
-using FWO.Basics;
 using FWO.Config.Api;
-using FWO.Config.Api.Data;
 using FWO.Data;
 using FWO.Logging;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 
-namespace FWO.Middleware.Server.Jobs
+namespace FWO.Services
 {
-    /// <summary>
-    /// Shared helper functions for Quartz jobs (logging and alerts).
-    /// </summary>
-    internal static class SchedulerJobHelper
+    public static class AlertHelper
     {
-        internal struct AdditionalAlertData
+        public struct AdditionalAlertData
         {
             public int? MgmtId { get; set; }
             public object? JsonData { get; set; }
@@ -23,13 +19,13 @@ namespace FWO.Middleware.Server.Jobs
             public long? RefAlertId { get; set; }
         }
 
-        internal static async Task LogErrorsWithAlert(ApiConnection apiConnection, GlobalConfig globalConfig, int severity, string title, string source, AlertCode alertCode, Exception exc)
+        public static async Task LogErrorsWithAlert(ApiConnection apiConnection, GlobalConfig globalConfig, int severity, string title, string source, AlertCode alertCode, Exception exc)
         {
             try
             {
                 Log.WriteError(title, "Ran into exception: ", exc);
                 string titletext = $"Error encountered while trying {title}";
-                await AddLogEntry(apiConnection, globalConfig, severity, title, globalConfig.GetText("ran_into_exception") + exc.Message, source);
+                await AddLogEntry(apiConnection, severity, title, globalConfig.GetText("ran_into_exception") + exc.Message, source);
                 await SetAlert(apiConnection, title, titletext, source, alertCode, new AdditionalAlertData());
             }
             catch (Exception exception)
@@ -38,7 +34,7 @@ namespace FWO.Middleware.Server.Jobs
             }
         }
 
-        internal static async Task AddLogEntry(ApiConnection apiConnection, GlobalConfig globalConfig, int severity, string cause, string description, string source, int? mgmtId = null)
+        public static async Task AddLogEntry(ApiConnection apiConnection, int severity, string cause, string description, string source, int? mgmtId = null)
         {
             try
             {
@@ -70,8 +66,15 @@ namespace FWO.Middleware.Server.Jobs
             }
         }
 
-        internal static async Task<long?> SetAlert(ApiConnection apiConnection, string title, string description, string source, AlertCode alertCode,
-            AdditionalAlertData additionalAlertData, bool compareDesc = false)
+        public static Task<long?> SetAlert(ApiConnection apiConnection, string title, string description, string source, AlertCode alertCode,
+            AdditionalAlertData additionalAlertData, bool compareDesc = false, bool compareTitle = false, int userId = 0)
+        {
+            return SetAlert(apiConnection, title, description, source, alertCode, additionalAlertData.MgmtId, additionalAlertData.JsonData,
+                additionalAlertData.DevId, additionalAlertData.RefAlertId, compareDesc, compareTitle, userId);
+        }
+
+        public static async Task<long?> SetAlert(ApiConnection apiConnection, string title, string description, string source, AlertCode alertCode,
+            int? mgmtId = null, object? jsonData = null, int? devId = null, long? refAlertId = null, bool compareDesc = false, bool compareTitle = false, int userId = 0)
         {
             long? alertId = null;
             try
@@ -80,49 +83,52 @@ namespace FWO.Middleware.Server.Jobs
                 var Variables = new
                 {
                     source = source,
-                    userId = 0,
+                    userId = userId,
                     title = title,
                     description = description,
-                    mgmId = additionalAlertData.MgmtId,
-                    devId = additionalAlertData.DevId,
+                    mgmId = mgmtId,
+                    devId = devId,
                     alertCode = (int)alertCode,
-                    jsonData = additionalAlertData.JsonData,
-                    refAlert = additionalAlertData.RefAlertId
+                    jsonData = jsonData,
+                    refAlert = refAlertId
                 };
                 ReturnId[]? returnIds = (await apiConnection.SendQueryAsync<ReturnIdWrapper>(MonitorQueries.addAlert, Variables)).ReturnIds;
                 if (returnIds != null)
                 {
+                    // Acknowledge older alert for same problem
                     alertId = returnIds[0].NewIdLong;
-                    Alert? existingAlert = openAlerts.FirstOrDefault(x => x.AlertCode == alertCode &&
-                        (x.ManagementId == additionalAlertData.MgmtId || (x.ManagementId == null && additionalAlertData.MgmtId == null))
-                        && (!compareDesc || x.Description == description));
+                    Alert? existingAlert = openAlerts.FirstOrDefault(x => x.AlertCode == alertCode
+                        && (x.ManagementId == mgmtId || (x.ManagementId == null && mgmtId == null))
+                        && (userId == 0 || x.UserId == userId)
+                        && (!compareDesc || x.Description == description)
+                        && (!compareTitle || x.Title == title));
                     if (existingAlert != null)
                     {
-                        await AcknowledgeAlert(apiConnection, existingAlert.Id);
+                        await AcknowledgeAlert(apiConnection, existingAlert.Id, userId);
                     }
                 }
                 else
                 {
                     Log.WriteError("Write Alert", "Log could not be written to database");
                 }
-                LogAlert(title, description, source, alertCode, additionalAlertData.MgmtId, additionalAlertData.JsonData, additionalAlertData.DevId);
+                LogAlert(title, description, source, alertCode, mgmtId, jsonData, devId);
             }
             catch (Exception exc)
             {
                 Log.WriteError("Write Alert", $"Could not write Alert for {source}: ", exc);
-                LogAlert(title, description, source, alertCode, additionalAlertData.MgmtId, additionalAlertData.JsonData, additionalAlertData.DevId);
+                LogAlert(title, description, source, alertCode, mgmtId, jsonData, devId);
             }
             return alertId;
         }
 
-        private static async Task AcknowledgeAlert(ApiConnection apiConnection, long alertId)
+        public static async Task AcknowledgeAlert(ApiConnection apiConnection, long alertId, int ackUser = 0)
         {
             try
             {
                 var Variables = new
                 {
                     id = alertId,
-                    ackUser = 0,
+                    ackUser = ackUser,
                     ackTime = DateTime.Now
                 };
                 await apiConnection.SendQueryAsync<ReturnId>(MonitorQueries.acknowledgeAlert, Variables);
@@ -133,12 +139,13 @@ namespace FWO.Middleware.Server.Jobs
             }
         }
 
-        private static void LogAlert(string title, string description, string source, AlertCode alertCode, int? mgmtId, object? JsonData, int? devId)
+        private static void LogAlert(string title, string description, string source, AlertCode alertCode, int? mgmtId, object? jsonData, int? devId)
         {
             string? mgmtIdString = mgmtId?.ToString() ?? "";
             string? devIdString = devId?.ToString() ?? "";
-            string jsonString = JsonData != null ? JsonSerializer.Serialize(JsonData) : "";
-            Log.WriteAlert($"source: \"{source}\"", $"userId: \"0\", title: \"{title}\", description: \"{description}\", mgmId: \"{mgmtIdString}\", devId: \"{devIdString}\", jsonData: \"{jsonString}\", alertCode: \"{alertCode}\"");
+            string jsonString = jsonData != null ? JsonSerializer.Serialize(jsonData) : "";
+            Log.WriteAlert($"source: \"{source}\"", $"userId: \"0\", title: \"{title}\", description: \"{description}\", " +
+                $"mgmId: \"{mgmtIdString}\", devId: \"{devIdString}\", jsonData: \"{jsonString}\", alertCode: \"{alertCode}\"");
         }
     }
 }

d90c357258fc905807fd155e0e6045c98765b812
Robin-Smets <136028234+Robin-Smets@users.noreply.github.com>
2026-03-03 08:13:02 +0100

    add expiration time to ComplianceReport (#4317)

--- roles/lib/files/FWO.Data/Rule.cs
+++ roles/lib/files/FWO.Data/Rule.cs
@@ -132,6 +132,9 @@ namespace FWO.Data
         [JsonProperty("rule_time"), JsonPropertyName("rule_time")]
         public string? Time { get; set; }
 
+        [JsonProperty("rule_times"), JsonPropertyName("rule_times")]
+        public List<RuleTime> RuleTimes { get; set; } = [];
+
         [JsonProperty("violations"), JsonPropertyName("violations")]
         public List<ComplianceViolation> Violations { get; set; } = [];
 
@@ -208,6 +211,7 @@ namespace FWO.Data
             EnforcingGateways = rule.EnforcingGateways;
             InstallOn = rule.InstallOn;
             Time = rule.Time;
+            RuleTimes = rule.RuleTimes;
             Violations = rule.Violations;
             Rulebase = rule.Rulebase;
             LastChangeAdmin = rule.LastChangeAdmin;

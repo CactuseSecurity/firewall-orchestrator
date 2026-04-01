f6aa30cb6fe0ce2ef14f438772b44e05918d9a22
Tim Purschke <tmp@cactus.de>
2026-04-01 13:35:40 +0200

    missing tz additions

--- roles/database/files/upgrade/9.0.17.sql
+++ roles/database/files/upgrade/9.0.17.sql
@@ -1,7 +1,7 @@
 ALTER TABLE ext_request
     ADD COLUMN IF NOT EXISTS lock_owner varchar,
-    ADD COLUMN IF NOT EXISTS lock_acquired_at timestamp,
-    ADD COLUMN IF NOT EXISTS lock_expires_at timestamp;
+    ADD COLUMN IF NOT EXISTS lock_acquired_at Timestamp with time zone,
+    ADD COLUMN IF NOT EXISTS lock_expires_at Timestamp with time zone;
 
 UPDATE ext_request
 SET lock_expires_at = NOW() - interval '1 second'

e6d2f3f22dd877d7d6b5677d6e8a3058a4ad8254
Tim Purschke <tmp@cactus.de>
2026-05-18 18:38:54 +0200

    ruff

--- roles/importer/files/importer/fw_modules/fortiadom5ff/fwcommon.py
+++ roles/importer/files/importer/fw_modules/fortiadom5ff/fwcommon.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import TYPE_CHECKING, Any, cast
 
 import fwo_const

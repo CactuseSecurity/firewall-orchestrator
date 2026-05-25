6f1bbf6e34c220867a8b215f6dc640df9979efb8
abarz722 <achim.barz@gmx.de>
2026-05-25 13:53:25 +0200

    sonar fix

--- roles/lib/files/FWO.Data/Workflow/WfReqElement.cs
+++ roles/lib/files/FWO.Data/Workflow/WfReqElement.cs
@@ -17,18 +17,6 @@ namespace FWO.Data.Workflow
         [JsonProperty("device_id"), JsonPropertyName("device_id")]
         public int? DeviceId { get; set; }
 
-        [JsonProperty("flow_nwobj_id"), JsonPropertyName("flow_nwobj_id")]
-        public long? FlowNetworkObjectId { get; set; }
-
-        [JsonProperty("flow_nwgrp_id"), JsonPropertyName("flow_nwgrp_id")]
-        public long? FlowNetworkGroupId { get; set; }
-
-        [JsonProperty("flow_svcobj_id"), JsonPropertyName("flow_svcobj_id")]
-        public long? FlowServiceObjectId { get; set; }
-
-        [JsonProperty("flow_svcgrp_id"), JsonPropertyName("flow_svcgrp_id")]
-        public long? FlowServiceGroupId { get; set; }
-
         public Cidr? Cidr { get; set; } = new();
         public Cidr? CidrEnd { get; set; } = new();
 
@@ -42,10 +30,6 @@ namespace FWO.Data.Workflow
             TaskId = element.TaskId;
             RequestAction = element.RequestAction;
             DeviceId = element.DeviceId;
-            FlowNetworkObjectId = element.FlowNetworkObjectId;
-            FlowNetworkGroupId = element.FlowNetworkGroupId;
-            FlowServiceObjectId = element.FlowServiceObjectId;
-            FlowServiceGroupId = element.FlowServiceGroupId;
             Cidr = element.Cidr;
             CidrEnd = element.CidrEnd;
         }
--- roles/lib/files/FWO.Data/Workflow/WfReqElementWriter.cs
+++ roles/lib/files/FWO.Data/Workflow/WfReqElementWriter.cs
@@ -11,26 +11,10 @@ namespace FWO.Data.Workflow
         [JsonProperty("device_id"), JsonPropertyName("device_id")]
         public int? DeviceId { get; set; }
 
-        [JsonProperty("flow_nwobj_id"), JsonPropertyName("flow_nwobj_id")]
-        public long? FlowNetworkObjectId { get; set; }
-
-        [JsonProperty("flow_nwgrp_id"), JsonPropertyName("flow_nwgrp_id")]
-        public long? FlowNetworkGroupId { get; set; }
-
-        [JsonProperty("flow_svcobj_id"), JsonPropertyName("flow_svcobj_id")]
-        public long? FlowServiceObjectId { get; set; }
-
-        [JsonProperty("flow_svcgrp_id"), JsonPropertyName("flow_svcgrp_id")]
-        public long? FlowServiceGroupId { get; set; }
-
         public WfReqElementWriter(WfReqElement element) : base(element)
         {
             RequestAction = element.RequestAction;
             DeviceId = element.DeviceId;
-            FlowNetworkObjectId = element.FlowNetworkObjectId;
-            FlowNetworkGroupId = element.FlowNetworkGroupId;
-            FlowServiceObjectId = element.FlowServiceObjectId;
-            FlowServiceGroupId = element.FlowServiceGroupId;
             IpString = element.Cidr != null && element.Cidr.Valid ? element.Cidr.CidrString : null;
             IpEnd = element.CidrEnd != null && element.CidrEnd.Valid ? element.CidrEnd.CidrString : null;
         }

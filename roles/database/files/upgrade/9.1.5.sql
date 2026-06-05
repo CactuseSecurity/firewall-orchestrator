15f02403223fb74e89b490209c09ace6f1a5af91
Luca <luca.weidmann@gmx.de>
2026-06-05 14:00:03 +0200

    actual migration script for old config values

--- roles/database/files/upgrade/9.1.5.sql
+++ roles/database/files/upgrade/9.1.5.sql
@@ -1,7 +1,31 @@
-DELETE FROM config
-WHERE config_key IN ('accessTokenLifetimeHours', 'refreshTokenLifetimeDays', 'sessionTimeout', 'sessionTimeoutNoticePeriod');
+INSERT INTO config (config_key, config_value, config_user)
+SELECT 'accessTokenLifetime', config_value, config_user
+FROM config
+WHERE config_key = 'accessTokenLifetimeHours'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO config (config_key, config_value, config_user)
+SELECT 'accessTokenLifetimeUnit', 'Hours', config_user
+FROM config
+WHERE config_key = 'accessTokenLifetimeHours'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO config (config_key, config_value, config_user)
+SELECT 'refreshTokenLifetime', config_value, config_user
+FROM config
+WHERE config_key = 'refreshTokenLifetimeDays'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO config (config_key, config_value, config_user)
+SELECT 'refreshTokenLifetimeUnit', 'Days', config_user
+FROM config
+WHERE config_key = 'refreshTokenLifetimeDays'
+ON CONFLICT DO NOTHING;
 
 INSERT INTO config (config_key, config_value, config_user) VALUES ('accessTokenLifetime', '1', 0) ON CONFLICT DO NOTHING;
 INSERT INTO config (config_key, config_value, config_user) VALUES ('accessTokenLifetimeUnit', 'Hours', 0) ON CONFLICT DO NOTHING;
 INSERT INTO config (config_key, config_value, config_user) VALUES ('refreshTokenLifetime', '1', 0) ON CONFLICT DO NOTHING;
 INSERT INTO config (config_key, config_value, config_user) VALUES ('refreshTokenLifetimeUnit', 'Days', 0) ON CONFLICT DO NOTHING;
+
+DELETE FROM config
+WHERE config_key IN ('accessTokenLifetimeHours', 'refreshTokenLifetimeDays', 'sessionTimeout', 'sessionTimeoutNoticePeriod');

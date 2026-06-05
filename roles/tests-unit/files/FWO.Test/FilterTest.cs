291946e50ee174cb3116f996685d1d146cdba766
cd <cd@cactus.de>
2026-06-05 10:56:40 +0200

    fix: AstNodeFilterDateTimeRange - filter last hit

--- roles/tests-unit/files/FWO.Test/FilterTest.cs
+++ roles/tests-unit/files/FWO.Test/FilterTest.cs
@@ -1009,7 +1009,10 @@ namespace FWO.Test
 
             DynGraphqlQuery query = Compiler.Compile(template);
 
-            StringAssert.Contains("rule: { rule_metadatum:{ rule_last_hit:", query.RuleWhereStatement);
+            StringAssert.Contains("rule: { rule_metadatum: { rule_last_hit:", query.RuleWhereStatement);
+            StringAssert.Contains("ruleByOldRuleId: { rule_metadatum: { rule_last_hit:", query.RuleWhereStatement);
+            StringAssert.DoesNotContain("rule: { rule: {", query.RuleWhereStatement);
+            StringAssert.DoesNotContain("ruleByOldRuleId: { rule: {", query.RuleWhereStatement);
             StringAssert.DoesNotContain("_is_null: true", query.RuleWhereStatement);
         }
 
@@ -1025,9 +1028,12 @@ namespace FWO.Test
 
             DynGraphqlQuery query = Compiler.Compile(template);
 
-            StringAssert.Contains("rule: { rule_metadatum: { rule_last_hit:", query.RuleWhereStatement);
+            StringAssert.Contains("rule: { _or: [", query.RuleWhereStatement);
+            StringAssert.Contains("ruleByOldRuleId: { _or: [", query.RuleWhereStatement);
+            StringAssert.Contains("rule_metadatum: { rule_last_hit:", query.RuleWhereStatement);
             StringAssert.Contains("_is_null: true", query.RuleWhereStatement);
-            StringAssert.Contains("_or:", query.RuleWhereStatement);
+            StringAssert.DoesNotContain("rule: { rule: {", query.RuleWhereStatement);
+            StringAssert.DoesNotContain("ruleByOldRuleId: { rule: {", query.RuleWhereStatement);
         }
 
         [Test]

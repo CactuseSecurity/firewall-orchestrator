a9b510cf04b6c28e16656114c4a44c6473236d3a
Tim Purschke <tmp@cactus.de>
2026-06-13 06:35:34 +0200

    renaming getInitialOwnerRecert

--- roles/tests-unit/files/FWO.Test/AppDataImportTest.cs
+++ roles/tests-unit/files/FWO.Test/AppDataImportTest.cs
@@ -1273,7 +1273,7 @@ namespace FWO.Test
 
             Assert.That(imported, Is.True);
             Assert.That(apiConn.LastNewOwnerRecertActive, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(1));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(1));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(1));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(1));
         }
@@ -1300,7 +1300,7 @@ namespace FWO.Test
             bool imported = await InvokeSaveApp(import, incomingApp, new OwnerChangeImportTracker(apiConn));
 
             Assert.That(imported, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(1));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(1));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(1));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(1));
         }
@@ -1325,7 +1325,7 @@ namespace FWO.Test
 
             Assert.That(imported, Is.True);
             Assert.That(apiConn.LastNewOwnerRecertActive, Is.False);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(0));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(0));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(0));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(0));
         }
@@ -1350,7 +1350,7 @@ namespace FWO.Test
 
             Assert.That(imported, Is.True);
             Assert.That(apiConn.LastNewOwnerRecertActive, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(0));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(0));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(0));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(0));
         }
@@ -1378,7 +1378,7 @@ namespace FWO.Test
 
             Assert.That(imported, Is.True);
             Assert.That(apiConn.LastUpdateOwnerRecertActive, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(1));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(1));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(1));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(1));
         }
@@ -1411,7 +1411,7 @@ namespace FWO.Test
             bool imported = await InvokeSaveApp(import, incomingApp, new OwnerChangeImportTracker(apiConn));
 
             Assert.That(imported, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(1));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(1));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(0));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(0));
         }
@@ -1444,7 +1444,7 @@ namespace FWO.Test
             bool imported = await InvokeSaveApp(import, incomingApp, new OwnerChangeImportTracker(apiConn));
 
             Assert.That(imported, Is.True);
-            Assert.That(apiConn.GetInitialOwnerRecertCalls, Is.EqualTo(1));
+            Assert.That(apiConn.GetOwnerRecertCalls, Is.EqualTo(1));
             Assert.That(apiConn.RecertifyOwnerCalls, Is.EqualTo(0));
             Assert.That(apiConn.SetOwnerLastRecertCalls, Is.EqualTo(0));
         }
@@ -2278,7 +2278,7 @@ namespace FWO.Test
             public Dictionary<string, string>? LastUpdateOwnerAdditionalInfo { get; private set; }
             public bool? LastNewOwnerRecertActive { get; private set; }
             public bool? LastUpdateOwnerRecertActive { get; private set; }
-            public int GetInitialOwnerRecertCalls { get; private set; }
+            public int GetOwnerRecertCalls { get; private set; }
             public int RecertifyOwnerCalls { get; private set; }
             public int SetOwnerLastRecertCalls { get; private set; }
             public List<OwnerRecertification> ExistingInitialOwnerRecerts { get; init; } = [];
@@ -2410,9 +2410,9 @@ namespace FWO.Test
                     });
                 }
 
-                if (query == RecertQueries.getInitialOwnerRecert)
+                if (query == RecertQueries.getOwnerRecert)
                 {
-                    ++GetInitialOwnerRecertCalls;
+                    ++GetOwnerRecertCalls;
                     return Task.FromResult((QueryResponseType)(object)ExistingInitialOwnerRecerts);
                 }
 

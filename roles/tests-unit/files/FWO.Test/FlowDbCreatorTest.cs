4a0885165da433ac68ebbb219a11f2b93311fd76
Yannik Hayn <yannik@hayn.de>
2026-06-16 16:23:30 +0200

    test(flow): fix test

--- roles/tests-unit/files/FWO.Test/FlowDbCreatorTest.cs
+++ roles/tests-unit/files/FWO.Test/FlowDbCreatorTest.cs
@@ -531,7 +531,7 @@ namespace FWO.Test
             apiConn.ExistingAccesses.Add(new FlowAccess
             {
                 Id = 900,
-                Hash = FlowHashGenerator.GenerateAccessHash([sourceHash], [destinationHash], [serviceHash], []) // TODO: include timeObjectHashes
+                Hash = FlowHashGenerator.GenerateAccessHash([sourceHash], [destinationHash], [serviceHash], [], true) // TODO: include timeObjectHashes and allowsTraffic
             });
             FlowDbCreator flowDbCreator = new(apiConn);
             WfReqTask task = CreateAccessTask(11, "10.0.0.1", "10.0.1.1", 443);

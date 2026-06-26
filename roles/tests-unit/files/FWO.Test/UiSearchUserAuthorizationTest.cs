c372ffb825aa15fbce149fb5bee2b39a98ab9dfc
Tim Purschke <tmp@cactus.de>
2026-06-26 18:02:24 +0200

    reverting SearchUser changes around ExecutionMode
    reverting the SearchUser/SelectFromLdap assignment controls back to plain admin-role authorization.
    Changed:
    [SearchUser.razor](/home/tim/dev/fwo-tim/roles/ui/files/FWO.UI/Pages/Settings/SearchUser.razor): removed UseExecutionModeAuthorization, assign buttons use AuthorizeView.
    [SelectFromLdap.razor](/home/tim/dev/fwo-tim/roles/ui/files/FWO.UI/Pages/Settings/SelectFromLdap.razor): LDAP-result assign buttons use AuthorizeView.
    [SettingsRoles.razor](/home/tim/dev/fwo-tim/roles/ui/files/FWO.UI/Pages/Settings/SettingsRoles.razor): removed the now-unneeded parameter.
    [UiSearchUserAuthorizationTest.cs](/home/tim/dev/fwo-tim/roles/tests-unit/files/FWO.Test/UiSearchUserAuthorizationTest.cs): now covers admin in user-role execution mode, non-admin denial, and the SelectFromLdap popup.

--- roles/tests-unit/files/FWO.Test/UiSearchUserAuthorizationTest.cs
+++ roles/tests-unit/files/FWO.Test/UiSearchUserAuthorizationTest.cs
@@ -9,6 +9,7 @@ using FWO.Middleware.Client;
 using FWO.Ui.Pages.Settings;
 using FWO.Ui.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
@@ -19,11 +20,26 @@ namespace FWO.Test
     internal class UiSearchUserAuthorizationTest
     {
         [Test]
-        public async Task SearchUser_DisablesAssignWhenExecutionModeDoesNotAllowAdmin()
+        public async Task SearchUser_EnablesAssignForAdminRoleWhenExecutionModeIsUserRoles()
         {
             await using BunitContext context = CreateContext([Roles.Admin, Roles.Modeller]);
 
-            IRenderedComponent<SearchUser> component = RenderSearchUser(context, true);
+            IRenderedComponent<SearchUser> component = RenderSearchUser(context);
+
+            component.WaitForAssertion(() =>
+            {
+                var assignButtons = FindAssignButtons(component);
+                Assert.That(assignButtons, Has.Count.EqualTo(2));
+                Assert.That(assignButtons.Any(button => button.HasAttribute("disabled")), Is.False);
+            });
+        }
+
+        [Test]
+        public async Task SearchUser_DisablesAssignWithoutAdminRole()
+        {
+            await using BunitContext context = CreateContext([Roles.Modeller]);
+
+            IRenderedComponent<SearchUser> component = RenderSearchUser(context);
 
             component.WaitForAssertion(() =>
             {
@@ -34,11 +50,11 @@ namespace FWO.Test
         }
 
         [Test]
-        public async Task SearchUser_CanUsePlainAdminAuthorizationForRoleAssignment()
+        public async Task SelectFromLdap_EnablesAssignForAdminRoleWhenExecutionModeIsUserRoles()
         {
             await using BunitContext context = CreateContext([Roles.Admin, Roles.Modeller]);
 
-            IRenderedComponent<SearchUser> component = RenderSearchUser(context, false);
+            IRenderedComponent<SelectFromLdap> component = RenderSelectFromLdap(context);
 
             component.WaitForAssertion(() =>
             {
@@ -53,30 +69,52 @@ namespace FWO.Test
             BunitContext context = new();
             context.JSInterop.Mode = JSRuntimeMode.Loose;
             context.Services.AddAuthorization();
-            context.Services.AddSingleton<IAuthorizationService, AllowAllAuthorizationService>();
+            context.Services.AddSingleton<IAuthorizationService, RoleAuthorizationService>();
             context.Services.AddScoped<DomEventService>();
             context.Services.AddSingleton<ApiConnection>(new SearchUserTestApiConnection());
-            context.Services.AddSingleton<UserConfig>(new SimulatedUserConfig
+            SimulatedUserConfig userConfig = new()
             {
                 User = { Name = "testuser", Roles = roles }
-            });
+            };
+            userConfig.SetExecutionMode(GlobalConst.kUserRolesSelection);
+            context.Services.AddSingleton<UserConfig>(userConfig);
             context.Services.AddSingleton<MiddlewareClient>(new MiddlewareClient("http://localhost/"));
             context.Services.AddSingleton<AuthenticationStateProvider>(new SearchUserAuthStateProvider(roles));
             return context;
         }
 
-        private static IRenderedComponent<SearchUser> RenderSearchUser(BunitContext context, bool useExecutionModeAuthorization)
+        private static IRenderedComponent<SearchUser> RenderSearchUser(BunitContext context)
         {
             return context.Render<CascadingAuthenticationState>(parameters => parameters
                 .AddChildContent<SearchUser>(childParameters => childParameters
                     .Add(parameter => parameter.Display, true)
                     .Add(parameter => parameter.UserSearchMode, true)
-                    .Add(parameter => parameter.GroupSearchMode, true)
-                    .Add(parameter => parameter.UseExecutionModeAuthorization, useExecutionModeAuthorization)))
+                    .Add(parameter => parameter.GroupSearchMode, true)))
                 .FindComponent<SearchUser>();
         }
 
-        private static List<AngleSharp.Dom.IElement> FindAssignButtons(IRenderedComponent<SearchUser> component)
+        private static IRenderedComponent<SelectFromLdap> RenderSelectFromLdap(BunitContext context)
+        {
+            return context.Render<CascadingAuthenticationState>(parameters => parameters
+                .AddChildContent<SelectFromLdap>(childParameters => childParameters
+                    .Add(parameter => parameter.Display, true)
+                    .Add(parameter => parameter.UserSelect, true)
+                    .Add(parameter => parameter.GroupSelect, true)
+                    .Add(parameter => parameter.LdapUsers, new List<UiUser>
+                    {
+                        new()
+                        {
+                            DbId = 2,
+                            Name = "ldapuser",
+                            Dn = "uid=ldapuser,ou=people,dc=example,dc=invalid"
+                        }
+                    })
+                    .Add(parameter => parameter.LdapGroups, ["cn=testgroup,ou=groups,dc=example,dc=invalid"])))
+                .FindComponent<SelectFromLdap>();
+        }
+
+        private static List<AngleSharp.Dom.IElement> FindAssignButtons<TComponent>(IRenderedComponent<TComponent> component)
+            where TComponent : Microsoft.AspNetCore.Components.IComponent
         {
             return component.FindAll("button")
                 .Where(button => button.TextContent.Trim() == "assign")
@@ -120,6 +158,27 @@ namespace FWO.Test
                     return Task.FromResult((QueryResponseType)users);
                 }
                 throw new InvalidOperationException($"Unexpected query: {query}");
+            }
+        }
+
+        private sealed class RoleAuthorizationService : IAuthorizationService
+        {
+            public Task<AuthorizationResult> AuthorizeAsync(ClaimsPrincipal user, object? resource, IEnumerable<IAuthorizationRequirement> requirements)
+            {
+                foreach (RolesAuthorizationRequirement roleRequirement in requirements.OfType<RolesAuthorizationRequirement>())
+                {
+                    if (!roleRequirement.AllowedRoles.Any(user.IsInRole))
+                    {
+                        return Task.FromResult(AuthorizationResult.Failed());
+                    }
+                }
+
+                return Task.FromResult(AuthorizationResult.Success());
+            }
+
+            public Task<AuthorizationResult> AuthorizeAsync(ClaimsPrincipal user, object? resource, string policyName)
+            {
+                return Task.FromResult(AuthorizationResult.Success());
             }
         }
     }

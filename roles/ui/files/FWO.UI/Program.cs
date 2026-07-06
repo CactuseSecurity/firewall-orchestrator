c62c9eaa7fd2532adbf9437c1cc3a4831fbebd51
Luca <luca.weidmann@gmx.de>
2026-07-06 13:42:21 +0200

    remove custom timeout that i added assuming keep alive would help

--- roles/ui/files/FWO.UI/Program.cs
+++ roles/ui/files/FWO.UI/Program.cs
@@ -44,13 +44,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor()
-    .AddHubOptions(options =>
-    {
-        options.ClientTimeoutInterval = TimeSpan.FromMinutes(5);
-        options.KeepAliveInterval = TimeSpan.FromSeconds(15);
-        options.HandshakeTimeout = TimeSpan.FromSeconds(30);
-    });
+builder.Services.AddServerSideBlazor();
 
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 builder.Services.AddScoped<KeyboardInputService, KeyboardInputService>();

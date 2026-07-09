9bda64fec7fcabde7002eca1ba0150aa1616e25f
Elias Kolbenschlag <ekolbenschlag@online.de>
2026-07-09 11:55:38 +0200

    homogenize query name

--- roles/tests-unit/files/FWO.Test/FlowQueriesTest.cs
+++ roles/tests-unit/files/FWO.Test/FlowQueriesTest.cs
@@ -28,7 +28,7 @@ namespace FWO.Test
             Assert.That(FlowQueries.getFlowServiceGroups, Does.Contain("query getFlowServiceGroups"));
             Assert.That(FlowQueries.getFlowServiceGroups, Does.Contain("fragment flowSvcGroupWithMembersFragment"));
             Assert.That(FlowQueries.getFlowServiceGroups, Does.Not.Contain("services("));
-            Assert.That(FlowQueries.getFlowTimeObjectId, Does.Contain("query getTimeObjectId"));
+            Assert.That(FlowQueries.getFlowTimeObjectId, Does.Contain("query getFlowTimeObjectId"));
             Assert.That(FlowQueries.getFlowTimeObjectId, Does.Contain("timeobjects: flow_timeobject(where: $where, order_by: [{ timeobj_id: asc }], limit: 1)"));
             Assert.That(FlowQueries.getFlowTimeObjectId, Does.Contain("timeobj_id"));
             Assert.That(FlowQueries.getFlowTimeObjectId, Does.Contain("name"));

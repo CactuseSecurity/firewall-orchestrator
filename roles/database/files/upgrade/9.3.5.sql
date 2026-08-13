199a677641af0c36f0a03e48e0d272a29652e02c
Yannik Hayn <yannik@hayn.de>
2026-08-13 14:18:32 +0200

    fix: refresh view_rule_with_owner after changing ips

--- roles/database/files/upgrade/9.3.5.sql
+++ roles/database/files/upgrade/9.3.5.sql
@@ -4,3 +4,6 @@ SET
     obj_ip = NULL,
     obj_ip_end = NULL
 WHERE obj_typ_id IN (5, 10);
+
+-- Recalculate ownership matches after removing the previously imported full address ranges.
+REFRESH MATERIALIZED VIEW view_rule_with_owner;

5bde350625af5940191a5e9cdaa7b9daefba65a6
Yannik Hayn <yannik@hayn.de>
2026-08-24 09:36:53 +0200

    feat: add access-role to upgrade script setting ip to null

--- roles/database/files/upgrade/9.4.1.sql
+++ roles/database/files/upgrade/9.4.1.sql
@@ -1,9 +1,9 @@
--- update fqdn and dynamic ip network objects to have NULL ip and ip_end values
+-- update fqdn, dynamic and access-role network objects to have NULL ip and ip_end values
 UPDATE nw_object
 SET
     obj_ip = NULL,
     obj_ip_end = NULL
-WHERE obj_typ_id IN (5, 10);
+WHERE obj_typ_id IN (5, 10, 21);
 
 -- Recalculate ownership matches after removing the previously imported full address ranges.
 REFRESH MATERIALIZED VIEW view_rule_with_owner;

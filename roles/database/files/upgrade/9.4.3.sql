2a020b3ab4b4949426aff0153923d0732a46bc4f
Yannik Hayn <yannik@hayn.de>
2026-08-27 09:23:11 +0200

    fix(upgrade): remove legacy dummy flow object

--- roles/database/files/upgrade/9.4.3.sql
+++ roles/database/files/upgrade/9.4.3.sql
@@ -10,5 +10,11 @@ SET
 WHERE obj_typ_id IN (5, 10, 21)
     AND (obj_ip IS NOT NULL OR obj_ip_end IS NOT NULL);
 
+-- Remove the legacy dummy Flow network object used for access roles.
+-- May be recreated by the Flow sync process later if there are other objects with dummy addresses.
+DELETE FROM flow.nwobject
+WHERE ip_start = '0.0.0.0/32'::cidr
+    AND ip_end = '0.0.0.0/32'::cidr;
+
 -- Recalculate ownership matches after removing the previously imported full address ranges.
 REFRESH MATERIALIZED VIEW view_rule_with_owner;
